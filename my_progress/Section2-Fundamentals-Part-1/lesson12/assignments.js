8550386b1cfb022c417b5cdac29e315b8ce22b20
Evgeniy_Utkin <eugene.utkin@gmail.com>
2022-02-16 18:03:08 +0500

    homework done

--- my_progress/Section2-Fundamentals-Part-1/lesson12/assignments.js
+++ my_progress/Section2-Fundamentals-Part-1/lesson12/assignments.js
@@ -1,7 +1,10 @@
 let country = 'Russia';
 let continent = 'Eurasia';
 let population = '145';
+let isIsland = false;
+let language;
 
-console.log(country);
-console.log(continent);
-console.log(population);
+console.log(typeof isIsland);
+console.log(typeof population);
+console.log(typeof country);
+console.log(typeof language);
