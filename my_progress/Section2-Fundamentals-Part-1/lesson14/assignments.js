61ceddd2f8e931fe75d08cbf361399992973a971
Evgeniy_Utkin <eugene.utkin@gmail.com>
2022-02-23 19:00:19 +0500

    comment added

---
+++ my_progress/Section2-Fundamentals-Part-1/lesson14/assignments.js
@@ -0,0 +1,13 @@
+let country = 'Russia';
+const continent = 'Eurasia';
+let population = '145';
+const isIsland = false;
+let language;
+
+language = 'Russian';
+// isIsland = true;
+
+console.log(typeof isIsland);
+console.log(typeof population);
+console.log(typeof country);
+console.log(typeof language);
