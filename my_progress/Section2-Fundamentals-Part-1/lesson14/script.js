e8356ded847e417364126da618d76fe5cab0fd5f
Evgeniy_Utkin <eugene.utkin@gmail.com>
2022-02-25 21:45:27 +0500

    Comparison operators added

--- my_progress/Section2-Fundamentals-Part-1/lesson14/script.js
+++ my_progress/Section2-Fundamentals-Part-1/lesson14/script.js
@@ -60,9 +60,10 @@ lastName = 'Schmedtmann';
 console.log(lastName);
 */
 
+// Math operators
 const now = 2037;
 const ageJonas = now - 1991;
-const ageSarah = now - 2018;
+const ageSarah = now - 2020;
 console.log(ageJonas, ageSarah);
 
 console.log(ageJonas * 2, ageJonas / 10, 2 ** 3);
@@ -72,7 +73,15 @@ const firstName = 'Jonas';
 const lastName = 'Schmedtmann';
 console.log(firstName + ' ' + lastName);
 
+// Assignment operators
 let x = 10 + 5; // 15
 x += 10; // x = x + 10 = 25
 x *= 4; // x = x * 4 = 100
+x++; // x = x + 1
+x--;
+x--;
 console.log(x);
+
+// Comparison operators
+console.log(ageJonas > ageSarah); // >, <, >=, <=
+console.log(ageSarah >= 18);
