77ba67f20ec858e977d37b9874dadaa93bbb12f6
Evgeniy_Utkin <eugene.utkin@gmail.com>
2022-05-31 20:53:38 +0500

    homework half done:

--- my_progress/Section2-Fundamentals-Part-1/lesson14/assignments.js
+++ my_progress/Section2-Fundamentals-Part-1/lesson14/assignments.js
@@ -1,11 +1,15 @@
 let country = 'Russia';
 const continent = 'Eurasia';
-let population = '145';
+let population = 145;
 const isIsland = false;
 let language;
 
 language = 'Russian';
 // isIsland = true;
 
-population /= 2; // population = 72.5 
-console.log(population);
+halfPopulation = population / 2; // population = 72.5
+increasedPopulation = population + 1; // population = 146
+console.log(increasedPopulation);
+
+let finlandPopulation = 6;
+console.log(population > finlandPopulation);
