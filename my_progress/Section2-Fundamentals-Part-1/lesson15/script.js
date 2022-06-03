c5de3abde3fb90a878d628e4044be6978270bfec
Evgeniy_Utkin <eugene.utkin@gmail.com>
2022-06-03 19:14:21 +0500

    averageAge const added

--- my_progress/Section2-Fundamentals-Part-1/lesson15/script.js
+++ my_progress/Section2-Fundamentals-Part-1/lesson15/script.js
@@ -97,5 +97,8 @@ const ageSarah = now - 2018;
 console.log(now - 1991 > now - 2018);
 
 let x, y;
-x = y = 25 - 10 - 5; // x = y = 10
+x = y = 25 - 10 - 5; // x = y = 10, x = 10
 console.log(x, y);
+
+const averageAge = ageJonas + ageSarah / 2
+console.log(ageJonas, ageSarah, averageAge);
