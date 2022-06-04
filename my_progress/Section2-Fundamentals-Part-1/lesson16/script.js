6541065d7c245c63e79f55c92c4f0379e7884d99
Evgeniy_Utkin <eugene.utkin@gmail.com>
2022-06-04 15:31:09 +0500

    console.log for Case 1 added

--- my_progress/Section2-Fundamentals-Part-1/lesson16/script.js
+++ my_progress/Section2-Fundamentals-Part-1/lesson16/script.js
@@ -7,3 +7,4 @@ let markBMI = markWeight / markHeight ** 2;
 let johnBMI = johnWeight / (johnHeight * johnHeight);
 
 let markHigherBMI = markBMI > johnBMI;
+console.log("Case 1. Mark's BMI is higher than John's: " + markHigherBMI);
