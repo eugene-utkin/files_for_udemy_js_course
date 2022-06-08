36e53b7c2a1ce7396460fb0cc912fceabb3ff319
Evgeniy_Utkin <eugene.utkin@gmail.com>
2022-06-08 21:19:03 +0500

    typo fixed

--- my_progress/Section2-Fundamentals-Part-1/lesson17/script.js
+++ my_progress/Section2-Fundamentals-Part-1/lesson17/script.js
@@ -137,7 +137,7 @@ const job = 'teacher';
 const birthYear = 1991;
 const year = 2037;
 
-const jonas = "I'm " + firstName + ', a ' + (year - birthYear) + ' years old ' + job + '!';
+const jonas = "I'm " + firstName + ', a ' + (year - birthYear) + ' year old ' + job + '!';
 console.log(jonas);
 
 const jonasNew = `I'm ${firstName}, a ${year - birthYear} year old ${job}!`;
