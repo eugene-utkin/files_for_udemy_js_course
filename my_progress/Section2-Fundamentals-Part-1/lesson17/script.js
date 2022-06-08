0e77afa4bd3c6e0a7cde2f79a65093cb7e33dd39
Evgeniy_Utkin <eugene.utkin@gmail.com>
2022-06-08 20:16:05 +0500

    const jonasNew updated

--- my_progress/Section2-Fundamentals-Part-1/lesson17/script.js
+++ my_progress/Section2-Fundamentals-Part-1/lesson17/script.js
@@ -140,5 +140,5 @@ const year = 2037;
 const jonas = "I'm " + firstName + ', a ' + (year - birthYear) + ' years old ' + job + '!';
 console.log(jonas);
 
-const jonasNew = `I'm ${firstName}`;
+const jonasNew = `I'm ${firstName}, a ${year - birthYear} year old ${job}!`;
 console.log(jonasNew);
