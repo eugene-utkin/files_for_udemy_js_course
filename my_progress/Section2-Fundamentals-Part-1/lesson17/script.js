487e01b8b921fbb16711be74dc2a17e1126d7770
Evgeniy_Utkin <eugene.utkin@gmail.com>
2022-06-09 21:22:55 +0500

    string with multiple lines (old way)

--- my_progress/Section2-Fundamentals-Part-1/lesson17/script.js
+++ my_progress/Section2-Fundamentals-Part-1/lesson17/script.js
@@ -144,3 +144,7 @@ const jonasNew = `I'm ${firstName}, a ${year - birthYear} year old ${job}!`;
 console.log(jonasNew);
 
 console.log(`Just a regular string...`);
+
+console.log('String with \n\
+multiple \n\
+lines')
