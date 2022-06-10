db0c10f06d5084c9301c4df1004a00d7e291bb94
Evgeniy_Utkin <eugene.utkin@gmail.com>
2022-06-10 20:16:38 +0500

    else console log added

--- my_progress/Section2-Fundamentals-Part-1/lesson18/script.js
+++ my_progress/Section2-Fundamentals-Part-1/lesson18/script.js
@@ -160,4 +160,5 @@ if (age >= 18) {
 	console.log('Sarah can start driving license 🚗');
 } else {
 	const yearsLeft = 18 - age;
+	console.log(`Sarah is too young. Wait another ${yearsLeft} years :)`);
 }
