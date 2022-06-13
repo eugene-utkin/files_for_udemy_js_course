2153b08a70f2eda50887f938d817cea430505a2c
Evgeniy_Utkin <eugene.utkin@gmail.com>
2022-06-13 18:45:35 +0500

    johnBMI var added to console.log fro case 1

--- my_progress/Section2-Fundamentals-Part-1/lesson19/script.js
+++ my_progress/Section2-Fundamentals-Part-1/lesson19/script.js
@@ -181,11 +181,10 @@ let johnWeight = 92;
 
 let markBMI = markWeight / markHeight ** 2;
 let johnBMI = johnWeight / (johnHeight * johnHeight);
-console.log("John's BMI: " + johnBMI);
 
 let markHigherBMI = markBMI > johnBMI;
 if (markHigherBMI) {
-  console.log(`Mark's BMI (${markBMI}) is higher than John's!`);
+  console.log(`Mark's BMI (${markBMI}) is higher than John's (${johnBMI})!`);
 } else {
   console.log("John's BMI is higher than Mark's!");
 }
