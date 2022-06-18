08ae16f9fb47b64e57728d3df03e07ce6f0f54fe
Evgeniy_Utkin <eugene.utkin@gmail.com>
2022-06-18 18:22:55 +0500

    assignments file added

---
+++ my_progress/Section2-Fundamentals-Part-1/lesson20/assignments.js
@@ -0,0 +1,32 @@
+let country = 'Russia';
+const continent = 'Eurasia';
+let population = 145;
+const isIsland = false;
+let language;
+
+language = 'Russian';
+
+const halfPopulation = population / 2; // population = 72.5
+console.log(halfPopulation);
+
+const increasedPopulation = population + 1; // population = 146
+console.log(increasedPopulation);
+
+const finlandPopulation = 6;
+console.log(population > finlandPopulation);
+
+const averagePopulation = 33;
+console.log(population < averagePopulation);
+
+const description = country + ' is in ' + continent + ', and its ' + population + ' million people speak ' + language
+console.log(description);
+
+console.log(`${country} is in ${continent}, and its ${population} million people speak ${language}`);
+
+if (population > 33) {
+  console.log(`${country}'s population is above average`);
+} else {
+  const populationLeft = 33 - population;
+  console.log(`${country}'s population is ${populationLeft} million below average`);
+}
+
