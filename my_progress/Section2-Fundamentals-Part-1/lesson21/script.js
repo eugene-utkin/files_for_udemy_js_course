4ed9d63f75620553f873b9ab296922b445d44a99
Evgeniy_Utkin <eugene.utkin@gmail.com>
2022-06-21 18:32:18 +0500

    money reassigned

--- my_progress/Section2-Fundamentals-Part-1/lesson21/script.js
+++ my_progress/Section2-Fundamentals-Part-1/lesson21/script.js
@@ -240,7 +240,7 @@ console.log(Boolean('Jonas'));
 console.log(Boolean({}));
 console.log(Boolean(''));
 
-const money = 0;
+const money = 100;
 if (money) {
   console.log("Don't spend it all ;)");
 } else {
