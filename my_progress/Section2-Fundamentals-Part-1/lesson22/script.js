9de6f47faeb3b94a824046f9eed6c396ea3751d4
Evgeniy_Utkin <eugene.utkin@gmail.com>
2022-06-23 20:21:11 +0500

    if condition added to favourite

--- my_progress/Section2-Fundamentals-Part-1/lesson22/script.js
+++ my_progress/Section2-Fundamentals-Part-1/lesson22/script.js
@@ -264,3 +264,7 @@ if (age == 18) console.log('You just became an adult :D (loose)');
 const favourite = prompt("What's your favourite number?");
 console.log(favourite);
 console.log(typeof favourite);
+
+if (favourite == 23) {
+  console.log('Cool! 23 is an amazing number!');
+}
