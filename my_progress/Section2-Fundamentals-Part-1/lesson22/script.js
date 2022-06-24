d19545dd789e024adde2ebe11fc16e6d53cc2532
Evgeniy_Utkin <eugene.utkin@gmail.com>
2022-06-24 19:41:19 +0500

    number 9 condition added

--- my_progress/Section2-Fundamentals-Part-1/lesson22/script.js
+++ my_progress/Section2-Fundamentals-Part-1/lesson22/script.js
@@ -269,6 +269,8 @@ if (favourite === 23) { // 22 === 23 -> FALSE
   console.log('Cool! 23 is an amazing number!');
 } else if (favourite === 7) {
   console.log('7 is also a cool number');
+} else if (favourite === 9) {
+  console.log('9 is also a cool number');
 } else {
-  console.log('Number is not 23 or 7');
+  console.log('Number is not 23 or 7 or 9');
 }
