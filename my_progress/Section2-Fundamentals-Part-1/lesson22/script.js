2572c85495ab4e597aae717c9512bf3dbaa081e0
Evgeniy_Utkin <eugene.utkin@gmail.com>
2022-06-24 19:39:03 +0500

    else condition added to favourite

--- my_progress/Section2-Fundamentals-Part-1/lesson22/script.js
+++ my_progress/Section2-Fundamentals-Part-1/lesson22/script.js
@@ -269,4 +269,6 @@ if (favourite === 23) { // 22 === 23 -> FALSE
   console.log('Cool! 23 is an amazing number!');
 } else if (favourite === 7) {
   console.log('7 is also a cool number');
+} else {
+  console.log('Number is not 23 or 7');
 }
