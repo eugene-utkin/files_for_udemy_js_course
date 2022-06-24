f8f0f139fe79c8e812c9712f5f56975c20115f50
Evgeniy_Utkin <eugene.utkin@gmail.com>
2022-06-24 19:34:04 +0500

    comment added

--- my_progress/Section2-Fundamentals-Part-1/lesson22/script.js
+++ my_progress/Section2-Fundamentals-Part-1/lesson22/script.js
@@ -265,6 +265,6 @@ const favourite = Number(prompt("What's your favourite number?"));
 console.log(favourite);
 console.log(typeof favourite);
 
-if (favourite === 23) {
+if (favourite === 23) { // 22 === 23 -> FALSE
   console.log('Cool! 23 is an amazing number!');
 }
