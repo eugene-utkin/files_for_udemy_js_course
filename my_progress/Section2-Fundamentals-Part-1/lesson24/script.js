86bfe492273a195bb5fdf3ae26ee868a742ac244
Evgeniy_Utkin <eugene.utkin@gmail.com>
2022-06-27 20:00:47 +0500

    if condition added to shouldDrive

--- my_progress/Section2-Fundamentals-Part-1/lesson24/script.js
+++ my_progress/Section2-Fundamentals-Part-1/lesson24/script.js
@@ -287,3 +287,7 @@ console.log(hasDriversLicense || hasGoodVision);
 console.log(!hasDriversLicense);
 
 const shouldDrive = hasDriversLicense && hasGoodVision;
+
+if (shouldDrive) {
+  console.log('Sarah is able to drive!');
+}
