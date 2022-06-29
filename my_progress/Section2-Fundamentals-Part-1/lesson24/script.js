ea346632245374050e8068ce68fac7df01f34198
Evgeniy_Utkin <eugene.utkin@gmail.com>
2022-06-29 16:20:44 +0500

    !isTired used

--- my_progress/Section2-Fundamentals-Part-1/lesson24/script.js
+++ my_progress/Section2-Fundamentals-Part-1/lesson24/script.js
@@ -296,3 +296,8 @@ console.log(!hasDriversLicense);
 const isTired = true; // C
 console.log(hasDriversLicense && hasGoodVision && isTired);
 
+if (hasDriversLicense && hasGoodVision && !isTired) {
+  console.log('Sarah is able to drive!');
+} else {
+  console.log('Someone else should drive...');
+}
