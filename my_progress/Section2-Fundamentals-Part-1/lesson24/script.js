78beb732805f703a936e06f019a90fa476ef2564
Evgeniy_Utkin <eugene.utkin@gmail.com>
2022-06-29 16:19:34 +0500

    old condition commented

--- my_progress/Section2-Fundamentals-Part-1/lesson24/script.js
+++ my_progress/Section2-Fundamentals-Part-1/lesson24/script.js
@@ -287,11 +287,12 @@ console.log(hasDriversLicense || hasGoodVision);
 console.log(!hasDriversLicense);
 
 
-if (hasDriversLicense && hasGoodVision) {
-  console.log('Sarah is able to drive!');
-} else {
-  console.log('Someone else should drive...');
-}
+// if (hasDriversLicense && hasGoodVision) {
+//   console.log('Sarah is able to drive!');
+// } else {
+//   console.log('Someone else should drive...');
+// }
 
-const isTired = false; // C
+const isTired = true; // C
 console.log(hasDriversLicense && hasGoodVision && isTired);
+
