bb0ae8789db0efd11edc6c2cb257391c2e38c287
Evgeniy_Utkin <eugene.utkin@gmail.com>
2022-06-29 16:33:52 +0500

    variables for assignment added

--- my_progress/Section2-Fundamentals-Part-1/lesson24/assignments.js
+++ my_progress/Section2-Fundamentals-Part-1/lesson24/assignments.js
@@ -8,3 +8,10 @@
 // 	console.log('No borders');
 // }
 
+let country = 'Russia';
+const continent = 'Eurasia';
+let population = 145;
+const isIsland = false;
+let language;
+
+language = 'Russian';
