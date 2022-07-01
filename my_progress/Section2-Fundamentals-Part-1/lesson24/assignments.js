70e601dc02988396f11d6330a26d377114790fef
Evgeniy_Utkin <eugene.utkin@gmail.com>
2022-07-01 19:16:48 +0600

    else condition added

--- my_progress/Section2-Fundamentals-Part-1/lesson24/assignments.js
+++ my_progress/Section2-Fundamentals-Part-1/lesson24/assignments.js
@@ -18,4 +18,6 @@ language = 'Russian';
 
 if ((language === "English") && (population < 50) && !isIsland) {
 	console.log(`You should live in ${country} :)`);
+} else {
+	console.log('Portugal does not meet your criteria :(');
 }
