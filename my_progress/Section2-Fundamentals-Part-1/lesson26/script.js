185c3afe284d7335a4d6751b33be3c5c2b5c7487
Evgeniy_Utkin <eugene.utkin@gmail.com>
2022-07-09 18:22:25 +0600

    if tuesday added

--- my_progress/Section2-Fundamentals-Part-1/lesson26/script.js
+++ my_progress/Section2-Fundamentals-Part-1/lesson26/script.js
@@ -388,4 +388,6 @@ switch (day) {
 if (day === 'monday') {
   console.log('Plan course structure');
   console.log('Go to coding meetup');
+} else if (day === 'tuesday') {
+  console.log('Prepare theory videos');
 }
