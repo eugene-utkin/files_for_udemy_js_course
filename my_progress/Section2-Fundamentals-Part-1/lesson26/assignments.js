9bbb5bd24c3313c2aec31afe8f4343b207b10c7a
Evgeniy_Utkin <eugene.utkin@gmail.com>
2022-07-10 17:04:13 +0600

    extra cide removed

--- my_progress/Section2-Fundamentals-Part-1/lesson26/assignments.js
+++ my_progress/Section2-Fundamentals-Part-1/lesson26/assignments.js
@@ -1,13 +1,3 @@
-// let numNeighbours = Number(prompt('How many neighbour countries does your country have?'));
-
-// if (numNeighbours === 1) {
-// 	console.log('Only 1 border!');
-// } else if (numNeighbours > 1) {
-// 	console.log('More than 1 border');
-// } else {
-// 	console.log('No borders');
-// }
-
 let country = 'Russia';
 const continent = 'Eurasia';
 let population = 145;
@@ -16,8 +6,3 @@ let language;
 
 language = 'Russian';
 
-if ((language === "English") && (population < 50) && !isIsland) {
-	console.log(`You should live in ${country} :)`);
-} else {
-	console.log(`${country} does not meet your criteria :(`);
-}
