d73db0c0c8aabdf1e4faf5561a06f12f408814ae
Evgeniy_Utkin <eugene.utkin@gmail.com>
2022-07-13 18:21:44 +0600

    console log drink added

--- my_progress/Section2-Fundamentals-Part-1/lesson28/script.js
+++ my_progress/Section2-Fundamentals-Part-1/lesson28/script.js
@@ -419,3 +419,4 @@ const age = 23;
 age >= 18 ? console.log('I like to drink wine 🍷') : console.log('I like to drink water 💧');
 
 const drink = age >= 18 ? 'wine 🍷' : 'water 💧';
+console.log(drink);
