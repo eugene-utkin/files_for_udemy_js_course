87a18f8ff6d76498b359b1d7e4ecc2a274afc128
Evgeniy_Utkin <eugene.utkin@gmail.com>
2022-07-13 18:20:55 +0600

    const drink added

--- my_progress/Section2-Fundamentals-Part-1/lesson28/script.js
+++ my_progress/Section2-Fundamentals-Part-1/lesson28/script.js
@@ -417,3 +417,5 @@ console.log(`I'm ${2037 - 1991} years old ${me}.`)
 
 const age = 23;
 age >= 18 ? console.log('I like to drink wine 🍷') : console.log('I like to drink water 💧');
+
+const drink = age >= 18 ? 'wine 🍷' : 'water 💧';
