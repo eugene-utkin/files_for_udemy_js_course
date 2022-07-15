0266174abef0c9e8e9f6378e89b8733929567ea7
Evgeniy_Utkin <eugene.utkin@gmail.com>
2022-07-15 17:43:30 +0600

    assignments file added

---
+++ my_progress/Section2-Fundamentals-Part-1/lesson28/assignments.js
@@ -0,0 +1,28 @@
+let country = 'Russia';
+const continent = 'Eurasia';
+let population = 145;
+const isIsland = false;
+let language;
+
+language = 'Russian';
+
+switch (language) {
+	case 'chinese':
+	case 'mandarin':
+		console.log('MOST number of native speakers');
+		break;
+	case 'spanish':
+		console.log('2nd place in number of native speakers');
+		break;
+	case 'english':
+		console.log('3rd place');
+		break;
+	case 'hindi':
+		console.log('Number 4');
+		break;
+	case 'arabic':
+		console.log('5th most spoken language');
+		break;
+	default:
+		console.log('Great language too :D');
+}
