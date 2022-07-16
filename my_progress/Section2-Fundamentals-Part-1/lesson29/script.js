501bb660a548d5fabe34d17d71aab2581dd322fa
Evgeniy_Utkin <eugene.utkin@gmail.com>
2022-07-16 18:36:46 +0600

    console log added to case 1

--- my_progress/Section2-Fundamentals-Part-1/lesson29/script.js
+++ my_progress/Section2-Fundamentals-Part-1/lesson29/script.js
@@ -434,4 +434,5 @@ console.log(`I like to drink ${age >= 18 ? 'wine 🍷' : 'water 💧'}`);
 
 let billValue = 275;
 let tip = billValue >= 50 && billValue <= 300 ? billValue * 0.15 : billValue * 0.2;
+console.log(`The bill was ${billValue}, the tip was ${tip}, and the total value ${billValue + tip}`);
 
