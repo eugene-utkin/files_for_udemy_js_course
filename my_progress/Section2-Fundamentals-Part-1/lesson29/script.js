8d629803bf75d5eeb573232feaf26873dbc43d7d
Evgeniy_Utkin <eugene.utkin@gmail.com>
2022-07-16 18:38:40 +0600

    empty console log added to case 1

--- my_progress/Section2-Fundamentals-Part-1/lesson29/script.js
+++ my_progress/Section2-Fundamentals-Part-1/lesson29/script.js
@@ -436,3 +436,4 @@ let billValue = 275;
 let tip = billValue >= 50 && billValue <= 300 ? billValue * 0.15 : billValue * 0.2;
 console.log('Case 1');
 console.log(`The bill was ${billValue}, the tip was ${tip}, and the total value ${billValue + tip}`);
+console.log('');
