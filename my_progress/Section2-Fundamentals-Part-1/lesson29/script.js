d52b7842f193a4463bf456224f721aa401c8242b
Evgeniy_Utkin <eugene.utkin@gmail.com>
2022-07-16 18:40:39 +0600

    console log added to case 2

--- my_progress/Section2-Fundamentals-Part-1/lesson29/script.js
+++ my_progress/Section2-Fundamentals-Part-1/lesson29/script.js
@@ -440,3 +440,4 @@ console.log('');
 
 billValue = 40;
 tip = billValue >= 50 && billValue <= 300 ? billValue * 0.15 : billValue * 0.2;
+console.log(`The bill was ${billValue}, the tip was ${tip}, and the total value ${billValue + tip}`);
