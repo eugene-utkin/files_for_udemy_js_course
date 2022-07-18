415ffd361a3702aab9f1bd7af5579f673cc01a4e
Evgeniy_Utkin <eugene.utkin@gmail.com>
2022-07-18 18:20:57 +0600

    test bug fixed

--- my_progress/Section3-Fundamentals-Part-2/lesson32/script.js
+++ my_progress/Section3-Fundamentals-Part-2/lesson32/script.js
@@ -1,7 +1,7 @@
-// 'use strict';
+'use strict';
 
 let hasDriversLicense = false;
 const passTest = true;
 
-if (passTest) hasDriverLicense = true;
+if (passTest) hasDriversLicense = true;
 if (hasDriversLicense) console.log('I can drive :D');
