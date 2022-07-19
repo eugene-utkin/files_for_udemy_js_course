f108bf526d3ecd4743051131d3a65cf45ed9cbd3
Evgeniy_Utkin <eugene.utkin@gmail.com>
2022-07-19 19:16:51 +0600

    old code commented out

--- my_progress/Section3-Fundamentals-Part-2/lesson32/script.js
+++ my_progress/Section3-Fundamentals-Part-2/lesson32/script.js
@@ -1,3 +1,4 @@
+/*
 'use strict';
 
 let hasDriversLicense = false;
@@ -8,3 +9,5 @@ if (hasDriversLicense) console.log('I can drive :D');
 
 // const interface = 'Audio';
 // const private = 534;
+*/
+
