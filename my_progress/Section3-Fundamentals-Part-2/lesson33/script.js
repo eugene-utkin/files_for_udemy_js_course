a3be7a2b8c9692efd73c457d22c068737a7660ce
Evgeniy_Utkin <eugene.utkin@gmail.com>
2022-07-21 18:38:45 +0600

    console log removed from fuction

--- my_progress/Section3-Fundamentals-Part-2/lesson33/script.js
+++ my_progress/Section3-Fundamentals-Part-2/lesson33/script.js
@@ -21,7 +21,6 @@ logger();
 logger();
 
 function fruitProcessor(apples, oranges) {
-	console.log(apples, oranges);
 	const juice = `Juice with ${apples} apples and ${oranges} oranges.`;
 	return juice;
 }
