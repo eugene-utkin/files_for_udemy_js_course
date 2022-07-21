5ec024036b8f82a730c0ebce8310261543e0d372
Evgeniy_Utkin <eugene.utkin@gmail.com>
2022-07-21 18:50:51 +0600

    assignments file added

---
+++ my_progress/Section3-Fundamentals-Part-2/lesson33/assignments.js
@@ -0,0 +1,10 @@
+let country = 'Russia';
+const continent = 'Eurasia';
+let population = 145;
+const isIsland = false;
+let language;
+
+language = 'Russian';
+
+const populationComparison = population > 33 ? 'above' : 'below';
+console.log(`${country}'s population is ${populationComparison} average`);
