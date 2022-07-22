4bc423c3c4229ce021bdf8f384853168293115ed
Evgeniy_Utkin <eugene.utkin@gmail.com>
2022-07-22 18:10:46 +0600

    const description added to describeCountry function

--- my_progress/Section3-Fundamentals-Part-2/lesson33/assignments.js
+++ my_progress/Section3-Fundamentals-Part-2/lesson33/assignments.js
@@ -10,5 +10,5 @@
 // console.log(`${country}'s population is ${populationComparison} average`);
 
 function describeCountry(country, population, capitalCity) {
-
+	const description = `Finland has 6 million people and its capital city is Helsinki`;
 }
