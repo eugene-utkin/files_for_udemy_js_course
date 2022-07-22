7609aa642421595d426813ba2e96273c488bd78c
Evgeniy_Utkin <eugene.utkin@gmail.com>
2022-07-22 17:52:06 +0600

    old code commented out

--- my_progress/Section3-Fundamentals-Part-2/lesson33/assignments.js
+++ my_progress/Section3-Fundamentals-Part-2/lesson33/assignments.js
@@ -1,10 +1,11 @@
-let country = 'Russia';
-const continent = 'Eurasia';
-let population = 145;
-const isIsland = false;
-let language;
+// let country = 'Russia';
+// const continent = 'Eurasia';
+// let population = 145;
+// const isIsland = false;
+// let language;
 
-language = 'Russian';
+// language = 'Russian';
 
-const populationComparison = population > 33 ? 'above' : 'below';
-console.log(`${country}'s population is ${populationComparison} average`);
+// const populationComparison = population > 33 ? 'above' : 'below';
+// console.log(`${country}'s population is ${populationComparison} average`);
+
