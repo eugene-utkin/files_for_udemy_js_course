6ee749f27a27877ec803bd41ca2161e25c271412
Evgeniy_Utkin <eugene.utkin@gmail.com>
2022-07-26 21:23:05 +0600

    assignments file added

---
+++ my_progress/Section3-Fundamentals-Part-2/lesson34/assignments.js
@@ -0,0 +1,24 @@
+// let country = 'Russia';
+// const continent = 'Eurasia';
+// let population = 145;
+// const isIsland = false;
+// let language;
+
+// language = 'Russian';
+
+// const populationComparison = population > 33 ? 'above' : 'below';
+// console.log(`${country}'s population is ${populationComparison} average`);
+
+function describeCountry(country, population, capitalCity) {
+	const description = `${country} has ${population} million people and its capital city is ${capitalCity}`;
+	return description;
+}
+
+const finlandDescription = describeCountry("Finland", 6, "Helsinki");
+console.log(finlandDescription);
+
+const kyrgyzstanDescription = describeCountry("Kyrgyzstan", 7, "Bishkek");
+console.log(kyrgyzstanDescription);
+
+const englandDescription = describeCountry("England", 56, "London");
+console.log(englandDescription);
