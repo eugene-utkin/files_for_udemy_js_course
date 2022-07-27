cdd2c448e62e9c0482493a849bc643cfe7e9a669
Evgeniy_Utkin <eugene.utkin@gmail.com>
2022-07-27 16:05:18 +0600

    kyrgyzstanComparison variable added

--- my_progress/Section3-Fundamentals-Part-2/lesson34/assignments.js
+++ my_progress/Section3-Fundamentals-Part-2/lesson34/assignments.js
@@ -19,7 +19,7 @@ function percentageOfWorld1(population) {
 }
 
 let chinaPopulation = percentageOfWorld1(1441);
-let chinaComparison = `China has 1441 million people, so it's about ${chinaPopulation}% of
-the world population.`
+let chinaComparison = `China has 1441 million people, so it's about ${chinaPopulation}% of the world population.`
 let kyrgyzstanPopulation = percentageOfWorld1(6.7);
+let kyrgyzstanComparison = `Kirgizstan has 6.7 million people, so it's about ${kyrgyzstanPopulation}% of the world population.`
 let russiaPopulation = percentageOfWorld1(145);
