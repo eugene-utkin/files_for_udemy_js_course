650c8db49344bf358206c08cc9935e514df11f86
Evgeniy_Utkin <eugene.utkin@gmail.com>
2022-07-28 20:17:36 +0600

    roundedKyrgyzstanPopulation variable added

--- my_progress/Section3-Fundamentals-Part-2/lesson34/assignments.js
+++ my_progress/Section3-Fundamentals-Part-2/lesson34/assignments.js
@@ -22,6 +22,7 @@ let chinaPopulation = percentageOfWorld1(1441);
 let roundedChinaPopulation = Number((chinaPopulation).toFixed(2));
 let chinaComparison = `China has 1441 million people, so it's about ${roundedChinaPopulation}% of the world population.`
 let kyrgyzstanPopulation = percentageOfWorld1(6.7);
+let roundedKyrgyzstanPopulation = Number((kyrgyzstanPopulation).toFixed(2));
 let kyrgyzstanComparison = `Kirgizstan has 6.7 million people, so it's about ${kyrgyzstanPopulation}% of the world population.`
 let russiaPopulation = percentageOfWorld1(145);
 let russiaComparison = `Russia has 145 million people, so it's about ${russiaPopulation}% of the world population.`
