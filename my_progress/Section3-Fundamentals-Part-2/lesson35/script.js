15c31afeb320377470087ed03144c10ae35777d5
Evgeniy_Utkin <eugene.utkin@gmail.com>
2022-08-01 20:58:03 +0600

    firstName parameter added to yearsUntilRetirement function

--- my_progress/Section3-Fundamentals-Part-2/lesson35/script.js
+++ my_progress/Section3-Fundamentals-Part-2/lesson35/script.js
@@ -57,7 +57,7 @@ const calcAge3 = birthYear => 2037 - birthYear;
 const age3 = calcAge3(1991);
 console.log(age3);
 
-const yearsUntilRetirement = (birthYear) => {
+const yearsUntilRetirement = (birthYear, firstName) => {
 	const age = 2037 - birthYear;
 	const retirement = 65 - age;
 	return retirement;
