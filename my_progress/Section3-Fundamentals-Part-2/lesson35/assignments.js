a0edecb69e0e8f52db28b6b0206e13769ab557ee
Evgeniy_Utkin <eugene.utkin@gmail.com>
2022-08-03 17:45:55 +0600

    const kyrgyzstanPopulation added

--- my_progress/Section3-Fundamentals-Part-2/lesson35/assignments.js
+++ my_progress/Section3-Fundamentals-Part-2/lesson35/assignments.js
@@ -66,3 +66,4 @@ const percentageOfWorld3 = population => population / 7900 * 100;
 let chinaPopulation = percentageOfWorld3(1441);
 let roundedChinaPopulation = Number((chinaPopulation).toFixed(2));
 let chinaComparison = `China has 1441 million people, so it's about ${roundedChinaPopulation}% of the world population.`
+let kyrgyzstanPopulation = percentageOfWorld3(6.7);
