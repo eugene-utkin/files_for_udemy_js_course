19f5c17dd79aca07934484bda902dd7df3446d59
Evgeniy_Utkin <eugene.utkin@gmail.com>
2022-08-06 18:52:21 +0600

    orangePieces used in juice

--- my_progress/Section3-Fundamentals-Part-2/lesson36/script.js
+++ my_progress/Section3-Fundamentals-Part-2/lesson36/script.js
@@ -77,7 +77,7 @@ function fruitProcessor(apples, oranges) {
 	const applePieces = cutFruitPieces(apples);
 	const orangePieces = cutFruitPieces(oranges);
 
-	const juice = `Juice with ${applePieces} pieces of apple and ${oranges} oranges.`;
+	const juice = `Juice with ${applePieces} pieces of apple and ${orangePieces} pieces of orange.`;
 	return juice;
 }
 fruitProcessor(2, 3);
