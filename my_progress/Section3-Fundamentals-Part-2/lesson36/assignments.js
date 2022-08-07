0b4e310650c3017d3888a899d593230c7af004fb
Evgeniy_Utkin <eugene.utkin@gmail.com>
2022-08-07 21:16:10 +0600

    roundedPercentage was used in description

--- my_progress/Section3-Fundamentals-Part-2/lesson36/assignments.js
+++ my_progress/Section3-Fundamentals-Part-2/lesson36/assignments.js
@@ -87,6 +87,6 @@ function percentageOfWorld1(population) {
 function describePopulation(country, population) {
 	const percentage = percentageOfWorld1(population);
 	const roundedPercentage = Number((percentage).toFixed(2));
-	const description = `${country} has ${population} million people, which is about 18.2% of the world.`;
+	const description = `${country} has ${population} million people, which is about ${roundedPercentage}% of the world.`;
 	return description;
 }
