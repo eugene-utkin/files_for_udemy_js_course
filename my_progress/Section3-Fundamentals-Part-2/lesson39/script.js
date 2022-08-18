0ad8577c4d5db2c9bc488e2ae0cad9f7f2f9e993
Evgeniy_Utkin <eugene.utkin@gmail.com>
2022-08-18 17:20:15 +0600

    const age3 added

--- my_progress/Section3-Fundamentals-Part-2/lesson39/script.js
+++ my_progress/Section3-Fundamentals-Part-2/lesson39/script.js
@@ -181,3 +181,4 @@ const calcAge = function (birthYear) {
 const years = [1990, 1967, 2002, 2010, 2018];
 const age1 = calcAge(years[0]);
 const age2 = calcAge(years[1]);
+const age3 = calcAge(years[years.length - 1]);
