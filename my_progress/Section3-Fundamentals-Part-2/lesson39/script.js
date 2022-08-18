9da5f4d1bd2de175c437ba9702c54ecdcebbc579
Evgeniy_Utkin <eugene.utkin@gmail.com>
2022-08-18 17:29:06 +0600

    console log ages added

--- my_progress/Section3-Fundamentals-Part-2/lesson39/script.js
+++ my_progress/Section3-Fundamentals-Part-2/lesson39/script.js
@@ -185,3 +185,4 @@ const age3 = calcAge(years[years.length - 1]);
 console.log(age1, age2, age3);
 
 const ages = [calcAge(years[0]), calcAge(years[1]), calcAge(years[years.length - 1])];
+console.log(ages);
