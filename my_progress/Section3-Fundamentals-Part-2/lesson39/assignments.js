23ed67544142bba406f12dfeaf2d9f0662e91897
Evgeniy_Utkin <eugene.utkin@gmail.com>
2022-08-19 18:05:28 +0600

    const percentage3 added

--- my_progress/Section3-Fundamentals-Part-2/lesson39/assignments.js
+++ my_progress/Section3-Fundamentals-Part-2/lesson39/assignments.js
@@ -123,3 +123,4 @@ function percentageOfWorld1(population) {
 
 const percentage1 = percentageOfWorld1(populations[0]);
 const percentage2 = percentageOfWorld1(populations[1]);
+const percentage3 = percentageOfWorld1(populations[2]);
