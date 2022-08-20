6c028aa31087a64da7e734ea9eaea1fba39b5199
Evgeniy_Utkin <eugene.utkin@gmail.com>
2022-08-20 17:17:26 +0600

    const friends added

--- my_progress/Section3-Fundamentals-Part-2/lesson40/script.js
+++ my_progress/Section3-Fundamentals-Part-2/lesson40/script.js
@@ -188,3 +188,5 @@ console.log(age1, age2, age3);
 const ages = [calcAge(years[0]), calcAge(years[1]), calcAge(years[years.length - 1])];
 console.log(ages);
 */
+
+const friends = ['Michael', 'Steven', 'Peter'];
