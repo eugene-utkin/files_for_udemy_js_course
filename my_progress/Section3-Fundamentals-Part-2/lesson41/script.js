f8fad89334eb40ceeb922e20b9d39afce10d2970
Evgeniy_Utkin <eugene.utkin@gmail.com>
2022-08-25 17:06:02 +0600

    const jonasArray added

--- my_progress/Section3-Fundamentals-Part-2/lesson41/script.js
+++ my_progress/Section3-Fundamentals-Part-2/lesson41/script.js
@@ -237,3 +237,11 @@ const total = [bills[0] + tips[0], bills[1] + tips[1], bills[2] + tips[2]];
 
 console.log(total);
 */
+
+const jonasArray = [
+	'Jonas',
+	'Schmedtmann',
+	2037 - 1991,
+	'teacher',
+	['Michael', 'Peter', 'Steven']
+]
