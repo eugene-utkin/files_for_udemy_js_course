902a6bdd19979896fad13e4fbdb8bff6714e7b66
Evgeniy_Utkin <eugene.utkin@gmail.com>
2022-08-28 17:53:40 +0600

    const interestedIn added

--- my_progress/Section3-Fundamentals-Part-2/lesson43/script.js
+++ my_progress/Section3-Fundamentals-Part-2/lesson43/script.js
@@ -273,4 +273,4 @@ console.log(jonas['last' + nameKey]);
 
 // console.log(jonas.'last' + nameKey);
 
-prompt('What do you want to know about Jonas? Choose between firstName, lastName, age, job and friends');
+const interestedIn = prompt('What do you want to know about Jonas? Choose between firstName, lastName, age, job and friends');
