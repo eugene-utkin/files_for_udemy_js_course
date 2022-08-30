78bcf2c9bee69d4bd390138249a1598e8d22d7ae
Evgeniy_Utkin <eugene.utkin@gmail.com>
2022-08-30 18:25:30 +0600

    const jonas added

--- my_progress/Section3-Fundamentals-Part-2/lesson44/script.js
+++ my_progress/Section3-Fundamentals-Part-2/lesson44/script.js
@@ -290,3 +290,11 @@ console.log(jonas);
 // "Jonas has 3 friends, and his best friend is called Michael"
 console.log(`${jonas.firstName} has ${jonas.friends.length} friends, and his best friend is called ${jonas.friends[0]}`);
 */
+
+const jonas = {
+	firstName: 'Jonas',
+	lastName: 'Schmedtmann',
+	age: 2037 - 1991,
+	job: 'teacher',
+	friends: ['Michael', 'Peter', 'Steven']
+};
