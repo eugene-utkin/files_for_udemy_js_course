aab34dd19e8e55c654d83e910d619c0eb979f96e
Evgeniy_Utkin <eugene.utkin@gmail.com>
2022-08-30 18:11:04 +0600

    console log myCountry.population

--- my_progress/Section3-Fundamentals-Part-2/lesson43/assignments.js
+++ my_progress/Section3-Fundamentals-Part-2/lesson43/assignments.js
@@ -156,3 +156,4 @@ const myCountry = {
 console.log(`${myCountry.country} has ${myCountry.population} million ${myCountry.language}-speaking people, ${myCountry.neighbours.length} neighbouring countries and a capital called ${myCountry.capital}.`);
 
 myCountry.population = myCountry.population + 2;
+console.log(myCountry.population);
