20e0f3006dd4e055b11b2b8cf8d94627beb119bf
Evgeniy_Utkin <eugene.utkin@gmail.com>
2022-08-30 18:16:37 +0600

    done with += an d -=

--- my_progress/Section3-Fundamentals-Part-2/lesson43/assignments.js
+++ my_progress/Section3-Fundamentals-Part-2/lesson43/assignments.js
@@ -155,7 +155,7 @@ const myCountry = {
 
 console.log(`${myCountry.country} has ${myCountry.population} million ${myCountry.language}-speaking people, ${myCountry.neighbours.length} neighbouring countries and a capital called ${myCountry.capital}.`);
 
-myCountry.population = myCountry.population + 2;
+myCountry.population += 2;
 console.log(myCountry.population);
-myCountry['population'] = myCountry['population'] - 2;
+myCountry['population'] -= 2;
 console.log(myCountry['population']);
