38bce840f39d63ecefcce0d04fe8db4634f4152c
Evgeniy_Utkin <eugene.utkin@gmail.com>
2022-09-02 18:23:24 +0600

    const licenseStatus removed

--- my_progress/Section3-Fundamentals-Part-2/lesson44/script.js
+++ my_progress/Section3-Fundamentals-Part-2/lesson44/script.js
@@ -323,5 +323,4 @@ console.log(jonas.age);
 // Challenge
 // "Jonas is a 46-year old teacher, and he has a/no driver's license"
 
-const licenseStatus = jonas.hasDriversLicense ? 'a' : 'no';
 console.log(`${jonas.firstName} is a ${jonas.age}-year old ${jonas.job}, and he has ${jonas.hasDriversLicense ? 'a' : 'no'} driver's license`);
