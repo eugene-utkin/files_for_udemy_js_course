512e07ff5e7f0c21a5af8bae9d8d90594b032f05
Evgeniy_Utkin <eugene.utkin@gmail.com>
2022-09-04 17:43:20 +0600

    checkIsland method added to myCountry object

--- my_progress/Section3-Fundamentals-Part-2/lesson44/assignments.js
+++ my_progress/Section3-Fundamentals-Part-2/lesson44/assignments.js
@@ -170,6 +170,9 @@ const myCountry = {
 	neighbours: ['Finland', 'Georgia'],
 	describe: function () {
 		console.log(`${this.country} has ${this.population} million ${this.language}-speaking people, ${this.neighbours.length} neighbouring countries and a capital called ${this.capital}.`);
+	},
+	checkIsland: function () {
+
 	}
 }
 
