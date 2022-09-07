ec3ca350c4bd02194c8baca1a55222c4fa3e5df2
Evgeniy_Utkin <eugene.utkin@gmail.com>
2022-09-07 19:58:51 +0600

    typo fixed

--- my_progress/Section3-Fundamentals-Part-2/lesson45/script.js
+++ my_progress/Section3-Fundamentals-Part-2/lesson45/script.js
@@ -355,7 +355,7 @@ mark.calcBMI();
 john.calcBMI();
 
 if (mark.BMI > john.BMI) {
-	console.log(`Mark's BMI (${mark.BMI}) is higher than John's (${john.MBI})!`);
+	console.log(`Mark's BMI (${mark.BMI}) is higher than John's (${john.BMI})!`);
 } else if (john.BMI > mark.BMI) {
 	console.log(`John's BMI (${john.BMI}) is higher than Mark's (${mark.MBI})!`);
 } else {
