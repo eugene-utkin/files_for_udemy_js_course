65314c2b853189221ad981cb9f7b24e088b1c262
Evgeniy_Utkin <eugene.utkin@gmail.com>
2022-09-09 19:24:53 +0600

    comment added

--- my_progress/Section3-Fundamentals-Part-2/lesson46/script.js
+++ my_progress/Section3-Fundamentals-Part-2/lesson46/script.js
@@ -375,4 +375,5 @@ console.log('Lifting weights repetition 8 🏋️');
 console.log('Lifting weights repetition 9 🏋️');
 console.log('Lifting weights repetition 10 🏋️');
 
+// for loop keeps running while condition is TRUE
 for (let rep = 1; rep <= 10)
