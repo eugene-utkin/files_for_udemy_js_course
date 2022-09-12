1b49827b73a668cba525dce313ef0652d8326f63
Evgeniy_Utkin <eugene.utkin@gmail.com>
2022-09-12 16:53:44 +0600

    second part of for statement added

--- my_progress/Section3-Fundamentals-Part-2/lesson47/script.js
+++ my_progress/Section3-Fundamentals-Part-2/lesson47/script.js
@@ -387,10 +387,9 @@ const jonas = [
 	'Schmedtmann',
 	2037 - 1991,
 	'teacher',
-	['Michael', 'Peter', 'Steven'],
-	true
+	['Michael', 'Peter', 'Steven']
 ];
 
-for (let i = 0; i < 5; i++) {
+for (let i = 0; i < jonas.length; i++) {
 	console.log(jonas[i]);
 }
