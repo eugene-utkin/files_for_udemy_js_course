76ab530b83ca60be0591ab7f6e3074219bc251bf
Evgeniy_Utkin <eugene.utkin@gmail.com>
2022-09-14 16:47:04 +0600

    break example

--- my_progress/Section3-Fundamentals-Part-2/lesson47/script.js
+++ my_progress/Section3-Fundamentals-Part-2/lesson47/script.js
@@ -426,3 +426,8 @@ for (let i = 0; i < jonas.length; i++) {
 }
 
 console.log('--- BREAK WITH NUMBER ---');
+for (let i = 0; i < jonas.length; i++) {
+	if (typeof jonas[i] === 'number') break;
+
+	console.log(jonas[i], typeof jonas[i]);
+}
