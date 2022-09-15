8a554baaf2e836473d4285dc69b6affa5f9c3097
Evgeniy_Utkin <eugene.utkin@gmail.com>
2022-09-15 17:02:46 +0600

    jonas array added

--- my_progress/Section3-Fundamentals-Part-2/lesson48/script.js
+++ my_progress/Section3-Fundamentals-Part-2/lesson48/script.js
@@ -433,3 +433,12 @@ for (let i = 0; i < jonas.length; i++) {
 	console.log(jonas[i], typeof jonas[i]);
 }
 */
+
+const jonas = [
+	'Jonas',
+	'Schmedtmann',
+	2037 - 1991,
+	'teacher',
+	['Michael', 'Peter', 'Steven'],
+	true
+];
