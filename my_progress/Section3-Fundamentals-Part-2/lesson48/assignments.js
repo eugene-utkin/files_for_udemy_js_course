4e1f0ff27cd3b9242dedc00dfc054d2f94046ef3
Evgeniy_Utkin <eugene.utkin@gmail.com>
2022-09-18 17:13:39 +0600

    empty console log added

--- my_progress/Section3-Fundamentals-Part-2/lesson48/assignments.js
+++ my_progress/Section3-Fundamentals-Part-2/lesson48/assignments.js
@@ -206,5 +206,5 @@ for (let i = 0; i < populations.length; i++) {
 const listOfNeighbours = [['Canada', 'Mexico'], ['Spain'], ['Norway', 'Sweden', 'Russia']];
 
 for (let i = 0; i < listOfNeighbours.length; i++) {
-
+	console.log('');
 }
