882643cb98e2e445d666e68c67b83afe2f9a87b1
Evgeniy_Utkin <eugene.utkin@gmail.com>
2022-09-21 20:32:58 +0600

    old code commented out

--- my_progress/Section3-Fundamentals-Part-2/lesson50/script.js
+++ my_progress/Section3-Fundamentals-Part-2/lesson50/script.js
@@ -463,6 +463,7 @@ for (let exercise = 1; exercise < 4; exercise++) {
 // 	console.log(`Lifting weights repetition ${rep} 🏋️`);
 // }
 
+/*
 let rep = 1;
 while (rep <= 10) {
 	// console.log(`WHILE: Lifting weights repetition ${rep} 🏋️`);
@@ -478,3 +479,4 @@ while (dice !== 6) {
 	dice = Math.trunc(Math.random() * 6) + 1;
 	if (dice === 6) console.log('Loop is about to end...');
 }
+*/
