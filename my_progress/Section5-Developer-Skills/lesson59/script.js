bdefacd3b97b01bafb10cfee78c855519614b6ba
Evgeniy_Utkin <eugene.utkin@gmail.com>
2022-09-26 19:12:58 +0600

    compute qestion added

--- my_progress/Section5-Developer-Skills/lesson59/script.js
+++ my_progress/Section5-Developer-Skills/lesson59/script.js
@@ -12,5 +12,6 @@ const temperatures = [3, -2, -6, -1, 'error', 9, 13, 17, 15, 14, 9, 5];
 
 // 1) Understanding the problem
 // - What is temp amplitude? Answer: difference between highest and lowest temp
+// - How to compute max and min temperatures?
 
 // 2) Breaking up into sub-problems
