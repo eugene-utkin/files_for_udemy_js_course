196f4e1b1b6d80050ec5f3f01e483eaed079ef45
Evgeniy_Utkin <eugene.utkin@gmail.com>
2022-09-27 17:35:27 +0600

    min value comment added

--- my_progress/Section5-Developer-Skills/lesson59/script.js
+++ my_progress/Section5-Developer-Skills/lesson59/script.js
@@ -18,3 +18,4 @@ const temperatures = [3, -2, -6, -1, 'error', 9, 13, 17, 15, 14, 9, 5];
 // 2) Breaking up into sub-problems
 // - How to ignore errors?
 // - Find max value in temp array
+// - Find min value in temp array
