c14f2a0646a38d9367fb5bc67df596a0e33cc2b4
Evgeniy_Utkin <eugene.utkin@gmail.com>
2022-09-27 17:33:09 +0600

    ignore errors question added

--- my_progress/Section5-Developer-Skills/lesson59/script.js
+++ my_progress/Section5-Developer-Skills/lesson59/script.js
@@ -16,3 +16,4 @@ const temperatures = [3, -2, -6, -1, 'error', 9, 13, 17, 15, 14, 9, 5];
 // - What's a sensor error? And what to do?
 
 // 2) Breaking up into sub-problems
+// - How to ignore errors?
