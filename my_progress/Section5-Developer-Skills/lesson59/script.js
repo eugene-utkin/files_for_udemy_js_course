68e5deb6a6efc41773366c660c749fb99c0b8e2b
Evgeniy_Utkin <eugene.utkin@gmail.com>
2022-09-28 19:20:39 +0600

    min variable added

--- my_progress/Section5-Developer-Skills/lesson59/script.js
+++ my_progress/Section5-Developer-Skills/lesson59/script.js
@@ -23,6 +23,8 @@ const temperatures = [3, -2, -6, -1, 'error', 9, 13, 17, 15, 14, 9, 5];
 
 const calcTempAmplitude = function (temps) {
 	let max = temps[0];
+	let mix = temps[0];
+	
 	for(let i = 0; i < temps.length; i++) {
 		if(temps[i] > max) max = temps[i];
 	}
