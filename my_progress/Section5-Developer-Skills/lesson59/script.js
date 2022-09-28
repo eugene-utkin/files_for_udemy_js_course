1f99c678ee32b92b1da091f4b0d9e3c5e6db7cda
Evgeniy_Utkin <eugene.utkin@gmail.com>
2022-09-28 21:41:17 +0600

    prettier fixed

--- my_progress/Section5-Developer-Skills/lesson59/script.js
+++ my_progress/Section5-Developer-Skills/lesson59/script.js
@@ -22,16 +22,16 @@ const temperatures = [3, -2, -6, -1, 'error', 9, 13, 17, 15, 14, 9, 5];
 // - Subtract min from max (amplitude) and return it
 
 const calcTempAmplitude = function (temps) {
-	let max = temps[0];
-	let mix = temps[0];
+  let max = temps[0];
+  let mix = temps[0];
 
-	for(let i = 0; i < temps.length; i++) {
-		const curTemp = temps[i];
-		
-		if(curTemp > max) max = curTemp;
-		if(curTemp < min) min = curTemp;
-	}
-	console.log(max, min);
+  for (let i = 0; i < temps.length; i++) {
+    const curTemp = temps[i];
+
+    if (curTemp > max) max = curTemp;
+    if (curTemp < min) min = curTemp;
+  }
+  console.log(max, min);
 };
 
 calcTempAmplitude([3, 7, 4]);
