377d17a8ad15272b4d0095c0fa2d47768492bcfe
Evgeniy_Utkin <eugene.utkin@gmail.com>
2022-09-29 17:01:36 +0600

    typo fixed

--- my_progress/Section5-Developer-Skills/lesson59/script.js
+++ my_progress/Section5-Developer-Skills/lesson59/script.js
@@ -23,7 +23,7 @@ const temperatures = [3, -2, -6, -1, 'error', 9, 13, 17, 15, 14, 9, 5];
 
 const calcTempAmplitude = function (temps) {
   let max = temps[0];
-  let mix = temps[0];
+  let min = temps[0];
 
   for (let i = 0; i < temps.length; i++) {
     const curTemp = temps[i];
