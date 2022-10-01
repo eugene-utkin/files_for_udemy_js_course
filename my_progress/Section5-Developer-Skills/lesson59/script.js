b18b2a692695601ae3c43bcfecbae4aabcfcc8f8
Evgeniy_Utkin <eugene.utkin@gmail.com>
2022-10-01 12:21:45 +0600

    const amplitudeNew added

--- my_progress/Section5-Developer-Skills/lesson59/script.js
+++ my_progress/Section5-Developer-Skills/lesson59/script.js
@@ -62,5 +62,5 @@ const calcTempAmplitudeNew = function (temps) {
   console.log(max, min);
   return max - min;
 };
-const amplitude = calcTempAmplitudeNew(temperatures);
-console.log(amplitude);
+const amplitudeNew = calcTempAmplitudeNew(temperatures);
+console.log(amplitudeNew);
