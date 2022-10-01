18055e8a2155ada87397ad00d1b818ce7887a585
Evgeniy_Utkin <eugene.utkin@gmail.com>
2022-10-01 12:20:06 +0600

    old code used again

--- my_progress/Section5-Developer-Skills/lesson59/script.js
+++ my_progress/Section5-Developer-Skills/lesson59/script.js
@@ -47,3 +47,20 @@ console.log(amplitude);
 
 // 2) Breaking up into sub-problems
 // - Merge 2 arrays
+
+const calcTempAmplitude = function (temps) {
+  let max = temps[0];
+  let min = temps[0];
+
+  for (let i = 0; i < temps.length; i++) {
+    const curTemp = temps[i];
+    if (typeof curTemp !== 'number') continue;
+
+    if (curTemp > max) max = curTemp;
+    if (curTemp < min) min = curTemp;
+  }
+  console.log(max, min);
+  return max - min;
+};
+const amplitude = calcTempAmplitude(temperatures);
+console.log(amplitude);
