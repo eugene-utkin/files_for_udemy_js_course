1895214b38b92ad36cc5fce9725bdca61c323ab3
Evgeniy_Utkin <eugene.utkin@gmail.com>
2022-10-04 14:29:48 +0600

    temps[0] returned

--- my_progress/Section5-Developer-Skills/lesson61/script.js
+++ my_progress/Section5-Developer-Skills/lesson61/script.js
@@ -98,8 +98,8 @@ const calcTempAmplitudeBug = function (t1, t2) {
   const temps = t1.concat(t2);
   console.log(temps);
 
-  let max = 0;
-  let min = 0;
+  let max = temps[0];
+  let min = temps[0];
 
   for (let i = 0; i < temps.length; i++) {
     const curTemp = temps[i];
@@ -112,5 +112,4 @@ const calcTempAmplitudeBug = function (t1, t2) {
   return max - min;
 };
 const amplitudeBug = calcTempAmplitudeBug([3, 5, 1], [9, 4, 5]);
-// A) IDENTIFY
 console.log(amplitudeBug);
