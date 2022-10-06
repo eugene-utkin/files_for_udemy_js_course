29bb33957253ed825a05500512a0aa0a961c492b
Evgeniy_Utkin <eugene.utkin@gmail.com>
2022-10-06 16:22:32 +0600

    const data2 added

--- my_progress/Section5-Developer-Skills/lesson62/script.js
+++ my_progress/Section5-Developer-Skills/lesson62/script.js
@@ -117,6 +117,7 @@ console.log(amplitudeBug);
 */
 
 const data1 = [17, 21, 23];
+const data2 = [12, 5, -5, 0, 4];
 function printForecast(arr) {
   let forecast = '...';
   for (let i = 0; i < arr.length; i++) {
