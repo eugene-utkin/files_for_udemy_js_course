eafe9429819e8d550e0bc805ae4b5a368c3fd154
Evgeniy_Utkin <eugene.utkin@gmail.com>
2022-10-13 15:30:38 +0600

    console log added again

--- my_progress/Section7-Guess-My-Number/lesson72/script.js
+++ my_progress/Section7-Guess-My-Number/lesson72/script.js
@@ -2,3 +2,4 @@
 
 console.log(document.querySelector('.message').textContent);
 document.querySelector('.message').textContent = '🎉 Correct Number!';
+console.log(document.querySelector('.message').textContent);
