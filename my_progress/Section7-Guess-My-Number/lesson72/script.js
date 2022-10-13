8f7adddd45a9a0027e22cc50eeafdd08586100ef
Evgeniy_Utkin <eugene.utkin@gmail.com>
2022-10-13 18:27:44 +0600

    lines switched

--- my_progress/Section7-Guess-My-Number/lesson72/script.js
+++ my_progress/Section7-Guess-My-Number/lesson72/script.js
@@ -5,5 +5,5 @@ document.querySelector('.message').textContent = '🎉 Correct Number!';
 document.querySelector('.number').textContent = 13;
 document.querySelector('.score').textContent = 10;
 
-console.log(document.querySelector('.guess').value);
 document.querySelector('.guess').value = 23;
+console.log(document.querySelector('.guess').value);
