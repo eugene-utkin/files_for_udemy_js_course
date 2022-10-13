ad065eac6ebfd370202ab03402eb09f0d79ed835
Evgeniy_Utkin <eugene.utkin@gmail.com>
2022-10-13 15:34:22 +0600

    setting the number

--- my_progress/Section7-Guess-My-Number/lesson72/script.js
+++ my_progress/Section7-Guess-My-Number/lesson72/script.js
@@ -2,4 +2,4 @@
 
 console.log(document.querySelector('.message').textContent);
 document.querySelector('.message').textContent = '🎉 Correct Number!';
-console.log(document.querySelector('.message').textContent);
+document.querySelector('.number') = 13;
