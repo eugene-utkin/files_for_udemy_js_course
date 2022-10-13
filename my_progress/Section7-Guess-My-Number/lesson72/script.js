f76c59f7c6dcc878a7e8c1e0c91a88ea4f920fdf
Evgeniy_Utkin <eugene.utkin@gmail.com>
2022-10-13 15:44:49 +0600

    console log guess value

--- my_progress/Section7-Guess-My-Number/lesson72/script.js
+++ my_progress/Section7-Guess-My-Number/lesson72/script.js
@@ -4,3 +4,5 @@ console.log(document.querySelector('.message').textContent);
 document.querySelector('.message').textContent = '🎉 Correct Number!';
 document.querySelector('.number').textContent = 13;
 document.querySelector('.score').textContent = 10;
+
+console.log(document.querySelector('.guess').value);
