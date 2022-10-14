28bc6272bcc6ae443c08eaba25e28f60717105cb
Evgeniy_Utkin <eugene.utkin@gmail.com>
2022-10-14 19:05:43 +0600

    old code commented out

--- my_progress/Section7-Guess-My-Number/lesson73/script.js
+++ my_progress/Section7-Guess-My-Number/lesson73/script.js
@@ -1,5 +1,6 @@
 'use strict';
 
+/*
 console.log(document.querySelector('.message').textContent);
 document.querySelector('.message').textContent = '🎉 Correct Number!';
 document.querySelector('.number').textContent = 13;
@@ -7,3 +8,4 @@ document.querySelector('.score').textContent = 10;
 
 document.querySelector('.guess').value = 23;
 console.log(document.querySelector('.guess').value);
+*/
