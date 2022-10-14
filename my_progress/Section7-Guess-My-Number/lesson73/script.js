5032637660437606a7e4f510f3808b6ed29e64cd
Evgeniy_Utkin <eugene.utkin@gmail.com>
2022-10-14 19:07:31 +0600

    querySelector .check added

--- my_progress/Section7-Guess-My-Number/lesson73/script.js
+++ my_progress/Section7-Guess-My-Number/lesson73/script.js
@@ -9,3 +9,5 @@ document.querySelector('.score').textContent = 10;
 document.querySelector('.guess').value = 23;
 console.log(document.querySelector('.guess').value);
 */
+
+document.querySelector('.check');
