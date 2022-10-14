9a45a98267e759c82d8fb44bbe3e950b51038b6c
Evgeniy_Utkin <eugene.utkin@gmail.com>
2022-10-14 19:12:47 +0600

    console log added to function

--- my_progress/Section7-Guess-My-Number/lesson73/script.js
+++ my_progress/Section7-Guess-My-Number/lesson73/script.js
@@ -10,4 +10,6 @@ document.querySelector('.guess').value = 23;
 console.log(document.querySelector('.guess').value);
 */
 
-document.querySelector('.check').addEventListener('click', function () {});
+document.querySelector('.check').addEventListener('click', function () {
+  console.log(document.querySelector('.guess').value);
+});
