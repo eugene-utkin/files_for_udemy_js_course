18956ddcc8bbcab8a05c0b6c0e12d2758d19057b
Evgeniy_Utkin <eugene.utkin@gmail.com>
2022-10-15 13:45:08 +0600

    ? replaced with number

--- my_progress/Section7-Guess-My-Number/lesson74/script.js
+++ my_progress/Section7-Guess-My-Number/lesson74/script.js
@@ -11,6 +11,7 @@ console.log(document.querySelector('.guess').value);
 */
 
 const number = Math.trunc(Math.random() * 20) + 1;
+document.querySelector('.number').textContent = number;
 
 document.querySelector('.check').addEventListener('click', function () {
   const guess = Number(document.querySelector('.guess').value);
