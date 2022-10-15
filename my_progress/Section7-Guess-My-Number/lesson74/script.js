6177223b5d447f6165a87668a61a814ed3c8283e
Evgeniy_Utkin <eugene.utkin@gmail.com>
2022-10-15 14:06:27 +0600

    score-- added to too high

--- my_progress/Section7-Guess-My-Number/lesson74/script.js
+++ my_progress/Section7-Guess-My-Number/lesson74/script.js
@@ -24,6 +24,7 @@ document.querySelector('.check').addEventListener('click', function () {
     document.querySelector('.message').textContent = '🎉 Correct Number!';
   } else if (guess > secretNumber) {
     document.querySelector('.message').textContent = '📈 Too high!';
+    score--;
   } else if (guess < secretNumber) {
     document.querySelector('.message').textContent = '📉 Too low!';
   }
