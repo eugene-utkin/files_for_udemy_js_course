05df109fcef0cd21aa54aedf25725b90b5c79c6a
Evgeniy_Utkin <eugene.utkin@gmail.com>
2022-10-15 13:38:09 +0600

    const x removed

--- my_progress/Section7-Guess-My-Number/lesson74/script.js
+++ my_progress/Section7-Guess-My-Number/lesson74/script.js
@@ -10,10 +10,6 @@ document.querySelector('.guess').value = 23;
 console.log(document.querySelector('.guess').value);
 */
 
-const x = function () {
-  console.log(23);
-};
-
 document.querySelector('.check').addEventListener('click', function () {
   const guess = Number(document.querySelector('.guess').value);
   console.log(guess, typeof guess);
