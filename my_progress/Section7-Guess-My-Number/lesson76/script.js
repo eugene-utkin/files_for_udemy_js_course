10772ebbd835f25c7cd15e5074b04ad2ee928551
Evgeniy_Utkin <eugene.utkin@gmail.com>
2022-10-16 09:44:08 +0600

    secretNumber value reset

--- my_progress/Section7-Guess-My-Number/lesson76/script.js
+++ my_progress/Section7-Guess-My-Number/lesson76/script.js
@@ -55,4 +55,5 @@ document.querySelector('.check').addEventListener('click', function () {
 
 document.querySelector('.again').addEventListener('click', function () {
   score = 20;
+  secretNumber = Math.trunc(Math.random() * 20) + 1;
 });
