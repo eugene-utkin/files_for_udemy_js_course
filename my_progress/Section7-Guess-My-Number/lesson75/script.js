2e0fe87e522d5be94d1d470f05219eb0983eb2f2
Evgeniy_Utkin <eugene.utkin@gmail.com>
2022-10-16 09:22:20 +0600

    no input comment added

--- my_progress/Section7-Guess-My-Number/lesson75/script.js
+++ my_progress/Section7-Guess-My-Number/lesson75/script.js
@@ -18,6 +18,7 @@ document.querySelector('.check').addEventListener('click', function () {
   const guess = Number(document.querySelector('.guess').value);
   console.log(guess, typeof guess);
 
+  // When there is no input
   if (!guess) {
     document.querySelector('.message').textContent = '⛔ No number!';
   } else if (guess === secretNumber) {
