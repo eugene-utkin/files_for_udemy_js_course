4b6d12012c1366d67b7c652bdb1a7a5331f198c3
Evgeniy_Utkin <eugene.utkin@gmail.com>
2022-10-16 09:39:20 +0600

    empty line removed

--- my_progress/Section7-Guess-My-Number/lesson75/script.js
+++ my_progress/Section7-Guess-My-Number/lesson75/script.js
@@ -27,7 +27,6 @@ document.querySelector('.check').addEventListener('click', function () {
     document.querySelector('.message').textContent = '🎉 Correct Number!';
 
     document.querySelector('body').style.backgroundColor = '#60b347';
-
     document.querySelector('.number').style.width = '30rem';
 
     // When guess is too high
