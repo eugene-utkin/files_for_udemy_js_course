3307edfca7d9430ae00446695e63b8c537aa7c6e
Evgeniy_Utkin <eugene.utkin@gmail.com>
2022-10-16 09:33:00 +0600

    element's width changed through js

--- my_progress/Section7-Guess-My-Number/lesson75/script.js
+++ my_progress/Section7-Guess-My-Number/lesson75/script.js
@@ -28,6 +28,8 @@ document.querySelector('.check').addEventListener('click', function () {
 
     document.querySelector('body').style.backgroundColor = '#60b347';
 
+    document.querySelector('.number').style.width = '30rem';
+
     // When guess is too high
   } else if (guess > secretNumber) {
     if (score > 1) {
