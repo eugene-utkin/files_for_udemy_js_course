6bd135d66f238d70fa880c610a3006c9874a44a6
Evgeniy_Utkin <eugene.utkin@gmail.com>
2022-10-17 18:08:48 +0600

    score and highscore comparison added

--- my_progress/Section7-Guess-My-Number/lesson77/script.js
+++ my_progress/Section7-Guess-My-Number/lesson77/script.js
@@ -30,6 +30,10 @@ document.querySelector('.check').addEventListener('click', function () {
     document.querySelector('.number').style.width = '30rem';
     document.querySelector('.number').textContent = secretNumber;
 
+    if (score > highscore) {
+      highscore = score;
+    }
+
     // When guess is too high
   } else if (guess > secretNumber) {
     if (score > 1) {
