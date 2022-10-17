4dc4d3d1184519433dd83e1b6ebf1232c19be76c
Evgeniy_Utkin <eugene.utkin@gmail.com>
2022-10-17 17:12:50 +0600

    Start message restored

--- my_progress/Section7-Guess-My-Number/lesson76/script.js
+++ my_progress/Section7-Guess-My-Number/lesson76/script.js
@@ -56,4 +56,5 @@ document.querySelector('.check').addEventListener('click', function () {
 document.querySelector('.again').addEventListener('click', function () {
   score = 20;
   secretNumber = Math.trunc(Math.random() * 20) + 1;
+  document.querySelector('.message').textContent = 'Start guessing...';
 });
