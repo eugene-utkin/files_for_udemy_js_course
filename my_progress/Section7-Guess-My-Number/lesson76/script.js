651f1212e3f5c39d2e9c84d1042e27d22c7f6bdf
Evgeniy_Utkin <eugene.utkin@gmail.com>
2022-10-17 17:28:45 +0600

    secret number moved

--- my_progress/Section7-Guess-My-Number/lesson76/script.js
+++ my_progress/Section7-Guess-My-Number/lesson76/script.js
@@ -12,7 +12,6 @@ console.log(document.querySelector('.guess').value);
 
 let secretNumber = Math.trunc(Math.random() * 20) + 1;
 let score = 20;
-document.querySelector('.number').textContent = secretNumber;
 
 document.querySelector('.check').addEventListener('click', function () {
   const guess = Number(document.querySelector('.guess').value);
@@ -28,6 +27,7 @@ document.querySelector('.check').addEventListener('click', function () {
 
     document.querySelector('body').style.backgroundColor = '#60b347';
     document.querySelector('.number').style.width = '30rem';
+    document.querySelector('.number').textContent = secretNumber;
 
     // When guess is too high
   } else if (guess > secretNumber) {
@@ -57,6 +57,7 @@ document.querySelector('.again').addEventListener('click', function () {
   score = 20;
   secretNumber = Math.trunc(Math.random() * 20) + 1;
   document.querySelector('.score').textContent = score;
+  document.querySelector('.number').textContent = secretNumber;
   document.querySelector('.message').textContent = 'Start guessing...';
   document.querySelector('.number').style.width = '15rem';
   document.querySelector('body').style.backgroundColor = '#222';
