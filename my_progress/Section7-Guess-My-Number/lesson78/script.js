bf79b4c2ba7e94449f6056e61195984dd1485bef
Evgeniy_Utkin <eugene.utkin@gmail.com>
2022-10-19 16:54:21 +0600

    displayMessage function called third time

--- my_progress/Section7-Guess-My-Number/lesson78/script.js
+++ my_progress/Section7-Guess-My-Number/lesson78/script.js
@@ -44,8 +44,9 @@ document.querySelector('.check').addEventListener('click', function () {
     // When guess is wrong
   } else if (guess !== secretNumber) {
     if (score > 1) {
-      document.querySelector('.message').textContent =
-        guess > secretNumber ? '📈 Too high!' : '📉 Too low!';
+      // document.querySelector('.message').textContent =
+      //   guess > secretNumber ? '📈 Too high!' : '📉 Too low!';
+      displayMessage(guess > secretNumber ? '📈 Too high!' : '📉 Too low!');
       score--;
       document.querySelector('.score').textContent = score;
     } else {
