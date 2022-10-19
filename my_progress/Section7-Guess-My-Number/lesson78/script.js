eefd4e422721892a72f358bfc65aef86e7104b1e
Evgeniy_Utkin <eugene.utkin@gmail.com>
2022-10-19 16:43:07 +0600

    const displayMessage added

--- my_progress/Section7-Guess-My-Number/lesson78/script.js
+++ my_progress/Section7-Guess-My-Number/lesson78/script.js
@@ -14,6 +14,8 @@ let secretNumber = Math.trunc(Math.random() * 20) + 1;
 let score = 20;
 let highscore = 0;
 
+const displayMessage = function (message) {};
+
 document.querySelector('.check').addEventListener('click', function () {
   const guess = Number(document.querySelector('.guess').value);
   console.log(guess, typeof guess);
