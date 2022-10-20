6fe6b7ca07c477e092905019dc5df7770fc5cb79
Evgeniy_Utkin <eugene.utkin@gmail.com>
2022-10-20 21:54:17 +0600

    const const btnsOpenModal added

--- my_progress/Section7-DOM-and-Events-Fundamentals/lesson79/script.js
+++ my_progress/Section7-DOM-and-Events-Fundamentals/lesson79/script.js
@@ -3,3 +3,4 @@
 const modal = document.querySelector('.modal');
 const overlay = document.querySelector('.overlay');
 const btnCloseModal = document.querySelector('.close-modal');
+const btnsOpenModal = document.querySelector('.open-modal');
