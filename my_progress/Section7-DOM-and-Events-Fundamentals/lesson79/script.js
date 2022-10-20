7397c95510561534457cb6fd6f7873fe9d9a0970
Evgeniy_Utkin <eugene.utkin@gmail.com>
2022-10-20 21:56:44 +0600

    typo fixed

--- my_progress/Section7-DOM-and-Events-Fundamentals/lesson79/script.js
+++ my_progress/Section7-DOM-and-Events-Fundamentals/lesson79/script.js
@@ -3,5 +3,5 @@
 const modal = document.querySelector('.modal');
 const overlay = document.querySelector('.overlay');
 const btnCloseModal = document.querySelector('.close-modal');
-const btnsOpenModal = document.querySelector('.open-modal');
+const btnsOpenModal = document.querySelector('.show-modal');
 console.log(btnsOpenModal);
