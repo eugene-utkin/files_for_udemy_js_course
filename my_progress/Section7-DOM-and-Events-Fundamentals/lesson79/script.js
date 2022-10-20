3902e2d0ec682fa7daca13968b629be4f89eb6b6
Evgeniy_Utkin <eugene.utkin@gmail.com>
2022-10-20 22:03:05 +0600

    querySelectorAll used

--- my_progress/Section7-DOM-and-Events-Fundamentals/lesson79/script.js
+++ my_progress/Section7-DOM-and-Events-Fundamentals/lesson79/script.js
@@ -3,5 +3,5 @@
 const modal = document.querySelector('.modal');
 const overlay = document.querySelector('.overlay');
 const btnCloseModal = document.querySelector('.close-modal');
-const btnsOpenModal = document.querySelector('.show-modal');
+const btnsOpenModal = document.querySelectorAll('.show-modal');
 console.log(btnsOpenModal);
