927769778f96578d967efe4529a8d76e9826e01c
Evgeniy_Utkin <eugene.utkin@gmail.com>
2022-10-20 21:54:55 +0600

    test console log added

--- my_progress/Section7-DOM-and-Events-Fundamentals/lesson79/script.js
+++ my_progress/Section7-DOM-and-Events-Fundamentals/lesson79/script.js
@@ -4,3 +4,4 @@ const modal = document.querySelector('.modal');
 const overlay = document.querySelector('.overlay');
 const btnCloseModal = document.querySelector('.close-modal');
 const btnsOpenModal = document.querySelector('.open-modal');
+console.log(btnsOpenModal);
