8752059647a12b9e19d60ac0d4f5f1f78686d60f
Evgeniy_Utkin <eugene.utkin@gmail.com>
2022-10-20 22:04:10 +0600

    first part of for statement added

--- my_progress/Section7-DOM-and-Events-Fundamentals/lesson79/script.js
+++ my_progress/Section7-DOM-and-Events-Fundamentals/lesson79/script.js
@@ -5,3 +5,5 @@ const overlay = document.querySelector('.overlay');
 const btnCloseModal = document.querySelector('.close-modal');
 const btnsOpenModal = document.querySelectorAll('.show-modal');
 console.log(btnsOpenModal);
+
+for (let i = 0)
