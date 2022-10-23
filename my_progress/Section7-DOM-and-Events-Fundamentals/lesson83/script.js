8b4270b747361d1fda93ef202eefcebae1947169
Evgeniy_Utkin <eugene.utkin@gmail.com>
2022-10-23 19:04:29 +0600

    currentScore variable added

--- my_progress/Section7-DOM-and-Events-Fundamentals/lesson83/script.js
+++ my_progress/Section7-DOM-and-Events-Fundamentals/lesson83/script.js
@@ -13,6 +13,8 @@ score0El.textContent = 0;
 score1El.textContent = 0;
 diceEl.classList.add('hidden');
 
+let currentScore = 0;
+
 // Rolling dice functionality
 btnRoll.addEventListener('click', function () {
   // 1. Generating a random dice roll
