68aa25d0a492f9b154e5aa19bcc2032cf16d35ea
Evgeniy_Utkin <eugene.utkin@gmail.com>
2022-10-23 15:23:10 +0600

    const btnHold added

--- my_progress/Section7-DOM-and-Events-Fundamentals/lesson83/script.js
+++ my_progress/Section7-DOM-and-Events-Fundamentals/lesson83/script.js
@@ -6,6 +6,7 @@ const score1El = document.getElementById('score--1');
 const diceEl = document.querySelector('.dice');
 const btnNew = document.querySelector('.btn--new');
 const btnRoll = document.querySelector('.btn--roll');
+const btnHold = document.querySelector('.btn--hold');
 
 // Starting conditions
 score0El.textContent = 0;
