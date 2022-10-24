5e16388d5bb52a54ccb6a3c279e2baeffe5c5b5d
Evgeniy_Utkin <eugene.utkin@gmail.com>
2022-10-24 19:25:02 +0600

    const player0El added

--- my_progress/Section7-DOM-and-Events-Fundamentals/lesson84/script.js
+++ my_progress/Section7-DOM-and-Events-Fundamentals/lesson84/script.js
@@ -1,6 +1,7 @@
 'use strict';
 
 // Selecting elements
+const player0El = document.querySelector('.player--0');
 const score0El = document.querySelector('#score--0');
 const score1El = document.getElementById('score--1');
 const current0El = document.getElementById('current--0');
@@ -38,7 +39,7 @@ btnRoll.addEventListener('click', function () {
   } else {
     // Switch to next player
     document.getElementById(`current--${activePlayer}`).textContent = 0;
-    activePlayer = activePlayer === 0 ? 1 : 0;
     currentScore = 0;
+    activePlayer = activePlayer === 0 ? 1 : 0;
   }
 });
