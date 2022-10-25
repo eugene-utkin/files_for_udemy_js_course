b919da1e9f014c9a0e117f7366772436f9a77dc0
Evgeniy_Utkin <eugene.utkin@gmail.com>
2022-10-25 17:38:34 +0600

    switchPlayer function added

--- my_progress/Section7-DOM-and-Events-Fundamentals/lesson85/script.js
+++ my_progress/Section7-DOM-and-Events-Fundamentals/lesson85/script.js
@@ -21,6 +21,14 @@ const scores = [0, 0];
 let currentScore = 0;
 let activePlayer = 0;
 
+const switchPlayer = function () {
+  document.getElementById(`current--${activePlayer}`).textContent = 0;
+  currentScore = 0;
+  activePlayer = activePlayer === 0 ? 1 : 0;
+  player0El.classList.toggle('player--active');
+  player1El.classList.toggle('player--active');
+};
+
 // Rolling dice functionality
 btnRoll.addEventListener('click', function () {
   // 1. Generating a random dice roll
@@ -39,11 +47,6 @@ btnRoll.addEventListener('click', function () {
       currentScore;
   } else {
     // Switch to next player
-    document.getElementById(`current--${activePlayer}`).textContent = 0;
-    currentScore = 0;
-    activePlayer = activePlayer === 0 ? 1 : 0;
-    player0El.classList.toggle('player--active');
-    player1El.classList.toggle('player--active');
   }
 });
 
