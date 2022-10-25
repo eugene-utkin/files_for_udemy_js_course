6b4eccb70e1db7ec0ba7aca838677308417f69c3
Evgeniy_Utkin <eugene.utkin@gmail.com>
2022-10-25 17:28:37 +0600

    switch the player comment added

--- my_progress/Section7-DOM-and-Events-Fundamentals/lesson85/script.js
+++ my_progress/Section7-DOM-and-Events-Fundamentals/lesson85/script.js
@@ -51,4 +51,5 @@ btnHold.addEventListener('click', function () {
   // 1. Add current score to active player's score
   // 2. Check if player's score is >= 100
   // Finish the game
+  // Switch to the next player
 });
