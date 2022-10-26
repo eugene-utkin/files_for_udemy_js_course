cbd6982f064f964fd0b1562b71329371277c553b
Evgeniy_Utkin <eugene.utkin@gmail.com>
2022-10-26 14:32:40 +0600

    let moved outside

--- my_progress/Section7-DOM-and-Events-Fundamentals/lesson86/script.js
+++ my_progress/Section7-DOM-and-Events-Fundamentals/lesson86/script.js
@@ -12,6 +12,8 @@ const btnNew = document.querySelector('.btn--new');
 const btnRoll = document.querySelector('.btn--roll');
 const btnHold = document.querySelector('.btn--hold');
 
+let scores, currentScore, activePlayer, playing;
+
 // Starting conditions
 const init = function () {
   scores = [0, 0];
