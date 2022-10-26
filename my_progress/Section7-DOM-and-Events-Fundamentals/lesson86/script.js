382dbd05214dda96a487dd316e748af60ed3cb9c
Evgeniy_Utkin <eugene.utkin@gmail.com>
2022-10-26 14:11:20 +0600

    current scores reset

--- my_progress/Section7-DOM-and-Events-Fundamentals/lesson86/script.js
+++ my_progress/Section7-DOM-and-Events-Fundamentals/lesson86/script.js
@@ -84,12 +84,15 @@ btnHold.addEventListener('click', function () {
 btnNew.addEventListener('click', function () {
   score0El.textContent = 0;
   score1El.textContent = 0;
+  current0El.textContent = 0;
+  current1El.textContent = 0;
   diceEl.classList.add('hidden');
 
   scores = [0, 0];
 
   currentScore = 0;
   activePlayer = 0;
+  playing = true;
 
   player0El.classList.add('player--active');
   player0El.classList.remove('player--winner');
