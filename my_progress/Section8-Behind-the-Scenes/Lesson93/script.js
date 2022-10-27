6ba33df54019ed86982edb1a76653dca43af894b
Evgeniy_Utkin <eugene.utkin@gmail.com>
2022-10-27 14:55:56 +0600

    firstName used in function

--- my_progress/Section8-Behind-the-Scenes/Lesson93/script.js
+++ my_progress/Section8-Behind-the-Scenes/Lesson93/script.js
@@ -4,7 +4,7 @@ function calcAge(birthYear) {
   const age = 2037 - birthYear;
 
   function printAge() {
-    const output = `You are ${age}, born in ${birthYear}`;
+    const output = `${firstName}, you are ${age}, born in ${birthYear}`;
     console.log(output);
   }
   printAge();
