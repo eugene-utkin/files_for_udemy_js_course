cec2a87c7990fdc79b3a952f932b0270250b0898
Evgeniy_Utkin <eugene.utkin@gmail.com>
2022-10-28 22:28:19 +0600

    const changed to let for output

--- my_progress/Section8-Behind-the-Scenes/Lesson93/script.js
+++ my_progress/Section8-Behind-the-Scenes/Lesson93/script.js
@@ -4,7 +4,7 @@ function calcAge(birthYear) {
   const age = 2037 - birthYear;
 
   function printAge() {
-    const output = `${firstName}, you are ${age}, born in ${birthYear}`;
+    let output = `${firstName}, you are ${age}, born in ${birthYear}`;
     console.log(output);
 
     if (birthYear >= 1981 && birthYear <= 1996) {
