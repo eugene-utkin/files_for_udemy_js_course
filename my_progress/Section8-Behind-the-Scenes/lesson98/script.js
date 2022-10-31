7a6f9c97a9e79146f92928990d201438838a089c
Evgeniy_Utkin <eugene.utkin@gmail.com>
2022-10-31 20:43:29 +0600

    trying to use arguments in arrow function

--- my_progress/Section8-Behind-the-Scenes/lesson98/script.js
+++ my_progress/Section8-Behind-the-Scenes/lesson98/script.js
@@ -156,4 +156,8 @@ const addExpr = function (a, b) {
 addExpr(2, 5);
 addExpr(2, 5, 8, 12);
 
-var addArrow = (a, b) => a + b;
+var addArrow = (a, b) => {
+  console.log(arguments);
+  return a + b;
+};
+addArrow(2, 5, 8);
