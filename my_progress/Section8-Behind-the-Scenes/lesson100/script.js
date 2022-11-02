1f0394273583af55e7fd0ab47e15bf2ca2d9271c
Evgeniy_Utkin <eugene.utkin@gmail.com>
2022-11-02 19:35:14 +0600

    old code commented out

--- my_progress/Section8-Behind-the-Scenes/lesson100/script.js
+++ my_progress/Section8-Behind-the-Scenes/lesson100/script.js
@@ -164,6 +164,7 @@ var addArrow = (a, b) => {
 addArrow(2, 5, 8);
 */
 
+/*
 let age = 30;
 let oldAge = age;
 age = 31;
@@ -179,3 +180,4 @@ const friend = me;
 friend.age = 27;
 console.log('Friend:', friend);
 console.log('Me:', me);
+*/
