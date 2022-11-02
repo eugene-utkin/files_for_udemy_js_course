a9d96438e8599470724b25191b3dd6bf2fc45538
Evgeniy_Utkin <eugene.utkin@gmail.com>
2022-11-02 20:05:44 +0600

    family added to jessica2

--- my_progress/Section8-Behind-the-Scenes/lesson100/script.js
+++ my_progress/Section8-Behind-the-Scenes/lesson100/script.js
@@ -205,6 +205,10 @@ const jessica2 = {
   firstName: 'Jessica',
   lastName: 'Williams',
   age: 27,
+  family: ['Alice', 'Bob'],
 };
 
 const jessicaCopy = Object.assign({}, jessica2);
+jessicaCopy.lastName = 'Davis';
+console.log('Before marriage:', jessica2);
+console.log('After marriage:', jessicaCopy);
