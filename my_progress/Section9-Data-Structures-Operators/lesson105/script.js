693ee2aef1289ab24ab7443a3416c3f8fdfd8dd4
Evgeniy_Utkin <eugene.utkin@gmail.com>
2022-11-06 16:23:09 +0600

    console log added

--- my_progress/Section9-Data-Structures-Operators/lesson105/script.js
+++ my_progress/Section9-Data-Structures-Operators/lesson105/script.js
@@ -91,6 +91,9 @@ const newRestaurant = { foundedIn: 1998, ...restaurant, founder: 'Guiseppe' };
 console.log(newRestaurant);
 
 const restaurantCopy = { ...restaurant };
+restaurantCopy.name = 'Ristorante Roma';
+console.log(restaurantCopy.name);
+console.log(restaurant.name);
 
 /*
 // Destructuring Objects
