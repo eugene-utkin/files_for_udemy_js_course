f1249795c7372e5e682e084bdfa36c0ec0d60033
Evgeniy_Utkin <eugene.utkin@gmail.com>
2022-11-06 13:59:10 +0600

    orderPasta method added

--- my_progress/Section9-Data-Structures-Operators/lesson105/script.js
+++ my_progress/Section9-Data-Structures-Operators/lesson105/script.js
@@ -40,6 +40,12 @@ const restaurant = {
       `Order received! ${this.starterMenu[starterIndex]} and ${this.mainMenu[mainIndex]} will be delivered to ${address} at ${time}`
     );
   },
+
+  orderPasta: function (ing1, ing2, ing3) {
+    console.log(
+      `Here is your delicious pasta with ${ing1}, ${ing2} and ${ing3}`
+    );
+  },
 };
 
 const arr = [7, 8, 9];
