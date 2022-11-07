f6b873949b588dee229fb43c122cb1495cc7e06e
Evgeniy_Utkin <eugene.utkin@gmail.com>
2022-11-07 19:49:15 +0600

    function refactored

--- my_progress/Section9-Data-Structures-Operators/lesson106/script.js
+++ my_progress/Section9-Data-Structures-Operators/lesson106/script.js
@@ -69,10 +69,8 @@ console.log(weekdays);
 // 2) Functions
 const add = function (...numbers) {
   let sum = 0;
-  for (let i = 0; i < numbers.length; i++) {
-    sum += numbers[i];
-  }
-  return sum;
+  for (let i = 0; i < numbers.length; i++) sum += numbers[i];
+  console.log(sum);
 };
 add(2, 3);
 add(5, 3, 7, 2);
