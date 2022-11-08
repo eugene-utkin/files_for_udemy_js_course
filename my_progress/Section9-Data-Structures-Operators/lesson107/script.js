580f8459fac37ad7d34ab4a336eb3d582afb0981
Evgeniy_Utkin <eugene.utkin@gmail.com>
2022-11-08 20:10:15 +0600

    restaurant.numGuests assigned

--- my_progress/Section9-Data-Structures-Operators/lesson107/script.js
+++ my_progress/Section9-Data-Structures-Operators/lesson107/script.js
@@ -61,6 +61,7 @@ console.log(undefined || null);
 
 console.log(undefined || 0 || '' || 'Hello' || 23 || null);
 
+restaurant.numGuests = 23;
 const guests1 = restaurant.numGuests ? restaurant.numGuests : 10;
 console.log(guests1);
 
