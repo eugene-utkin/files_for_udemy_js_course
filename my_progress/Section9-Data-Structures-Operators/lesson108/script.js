52e3aacaec2f9a795be0f25cc56b6865bb6b84d0
Evgeniy_Utkin <eugene.utkin@gmail.com>
2022-11-09 18:49:36 +0600

    old code copied

--- my_progress/Section9-Data-Structures-Operators/lesson108/script.js
+++ my_progress/Section9-Data-Structures-Operators/lesson108/script.js
@@ -53,6 +53,13 @@ const restaurant = {
   },
 };
 
+restaurant.numGuests = 0;
+const guests1 = restaurant.numGuests ? restaurant.numGuests : 10;
+console.log(guests1);
+
+const guests2 = restaurant.numGuests || 10;
+console.log(guests2);
+
 /*
 // Short Circuiting (&& and ||)
 console.log('---- OR ----');
