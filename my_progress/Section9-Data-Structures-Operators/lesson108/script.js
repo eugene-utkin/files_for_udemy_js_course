f1be24f95b10d08b0ba857b95244d647fb074a8c
Evgeniy_Utkin <eugene.utkin@gmail.com>
2022-11-09 18:56:18 +0600

    Nullish comment added

--- my_progress/Section9-Data-Structures-Operators/lesson108/script.js
+++ my_progress/Section9-Data-Structures-Operators/lesson108/script.js
@@ -57,6 +57,7 @@ restaurant.numGuests = 0;
 const guests = restaurant.numGuests || 10;
 console.log(guests);
 
+// Nullish: null or undefined (NOT 0 or '')
 const guestsCorrect = restaurant.numGuests ?? 10;
 console.log(guestsCorrect);
 
