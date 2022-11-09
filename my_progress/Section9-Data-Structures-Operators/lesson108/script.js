cfb0a0745c8618c2a9e615556d87c5dde0da715f
Evgeniy_Utkin <eugene.utkin@gmail.com>
2022-11-09 18:50:11 +0600

    only || operator kept

--- my_progress/Section9-Data-Structures-Operators/lesson108/script.js
+++ my_progress/Section9-Data-Structures-Operators/lesson108/script.js
@@ -54,8 +54,6 @@ const restaurant = {
 };
 
 restaurant.numGuests = 0;
-const guests1 = restaurant.numGuests ? restaurant.numGuests : 10;
-console.log(guests1);
 
 const guests2 = restaurant.numGuests || 10;
 console.log(guests2);
