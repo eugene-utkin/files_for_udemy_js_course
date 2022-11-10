4c431516729e230603bf82e1382ea7b004898bb5
Evgeniy_Utkin <eugene.utkin@gmail.com>
2022-11-10 20:24:54 +0600

    &&= used

--- my_progress/Section9-Data-Structures-Operators/lesson109/script.js
+++ my_progress/Section9-Data-Structures-Operators/lesson109/script.js
@@ -76,6 +76,8 @@ rest2.numGuests ??= 10;
 
 rest1.owner = rest1.owner && '<ANONYMOUS>';
 rest2.owner = rest2.owner && '<ANONYMOUS>';
+rest1 &&= '<ANONYMOUS>';
+rest2 &&= '<ANONYMOUS>';
 
 console.log(rest1);
 console.log(rest2);
