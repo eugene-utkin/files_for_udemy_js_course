e024ecbdf8ce6dc08cb8a407dfd90620d1bbc515
Evgeniy_Utkin <eugene.utkin@gmail.com>
2022-11-10 18:59:47 +0600

    old code commented out

--- my_progress/Section9-Data-Structures-Operators/lesson109/script.js
+++ my_progress/Section9-Data-Structures-Operators/lesson109/script.js
@@ -63,8 +63,8 @@ const rest2 = {
   owner: 'Giovanni Rossi',
 };
 
-rest1.numGuests = rest1.numGuests || 10;
-rest2.numGuests = rest2.numGuests || 10;
+// rest1.numGuests = rest1.numGuests || 10;
+// rest2.numGuests = rest2.numGuests || 10;
 
 rest1.numGuests ||= 10;
 rest2.numGuests ||= 10;
