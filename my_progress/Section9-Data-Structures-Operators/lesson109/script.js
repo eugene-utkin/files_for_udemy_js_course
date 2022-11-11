b9d8842cc7e364e25248afd8ebe5fcfd68f0b1fb
Evgeniy_Utkin <eugene.utkin@gmail.com>
2022-11-11 18:54:23 +0600

    old code commented out

--- my_progress/Section9-Data-Structures-Operators/lesson109/script.js
+++ my_progress/Section9-Data-Structures-Operators/lesson109/script.js
@@ -4,6 +4,7 @@
 const flights =
   '_Delayed_Departure;fao93766109;txl2133758440;11:25+_Arrival;bru0943384722;fao93766109;11:45+_Delayed_Arrival;hel7439299980;fao93766109;12:05+_Departure;fao93766109;lis2323639855;12:30';
 
+/*
 // Data needed for first part of the section
 const restaurant = {
   name: 'Classico Italiano',
@@ -82,6 +83,7 @@ rest2.owner &&= '<ANONYMOUS>';
 
 console.log(rest1);
 console.log(rest2);
+*/
 
 /*
 // The Nullish Coalescing Operator
