29c27ece471ae8f41dfa86585a7d6f8bf15da3ad
Evgeniy_Utkin <eugene.utkin@gmail.com>
2022-11-13 10:56:41 +0600

    enhanced object literals comment added

--- my_progress/Section9-Data-Structures-Operators/lesson112/script.js
+++ my_progress/Section9-Data-Structures-Operators/lesson112/script.js
@@ -27,6 +27,8 @@ const restaurant = {
   categories: ['Italian', 'Pizzeria', 'Vegetarian', 'Organic'],
   starterMenu: ['Focaccia', 'Bruschetta', 'Garlic Bread', 'Caprese Salad'],
   mainMenu: ['Pizza', 'Pasta', 'Risotto'],
+
+  // ES6 enhanced object literals
   openingHours,
 
   order: function (starterIndex, mainIndex) {
