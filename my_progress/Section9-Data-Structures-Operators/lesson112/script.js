261bc909ea7f45928397a178c3cf70ddaf8b511c
Evgeniy_Utkin <eugene.utkin@gmail.com>
2022-11-13 11:06:01 +0600

    enhanced object literals with methods third example

--- my_progress/Section9-Data-Structures-Operators/lesson112/script.js
+++ my_progress/Section9-Data-Structures-Operators/lesson112/script.js
@@ -41,7 +41,7 @@ const restaurant = {
     );
   },
 
-  orderPasta: function (ing1, ing2, ing3) {
+  orderPasta(ing1, ing2, ing3) {
     console.log(
       `Here is your delicious pasta with ${ing1}, ${ing2} and ${ing3}`
     );
