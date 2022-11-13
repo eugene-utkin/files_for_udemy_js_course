e531c4406a670280211185204dba230b56060a28
Evgeniy_Utkin <eugene.utkin@gmail.com>
2022-11-13 11:05:19 +0600

    enhanced object literals with methods second example

--- my_progress/Section9-Data-Structures-Operators/lesson112/script.js
+++ my_progress/Section9-Data-Structures-Operators/lesson112/script.js
@@ -35,12 +35,7 @@ const restaurant = {
     return [this.starterMenu[starterIndex], this.mainMenu[mainIndex]];
   },
 
-  orderDelivery: function ({
-    starterIndex = 1,
-    mainIndex = 0,
-    time = '20:00',
-    address,
-  }) {
+  orderDelivery({ starterIndex = 1, mainIndex = 0, time = '20:00', address }) {
     console.log(
       `Order received! ${this.starterMenu[starterIndex]} and ${this.mainMenu[mainIndex]} will be delivered to ${address} at ${time}`
     );
