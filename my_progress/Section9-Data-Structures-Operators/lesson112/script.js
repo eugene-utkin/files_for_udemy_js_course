bf9a7879f69cda96d434a61a847cc7b41bd634be
Evgeniy_Utkin <eugene.utkin@gmail.com>
2022-11-13 11:24:43 +0600

    enhanced object literals with keys examples

--- my_progress/Section9-Data-Structures-Operators/lesson112/script.js
+++ my_progress/Section9-Data-Structures-Operators/lesson112/script.js
@@ -8,15 +8,15 @@ const flights =
 
 const weekdays = ['mon', 'tue', 'wed', 'thu', 'fri', 'sat', 'sun'];
 const openingHours = {
-  thu: {
+  [weekdays[3]]: {
     open: 12,
     close: 22,
   },
-  fri: {
+  [weekdays[4]]: {
     open: 11,
     close: 23,
   },
-  sat: {
+  [weekdays[5]]: {
     open: 0, // Open 24 hours
     close: 24,
   },
