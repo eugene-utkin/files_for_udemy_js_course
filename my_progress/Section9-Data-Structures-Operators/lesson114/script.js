46bb24def853ca9bc81ec5dcbd0ff57d17d98db5
Evgeniy_Utkin <eugene.utkin@gmail.com>
2022-11-15 19:41:47 +0600

    let openStr added

--- my_progress/Section9-Data-Structures-Operators/lesson114/script.js
+++ my_progress/Section9-Data-Structures-Operators/lesson114/script.js
@@ -57,7 +57,7 @@ const restaurant = {
 const properties = Object.keys(openingHours);
 console.log(properties);
 
-console.log(`We are open on ${properties.length} days`);
+let openStr = console.log(`We are open on ${properties.length} days`);
 
 for (const day of properties) {
   console.log(day);
