98163b82c90874e960c1d3b8340dd4e86cb7dc29
Evgeniy_Utkin <eugene.utkin@gmail.com>
2022-11-18 18:43:31 +0600

    rest.clear commented out

--- my_progress/Section9-Data-Structures-Operators/lesson117/script.js
+++ my_progress/Section9-Data-Structures-Operators/lesson117/script.js
@@ -74,7 +74,7 @@ console.log(rest.get(time > rest.get('open') && time < rest.get('close')));
 
 console.log(rest.has('categories'));
 rest.delete(2);
-rest.clear();
+// rest.clear();
 console.log(rest);
 console.log(rest.size);
 
