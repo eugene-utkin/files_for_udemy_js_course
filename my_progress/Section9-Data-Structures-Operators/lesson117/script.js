240fb576b808bdd6046ac016d2c6a88217859be0
Evgeniy_Utkin <eugene.utkin@gmail.com>
2022-11-18 18:26:55 +0600

    map has examples

--- my_progress/Section9-Data-Structures-Operators/lesson117/script.js
+++ my_progress/Section9-Data-Structures-Operators/lesson117/script.js
@@ -69,8 +69,10 @@ rest
 console.log(rest.get('name'));
 console.log(rest.get(true));
 
-const time = 21;
+const time = 8;
 console.log(rest.get(time > rest.get('open') && time < rest.get('close')));
+
+console.log(rest.has('categories'));
 
 /*
 // Sets
