50f3856a142cc1782b4ff1ccfaf71347a893211f
Evgeniy_Utkin <eugene.utkin@gmail.com>
2022-11-18 18:17:41 +0600

    more sets added

--- my_progress/Section9-Data-Structures-Operators/lesson117/script.js
+++ my_progress/Section9-Data-Structures-Operators/lesson117/script.js
@@ -62,7 +62,9 @@ console.log(rest.set(2, 'Lisbon, Portugal'));
 rest
   .set('categories', ['Italian', 'Pizzeria', 'Vegetarian', 'Organic'])
   .set('open', 11)
-  .set('close', 23);
+  .set('close', 23)
+  .set(true, 'We are open :D')
+  .set(false, 'We are closed :(');
 
 /*
 // Sets
