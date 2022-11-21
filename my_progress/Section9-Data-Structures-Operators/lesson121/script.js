d6716721b14d521ebf3166640e41dc4796f8fd51
Evgeniy_Utkin <eugene.utkin@gmail.com>
2022-11-21 20:21:57 +0600

    more string slice examples

--- my_progress/Section9-Data-Structures-Operators/lesson121/script.js
+++ my_progress/Section9-Data-Structures-Operators/lesson121/script.js
@@ -72,6 +72,9 @@ console.log(airline.indexOf('portugal'));
 console.log(airline.slice(4));
 console.log(airline.slice(4, 7));
 
+console.log(airline.slice(0, airline.indexOf(' ')));
+console.log(airline.slice(airline.lastIndexOf(' ') + 1));
+
 /*
 const question = new Map([
   ['question', 'What is the best programming language in the world?'],
