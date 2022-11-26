15c87068cca6b618479105b261b254629835d680
Evgeniy_Utkin <eugene.utkin@gmail.com>
2022-11-26 18:18:30 +0600

    third variant (with replace)

--- my_progress/Section9-Data-Structures-Operators/lesson123/script.js
+++ my_progress/Section9-Data-Structures-Operators/lesson123/script.js
@@ -64,12 +64,12 @@ const newName = ['Mr.', firstName, lastName.toUpperCase()].join(' ');
 console.log(newName);
 
 const capitalizeName = function (name) {
-  const names = name.toUpperCase().split(' ');
+  const names = name.split(' ');
   const namesUpper = [];
 
   for (const n of names) {
     // namesUpper.push(n[0].toUpperCase() + n.slice(1));
-    namesUpper.push(n[0] + n.slice(1).toLowerCase());
+    namesUpper.push(n.replace(n[0], n[0].toUpperCase()));
   }
   console.log(namesUpper.join(' '));
 };
