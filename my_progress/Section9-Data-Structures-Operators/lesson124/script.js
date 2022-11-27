d61336b230488ee069c7ad2d768d05ced17ae456
Evgeniy_Utkin <eugene.utkin@gmail.com>
2022-11-27 21:20:35 +0600

    typo fixed

--- my_progress/Section9-Data-Structures-Operators/lesson124/script.js
+++ my_progress/Section9-Data-Structures-Operators/lesson124/script.js
@@ -828,8 +828,12 @@ document.querySelector('button').addEventListener('click', function () {
   const rows = text.split('\n');
   console.log(rows);
 
-  for (const row in rows) {
+  for (const row of rows) {
     const [first, second] = row.toLowerCase().trim().split('_');
+    const output = `${first}${second.replace(
+      second[0],
+      second[0].toUpperCase
+    )}`;
   }
 });
 
