61f5b474f9f814881fef85c5d5b55f1bacadd718
Evgeniy_Utkin <eugene.utkin@gmail.com>
2022-11-30 16:26:13 +0600

    third console log added to transformer function

--- my_progress/Section10-Functions/lesson131/script.js
+++ my_progress/Section10-Functions/lesson131/script.js
@@ -76,6 +76,8 @@ const upperFirstWord = function (str) {
 const transformer = function (str, fn) {
   console.log(`Original string: ${str}`);
   console.log(`Transformed string: ${fn(str)}`);
+
+  console.log(`Transformed by: ${fn.name}`);
 };
 
 transformer('JavaScript is the best!', upperFirstWord);
