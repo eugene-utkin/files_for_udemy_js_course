8ec3992d20d74ad2882b9a9c2850f57076bb2c5a
Evgeniy_Utkin <eugene.utkin@gmail.com>
2022-12-01 13:07:40 +0600

    return function name added to greet function

--- my_progress/Section10-Functions/lesson132/script.js
+++ my_progress/Section10-Functions/lesson132/script.js
@@ -92,4 +92,6 @@ document.body.addEventListener('click', high5);
 ['Jonas', 'Martha', 'Adam'].forEach(high5);
 */
 
-const greet = function (greeting) {};
+const greet = function (greeting) {
+  return function (name) {};
+};
