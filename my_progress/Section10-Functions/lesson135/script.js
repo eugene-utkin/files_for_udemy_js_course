e80e026668b61bc03a0aed22a5dd0130b7be463a
Evgeniy_Utkin <eugene.utkin@gmail.com>
2022-12-04 18:49:55 +0600

    registerNewAnswer method added

--- my_progress/Section10-Functions/lesson135/script.js
+++ my_progress/Section10-Functions/lesson135/script.js
@@ -272,4 +272,11 @@ const poll = {
   options: ['0: JavaScript', '1: Python', '2: Rust', '3: C++'],
   // This generates [0, 0, 0, 0]. More in the next section!
   answers: new Array(4).fill(0),
+  registerNewAnswer() {
+    const answer = Number(
+      prompt(
+        `${this.question}\n${this.options.join('\n')}\n(Write option number)`
+      )
+    );
+  },
 };
