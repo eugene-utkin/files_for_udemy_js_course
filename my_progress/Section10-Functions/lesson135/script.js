670ef35e1007071f7f30dbcfb731cc8ed81763a5
Evgeniy_Utkin <eugene.utkin@gmail.com>
2022-12-04 19:40:08 +0600

    displayResults method added

--- my_progress/Section10-Functions/lesson135/script.js
+++ my_progress/Section10-Functions/lesson135/script.js
@@ -286,7 +286,16 @@ const poll = {
       answer < this.answers.length &&
       this.answers[answer]++;
 
-    console.log(this.answers);
+    this.displayResults();
+    this.displayResults('string');
+  },
+  displayResults(type = 'array') {
+    if (type === 'array') {
+      console.log(this.answers);
+    } else if (type === 'string') {
+      // Poll results are 13, 2, 4, 1
+      console.log(`Poll results are ${this.answers.join(', ')}`);
+    }
   },
 };
 // poll.registerNewAnswer();
