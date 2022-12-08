25874b19179f43b35654fe4726f8e46f6679cc7f
Evgeniy_Utkin <eugene.utkin@gmail.com>
2022-12-08 14:07:55 +0600

    eventListener added

--- my_progress/Section10-Functions/lesson139/script.js
+++ my_progress/Section10-Functions/lesson139/script.js
@@ -402,4 +402,7 @@ boardPassengers(180, 3);
 (function () {
   const header = document.querySelector('h1');
   header.style.color = 'red';
+  document.querySelector('body').addEventListener('click', function () {
+    header.style.color = 'blue';
+  });
 });
