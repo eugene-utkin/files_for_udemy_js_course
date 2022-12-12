560eae54d64daf493d53495cb6f01fcf128a19fd
Evgeniy_Utkin <eugene.utkin@gmail.com>
2022-12-12 16:20:22 +0600

    if condition added to for loop

--- my_progress/Section11-Arrays-Bankist/lesson144/script.js
+++ my_progress/Section11-Arrays-Bankist/lesson144/script.js
@@ -127,4 +127,7 @@ console.log('jonas'.at(-1));
 const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
 
 for (const movement of movements) {
+  if (movement > 0) {
+    console.log(`You deposited ${movement}`);
+  }
 }
