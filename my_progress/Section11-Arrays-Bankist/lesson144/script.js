9c85640eb4e5fb4042862736cdfd190b7bb88cb1
Evgeniy_Utkin <eugene.utkin@gmail.com>
2022-12-12 16:22:10 +0600

    Math abs used

--- my_progress/Section11-Arrays-Bankist/lesson144/script.js
+++ my_progress/Section11-Arrays-Bankist/lesson144/script.js
@@ -130,6 +130,6 @@ for (const movement of movements) {
   if (movement > 0) {
     console.log(`You deposited ${movement}`);
   } else {
-    console.log(`You withdrew ${movement}`);
+    console.log(`You withdrew ${Math.abs(movement)}`);
   }
 }
