6cf991d0c1a9be48804af7c2a88c8025453ffa1b
Evgeniy_Utkin <eugene.utkin@gmail.com>
2022-12-14 22:04:23 +0600

    insertAdjacentHTML example

--- my_progress/Section11-Arrays-Bankist/lesson147/script.js
+++ my_progress/Section11-Arrays-Bankist/lesson147/script.js
@@ -64,6 +64,7 @@ const inputClosePin = document.querySelector('.form__input--pin');
 const displayMovements = function (movements) {
   movements.forEach(function (mov, i) {
     const type = mov > 0 ? 'deposit' : 'withdrawal';
+
     const html = `
       <div class="movements__row">
         <div class="movements__type movements__type--${type}">${
@@ -72,6 +73,8 @@ const displayMovements = function (movements) {
         <div class="movements__value">${mov}</div>
       </div>
     `;
+
+    containerMovements.insertAdjacentHTML('afterbegin', html);
   });
 };
 displayMovements(account1.movements);
