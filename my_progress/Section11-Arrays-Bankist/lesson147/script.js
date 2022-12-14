1e629935c37c6d5c9c0262284bd14566b9a9c630
Evgeniy_Utkin <eugene.utkin@gmail.com>
2022-12-14 21:32:10 +0600

    const html added to displayMovements function

--- my_progress/Section11-Arrays-Bankist/lesson147/script.js
+++ my_progress/Section11-Arrays-Bankist/lesson147/script.js
@@ -62,7 +62,15 @@ const inputCloseUsername = document.querySelector('.form__input--user');
 const inputClosePin = document.querySelector('.form__input--pin');
 
 const displayMovements = function (movements) {
-  movements.forEach(function (mov, i) {});
+  movements.forEach(function (mov, i) {
+    const html = `
+      <div class="movements__row">
+        <div class="movements__type movements__type--deposit">2 deposit</div>
+        <div class="movements__date">3 days ago</div>
+        <div class="movements__value">4 000€</div>
+      </div>
+    `;
+  });
 };
 displayMovements(account1.movements);
 
