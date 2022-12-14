adfc8c4775a4c0b422d01753e7cf0d86a2d3365a
Evgeniy_Utkin <eugene.utkin@gmail.com>
2022-12-14 21:36:53 +0600

    type used in const html

--- my_progress/Section11-Arrays-Bankist/lesson147/script.js
+++ my_progress/Section11-Arrays-Bankist/lesson147/script.js
@@ -66,7 +66,9 @@ const displayMovements = function (movements) {
     const type = mov > 0 ? 'deposit' : 'withdrawal';
     const html = `
       <div class="movements__row">
-        <div class="movements__type movements__type--deposit">${i + 1}</div>
+        <div class="movements__type movements__type--${type}">${
+      i + 1
+    } ${type}</div>
         <div class="movements__value">${mov}</div>
       </div>
     `;
