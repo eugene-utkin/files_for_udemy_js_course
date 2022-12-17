ee73442d5d9aa7d73eac0deb957e2d616792adf7
Evgeniy_Utkin <eugene.utkin@gmail.com>
2022-12-17 15:53:28 +0600

    example comment added

--- my_progress/Section11-Arrays-Bankist/lesson147/script.js
+++ my_progress/Section11-Arrays-Bankist/lesson147/script.js
@@ -219,5 +219,7 @@ const checkDogs = function (dogsJulia, dogsKate) {
   // dogsJulia.slice(1, 3);
   const dogs = dogsJuliaCorrected.concat(dogsKate);
   console.log(dogs);
+
+  // "Dog number 1 is an adult, and is 5 years old") or a puppy ("Dog number 2 is still a puppy 🐶"
 };
 checkDogs([3, 5, 2, 12, 7], [4, 1, 15, 8, 3]);
