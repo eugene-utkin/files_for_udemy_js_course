9a9a3e37d9f486ca026dce7090ee0fe1be391c5f
Evgeniy_Utkin <eugene.utkin@gmail.com>
2022-12-17 17:06:46 +0600

    else condition added to forEach loop

--- my_progress/Section11-Arrays-Bankist/lesson147/script.js
+++ my_progress/Section11-Arrays-Bankist/lesson147/script.js
@@ -224,6 +224,8 @@ const checkDogs = function (dogsJulia, dogsKate) {
   dogs.forEach(function (dog, i) {
     if (dog >= 3) {
       console.log(`Dog number ${i + 1} is an adult, and is ${dog} years old`);
+    } else {
+      console.log(`Dog number ${i + 1} is still a puppy 🐶`);
     }
   });
 };
