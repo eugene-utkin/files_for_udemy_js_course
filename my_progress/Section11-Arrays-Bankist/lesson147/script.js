41a8ba63771c8550917e857b3ab09b0ced1b8e9d
Evgeniy_Utkin <eugene.utkin@gmail.com>
2022-12-17 14:58:58 +0600

    console log added

--- my_progress/Section11-Arrays-Bankist/lesson147/script.js
+++ my_progress/Section11-Arrays-Bankist/lesson147/script.js
@@ -216,5 +216,6 @@ const checkDogs = function (dogsJulia, dogsKate) {
   const dogsJuliaCorrected = dogsJulia.slice();
   dogsJuliaCorrected.splice(0, 1);
   dogsJuliaCorrected.splice(-2);
+  console.log(dogsJuliaCorrected);
 };
 checkDogs([3, 5, 2, 12, 7], [4, 1, 15, 8, 3]);
