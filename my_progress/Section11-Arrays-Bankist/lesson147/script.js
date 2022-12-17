46ee3182e3ddfd29a21cece3a039f579c9b9b6dc
Evgeniy_Utkin <eugene.utkin@gmail.com>
2022-12-17 15:49:51 +0600

    comment with alt solution added

--- my_progress/Section11-Arrays-Bankist/lesson147/script.js
+++ my_progress/Section11-Arrays-Bankist/lesson147/script.js
@@ -216,6 +216,6 @@ const checkDogs = function (dogsJulia, dogsKate) {
   const dogsJuliaCorrected = dogsJulia.slice();
   dogsJuliaCorrected.splice(0, 1);
   dogsJuliaCorrected.splice(-2);
-  console.log(dogsJuliaCorrected);
+  // dogsJulia.slice(1, 3);
 };
 checkDogs([3, 5, 2, 12, 7], [4, 1, 15, 8, 3]);
