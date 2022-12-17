bf57aa5d21d29606bdfafa6412fcb80882411974
Evgeniy_Utkin <eugene.utkin@gmail.com>
2022-12-17 14:58:16 +0600

    last two dogs deleted

--- my_progress/Section11-Arrays-Bankist/lesson147/script.js
+++ my_progress/Section11-Arrays-Bankist/lesson147/script.js
@@ -215,5 +215,6 @@ checkDogs(juliaData2, kateData2);
 const checkDogs = function (dogsJulia, dogsKate) {
   const dogsJuliaCorrected = dogsJulia.slice();
   dogsJuliaCorrected.splice(0, 1);
+  dogsJuliaCorrected.splice(-2);
 };
 checkDogs([3, 5, 2, 12, 7], [4, 1, 15, 8, 3]);
