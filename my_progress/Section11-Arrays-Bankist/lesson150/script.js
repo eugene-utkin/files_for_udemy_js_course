000eed73e35998eef20532921855fd4c6475214d
Evgeniy_Utkin <eugene.utkin@gmail.com>
2022-12-19 21:43:11 +0600

    arr para,eter removed

--- my_progress/Section11-Arrays-Bankist/lesson150/script.js
+++ my_progress/Section11-Arrays-Bankist/lesson150/script.js
@@ -246,7 +246,7 @@ const movementsUSDfor = [];
 for (const mov of movements) movementsUSDfor.push(mov * eurToUsd);
 console.log(movementsUSDfor);
 
-const movementsDescriptions = movements.map((mov, i, arr) => {
+const movementsDescriptions = movements.map((mov, i) => {
   `Movement ${i + 1}: You ${mov > 0 ? 'deposited' : 'withdrew'} ${Math.abs(
     mov
   )}`;
