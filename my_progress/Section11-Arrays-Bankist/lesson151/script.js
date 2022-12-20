a2f36dbb8e29e4b42ef59848ab3e8652dc2aacf4
Evgeniy_Utkin <eugene.utkin@gmail.com>
2022-12-20 13:07:14 +0600

    comment with desired result added

--- my_progress/Section11-Arrays-Bankist/lesson151/script.js
+++ my_progress/Section11-Arrays-Bankist/lesson151/script.js
@@ -82,7 +82,7 @@ const displayMovements = function (movements) {
 };
 displayMovements(account1.movements);
 
-const user = 'Steven Thomas Williams';
+const user = 'Steven Thomas Williams'; // stw
 
 /////////////////////////////////////////////////
 /////////////////////////////////////////////////
