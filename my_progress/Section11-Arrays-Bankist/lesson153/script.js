cd97dbee6c39d095473f408a1446aae04152337e
Evgeniy_Utkin <eugene.utkin@gmail.com>
2022-12-23 20:43:52 +0600

    typo fixed

--- my_progress/Section11-Arrays-Bankist/lesson153/script.js
+++ my_progress/Section11-Arrays-Bankist/lesson153/script.js
@@ -292,4 +292,4 @@ const balance = movements.reduce(function (acc, cur, i, arr) {
 console.log(balance);
 
 let balance2 = 0;
-for (const mov of movements) sum += mov;
+for (const mov of movements) balance2 += mov;
