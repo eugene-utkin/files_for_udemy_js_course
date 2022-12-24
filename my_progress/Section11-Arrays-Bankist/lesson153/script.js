c7af03b3f131df8d31d66917cf94c38c2c95734a
Evgeniy_Utkin <eugene.utkin@gmail.com>
2022-12-24 16:51:55 +0600

    calcPrintBalance function renamed

--- my_progress/Section11-Arrays-Bankist/lesson153/script.js
+++ my_progress/Section11-Arrays-Bankist/lesson153/script.js
@@ -82,7 +82,7 @@ const displayMovements = function (movements) {
 };
 displayMovements(account1.movements);
 
-const calcPrintBalance = function (movements) {
+const calcDisplayBalance = function (movements) {
   const balance = movements.reduce((acc, mov) => acc + mov, 0);
   labelBalance.textContent = `${balance} EUR`;
 };
