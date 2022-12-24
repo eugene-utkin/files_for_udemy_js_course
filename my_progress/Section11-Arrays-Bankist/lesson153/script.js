9427e3bd75b8780bda628651da6346496351bacb
Evgeniy_Utkin <eugene.utkin@gmail.com>
2022-12-24 17:33:12 +0600

    arrow function refactored

--- my_progress/Section11-Arrays-Bankist/lesson153/script.js
+++ my_progress/Section11-Arrays-Bankist/lesson153/script.js
@@ -303,8 +303,8 @@ for (const mov of movements) balance2 += mov;
 console.log(balance2);
 
 // Maximum value
-const max = movements.reduce((acc, mov) => {
-  if (acc > mov) return acc;
-  else return mov;
-}, movements[0]);
+const max = movements.reduce(
+  (acc, mov) => (acc > mov ? acc : mov),
+  movements[0]
+);
 console.log(max);
