5bd0c502f9aab43caf36734ac78f046fa7eb4028
Evgeniy_Utkin <eugene.utkin@gmail.com>
2022-12-26 21:36:16 +0600

    calcAverageHumanAge caled

--- my_progress/Section11-Arrays-Bankist/lesson154/script.js
+++ my_progress/Section11-Arrays-Bankist/lesson154/script.js
@@ -334,3 +334,4 @@ const calcAverageHumanAge = function (ages) {
   const humanAges = ages.map(age => (age <= 2 ? 2 * age : 16 + age * 4));
   console.log(humanAges);
 };
+calcAverageHumanAge([5, 2, 4, 1, 15, 8, 3]);
