d05bd81b5b134b8a756cd692ac985bd615523bc8
Evgeniy_Utkin <eugene.utkin@gmail.com>
2022-12-26 21:34:23 +0600

    const humanAges added

--- my_progress/Section11-Arrays-Bankist/lesson154/script.js
+++ my_progress/Section11-Arrays-Bankist/lesson154/script.js
@@ -330,4 +330,6 @@ const data2 = [16, 6, 10, 5, 6, 1, 4];
 console.log(calcAverageHumanAge(data2));
 */
 // Teacher's solution
-const calcAverageHumanAge = function (ages) {};
+const calcAverageHumanAge = function (ages) {
+  const humanAges = ages.map(age => (age <= 2 ? 2 * age : 16 + age * 4));
+};
