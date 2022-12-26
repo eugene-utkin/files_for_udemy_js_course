9a4d9cecf08e5cc7f24830393db80f515354f55e
Evgeniy_Utkin <eugene.utkin@gmail.com>
2022-12-26 21:48:58 +0600

    const average added

--- my_progress/Section11-Arrays-Bankist/lesson154/script.js
+++ my_progress/Section11-Arrays-Bankist/lesson154/script.js
@@ -335,5 +335,7 @@ const calcAverageHumanAge = function (ages) {
   const adults = humanAges.filter(age => age >= 18);
   console.log(humanAges);
   console.log(adults);
+
+  const average = adults.reduce((acc, age) => acc + age, 0) / adults.length;
 };
 calcAverageHumanAge([5, 2, 4, 1, 15, 8, 3]);
