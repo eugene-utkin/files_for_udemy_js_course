09f6640dcd9e26d02a6d40095a57048f116b2080
Evgeniy_Utkin <eugene.utkin@gmail.com>
2022-12-27 21:44:00 +0600

    old code commented out

--- my_progress/Section11-Arrays-Bankist/lesson155/script.js
+++ my_progress/Section11-Arrays-Bankist/lesson155/script.js
@@ -328,7 +328,7 @@ console.log(calcAverageHumanAge(data1));
 
 const data2 = [16, 6, 10, 5, 6, 1, 4];
 console.log(calcAverageHumanAge(data2));
-*/
+
 // Teacher's solution
 const calcAverageHumanAge = function (ages) {
   const humanAges = ages.map(age => (age <= 2 ? 2 * age : 16 + age * 4));
@@ -350,3 +350,4 @@ const calcAverageHumanAge = function (ages) {
 const avg1 = calcAverageHumanAge([5, 2, 4, 1, 15, 8, 3]);
 const avg2 = calcAverageHumanAge([16, 6, 10, 5, 6, 1, 4]);
 console.log(avg1, avg2);
+*/
