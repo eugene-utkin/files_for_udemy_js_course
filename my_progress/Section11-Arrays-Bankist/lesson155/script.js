4974bffcb26640094f30650da4f74bdef02ddee6
Evgeniy_Utkin <eugene.utkin@gmail.com>
2022-12-28 20:40:03 +0600

    textContent used

--- my_progress/Section11-Arrays-Bankist/lesson155/script.js
+++ my_progress/Section11-Arrays-Bankist/lesson155/script.js
@@ -103,6 +103,8 @@ const calcDisplaySummary = function (movements) {
     .filter(mov => mov > 0)
     .map(deposit => (deposit * 1.2) / 100)
     .reduce((acc, int) => acc + int, 0);
+
+  labelSumInterest.textContent = `${interest}€`;
 };
 calcDisplaySummary(account1.movements);
 
