04115cbd8ef49bfac6ae11f53e9fa9ddef47fec4
Evgeniy_Utkin <eugene.utkin@gmail.com>
2022-12-28 20:18:09 +0600

    mov <

--- my_progress/Section11-Arrays-Bankist/lesson155/script.js
+++ my_progress/Section11-Arrays-Bankist/lesson155/script.js
@@ -356,7 +356,7 @@ const eurToUsd = 1.1;
 
 // PIPELINE
 const totalDepositsUSD = movements
-  .filter(mov => mov > 0)
+  .filter(mov => mov < 0)
   .map((mov, i, arr) => {
     console.log(arr);
     return mov * eurToUsd;
