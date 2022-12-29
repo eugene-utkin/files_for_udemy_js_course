1596973bfd800a5fac37b74e005f6ab6182663f7
Evgeniy_Utkin <eugene.utkin@gmail.com>
2022-12-29 16:52:18 +0600

    another filter added

--- my_progress/Section11-Arrays-Bankist/lesson155/script.js
+++ my_progress/Section11-Arrays-Bankist/lesson155/script.js
@@ -102,6 +102,7 @@ const calcDisplaySummary = function (movements) {
   const interest = movements
     .filter(mov => mov > 0)
     .map(deposit => (deposit * 1.2) / 100)
+    .filter((int, i, arr) => {})
     .reduce((acc, int) => acc + int, 0);
   labelSumInterest.textContent = `${interest}€`;
 };
