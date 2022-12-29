7d25b6402e0a14e53319ce7e1745594e36039f41
Evgeniy_Utkin <eugene.utkin@gmail.com>
2022-12-29 17:51:33 +0600

    refactored with arrow function

--- my_progress/Section11-Arrays-Bankist/lesson156/script.js
+++ my_progress/Section11-Arrays-Bankist/lesson156/script.js
@@ -391,8 +391,8 @@ console.log(totalDepositsUSD);
 
 // Coding Challenge #3
 // My solution
-const calcAverageHumanAge = function (ages) {
-  const average = ages
+const calcAverageHumanAge = ages => {
+  ages
     .map(age => (age <= 2 ? 2 * age : 16 + age * 4))
     .filter(age => age >= 18)
     .reduce((acc, age, i, arr) => acc + age / arr.length, 0);
