3d6ab3e71482a412d9d1e06ee84e434b011f1b64
Evgeniy_Utkin <eugene.utkin@gmail.com>
2022-12-30 20:41:14 +0600

    reduce added to arrow function

--- my_progress/Section11-Arrays-Bankist/lesson156/script.js
+++ my_progress/Section11-Arrays-Bankist/lesson156/script.js
@@ -418,7 +418,10 @@ const calcAverageHumanAge2 = function (ages) {
 };
 
 const calcAverageHumanAge = ages =>
-  ages.map(age => (age <= 2 ? 2 * age : 16 + age * 4)).filter(age => age >= 18);
+  ages
+    .map(age => (age <= 2 ? 2 * age : 16 + age * 4))
+    .filter(age => age >= 18)
+    .reduce((acc, age, i, arr) => acc + age / arr.length, 0);
 
 const avg1 = calcAverageHumanAge([5, 2, 4, 1, 15, 8, 3]);
 const avg2 = calcAverageHumanAge([16, 6, 10, 5, 6, 1, 4]);
