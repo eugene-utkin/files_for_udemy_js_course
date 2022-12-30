ed75476a27b869c0c6db62ba0c10d5a85d4cc615
Evgeniy_Utkin <eugene.utkin@gmail.com>
2022-12-30 20:39:57 +0600

    filter added to arrow function

--- my_progress/Section11-Arrays-Bankist/lesson156/script.js
+++ my_progress/Section11-Arrays-Bankist/lesson156/script.js
@@ -418,7 +418,7 @@ const calcAverageHumanAge = function (ages) {
 };
 
 const calcAverageHumanAge = ages =>
-  ages.map(age => (age <= 2 ? 2 * age : 16 + age * 4));
+  ages.map(age => (age <= 2 ? 2 * age : 16 + age * 4)).filter(age => age >= 18);
 
 const avg1 = calcAverageHumanAge([5, 2, 4, 1, 15, 8, 3]);
 const avg2 = calcAverageHumanAge([16, 6, 10, 5, 6, 1, 4]);
