f4dda52338ece54f8e7abe0025d9dd970cf9cc92
Evgeniy_Utkin <eugene.utkin@gmail.com>
2022-12-30 20:39:14 +0600

    map added to arrow function

--- my_progress/Section11-Arrays-Bankist/lesson156/script.js
+++ my_progress/Section11-Arrays-Bankist/lesson156/script.js
@@ -416,6 +416,10 @@ const calcAverageHumanAge = function (ages) {
 
   return average;
 };
+
+const calcAverageHumanAge = ages =>
+  ages.map(age => (age <= 2 ? 2 * age : 16 + age * 4));
+
 const avg1 = calcAverageHumanAge([5, 2, 4, 1, 15, 8, 3]);
 const avg2 = calcAverageHumanAge([16, 6, 10, 5, 6, 1, 4]);
 console.log(avg1, avg2);
