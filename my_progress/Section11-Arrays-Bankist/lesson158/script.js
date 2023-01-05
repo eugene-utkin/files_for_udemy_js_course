7acba09b7dc973ad0e091772d5ff99c401c267fe
Evgeniy_Utkin <eugene.utkin@gmail.com>
2023-01-05 12:35:12 +0600

    blur used

--- my_progress/Section11-Arrays-Bankist/lesson158/script.js
+++ my_progress/Section11-Arrays-Bankist/lesson158/script.js
@@ -140,6 +140,7 @@ btnLogin.addEventListener('click', function (e) {
 
     // Clear input fields
     inputLoginUsername.value = inputLoginPin.value = '';
+    inputLoginPin.blur();
 
     // Display movements
     displayMovements(currentAccount.movements);
