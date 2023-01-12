378d433b947114201c957e74701fef5750bd7298
Evgeniy_Utkin <eugene.utkin@gmail.com>
2023-01-12 19:35:36 +0600

    findIndex used

--- my_progress/Section11-Arrays-Bankist/lesson160/script.js
+++ my_progress/Section11-Arrays-Bankist/lesson160/script.js
@@ -197,6 +197,9 @@ btnClose.addEventListener('click', function (e) {
     inputCloseUsername.value === currentAccount.username &&
     Number(inputClosePin.value) === currentAccount.pin
   ) {
+    const index = accounts.findIndex(
+      acc => acc.username === currentAccount.username
+    );
     accounts.splice(index, 1);
   }
 });
