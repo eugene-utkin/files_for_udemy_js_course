c80be5f594c6f21c8dcb3655015acbe343dea29c
Evgeniy_Utkin <eugene.utkin@gmail.com>
2023-01-14 18:29:05 +0600

    inputTransferAmount emptied

--- my_progress/Section11-Arrays-Bankist/lesson160/script.js
+++ my_progress/Section11-Arrays-Bankist/lesson160/script.js
@@ -183,7 +183,7 @@ btnTransfer.addEventListener('click', function (e) {
 
 btnClose.addEventListener('click', function (e) {
   e.preventDefault();
-  inputTransferTo.value = '';
+  inputTransferAmount.value = inputTransferTo.value = '';
 
   // My solution
   // const closingAcc = accounts.find(
