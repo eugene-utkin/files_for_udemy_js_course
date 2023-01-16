05101ee224d6f58eae5e8e65bb49d7dba48973fb
Evgeniy_Utkin <eugene.utkin@gmail.com>
2023-01-16 19:43:24 +0600

    another condition example added

--- my_progress/Section11-Arrays-Bankist/lesson161/script.js
+++ my_progress/Section11-Arrays-Bankist/lesson161/script.js
@@ -545,5 +545,7 @@ console.log(movements);
 console.log(movements.includes(-130));
 
 // CONDITION
+console.log(movements.some(mov => mov === -130));
+
 const anyDeposits = movements.some(mov => mov > 1500);
 console.log(anyDeposits);
