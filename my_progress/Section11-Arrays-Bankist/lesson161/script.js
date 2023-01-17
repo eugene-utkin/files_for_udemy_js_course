17786b16b252a8a936f9a013de0544dc32000c04
Evgeniy_Utkin <eugene.utkin@gmail.com>
2023-01-17 19:22:54 +0600

    const amount added

--- my_progress/Section11-Arrays-Bankist/lesson161/script.js
+++ my_progress/Section11-Arrays-Bankist/lesson161/script.js
@@ -183,6 +183,8 @@ btnTransfer.addEventListener('click', function (e) {
 
 btnLoan.addEventListener('click', function (e) {
   e.preventDefault();
+
+  const amount = Number(inputLoanAmount.value);
 });
 
 btnClose.addEventListener('click', function (e) {
