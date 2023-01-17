beb1bdd6e429cb9e5d4460f4e73e2772fa2be2fc
Evgeniy_Utkin <eugene.utkin@gmail.com>
2023-01-17 19:30:11 +0600

    Update UI comment added

--- my_progress/Section11-Arrays-Bankist/lesson161/script.js
+++ my_progress/Section11-Arrays-Bankist/lesson161/script.js
@@ -189,6 +189,8 @@ btnLoan.addEventListener('click', function (e) {
   if (amount > 0 && currentAccount.movements.some(mov => mov >= amount * 0.1)) {
     // Add movement
     currentAccount.movements.push(amount);
+
+    // Update UI
   }
 });
 
