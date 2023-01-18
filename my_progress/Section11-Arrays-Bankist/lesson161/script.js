ad697aaa83a1a0347ea52c041ea4e9163d7d848a
Evgeniy_Utkin <eugene.utkin@gmail.com>
2023-01-18 19:29:25 +0600

    SOME comment added

--- my_progress/Section11-Arrays-Bankist/lesson161/script.js
+++ my_progress/Section11-Arrays-Bankist/lesson161/script.js
@@ -559,7 +559,7 @@ console.log(movements);
 // EQUALITY
 console.log(movements.includes(-130));
 
-// CONDITION
+// SOME: CONDITION
 console.log(movements.some(mov => mov === -130));
 
 const anyDeposits = movements.some(mov => mov > 1500);
