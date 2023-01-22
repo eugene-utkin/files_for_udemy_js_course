da619928eda15df6fcf9a4fce8896172c12dd6f1
Evgeniy_Utkin <eugene.utkin@gmail.com>
2023-01-22 18:20:57 +0600

    flat comment added

--- my_progress/Section11-Arrays-Bankist/lesson162/script.js
+++ my_progress/Section11-Arrays-Bankist/lesson162/script.js
@@ -583,6 +583,7 @@ console.log(arr.flat());
 const arrDeep = [[[1, 2], 3], [4, [5, 6]], 7, 8];
 console.log(arrDeep.flat(2));
 
+// flat
 const overalBalance = accounts
   .map(acc => acc.movements)
   .flat()
