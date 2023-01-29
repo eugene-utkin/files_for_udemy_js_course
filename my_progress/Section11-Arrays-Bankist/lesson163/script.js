2a0c1a1db6214d625a122a118a7eb09fc8e2e475
Evgeniy_Utkin <eugene.utkin@gmail.com>
2023-01-29 11:23:10 +0600

    sorted reassigned

--- my_progress/Section11-Arrays-Bankist/lesson163/script.js
+++ my_progress/Section11-Arrays-Bankist/lesson163/script.js
@@ -234,6 +234,7 @@ let sorted = false;
 btnSort.addEventListener('click', function (e) {
   e.preventDefault();
   displayMovements(currentAccount.movements, !sorted);
+  sorted = !sorted;
 });
 
 /////////////////////////////////////////////////
