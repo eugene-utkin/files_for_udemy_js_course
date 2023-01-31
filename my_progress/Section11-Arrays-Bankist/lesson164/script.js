c1577aadf6fe46e8e0b2b93d09ea58030e73932d
Evgeniy_Utkin <eugene.utkin@gmail.com>
2023-01-31 21:29:41 +0600

    const arr added

--- my_progress/Section11-Arrays-Bankist/lesson164/script.js
+++ my_progress/Section11-Arrays-Bankist/lesson164/script.js
@@ -634,7 +634,7 @@ movements.sort((a, b) => b - a);
 console.log(movements);
 */
 
-console.log([1, 2, 3, 4, 5, 6, 7]);
+const arr = [1, 2, 3, 4, 5, 6, 7];
 console.log(new Array(1, 2, 3, 4, 5, 6, 7));
 
 const x = new Array(7);
