bbf8ea56c01bc65ef310c3f7910bf236a92919b7
Evgeniy_Utkin <eugene.utkin@gmail.com>
2023-02-01 23:28:59 +0600

    old code uncommented

--- my_progress/Section11-Arrays-Bankist/lesson164/script.js
+++ my_progress/Section11-Arrays-Bankist/lesson164/script.js
@@ -641,8 +641,8 @@ console.log(new Array(1, 2, 3, 4, 5, 6, 7));
 const x = new Array(7);
 console.log(x);
 // console.log(x.map(() => 5));
-// x.fill(1);
 x.fill(1, 3, 5);
+x.fill(1);
 console.log(x);
 
 arr.fill(23, 2, 6);
