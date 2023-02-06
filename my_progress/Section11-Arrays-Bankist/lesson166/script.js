f53bcd84d5238a3a15cb8546c3b1bbb9adb248d9
Evgeniy_Utkin <eugene.utkin@gmail.com>
2023-02-06 21:15:18 +0600

    first variant commented out

--- my_progress/Section11-Arrays-Bankist/lesson166/script.js
+++ my_progress/Section11-Arrays-Bankist/lesson166/script.js
@@ -683,8 +683,8 @@ const bankDepositSum = accounts
 console.log(bankDepositSum);
 
 // 2.
-const numDeposits1000 = accounts
-  .flatMap(acc => acc.movements)
-  .filter(mov => mov >= 1000).length;
+// const numDeposits1000 = accounts
+//   .flatMap(acc => acc.movements)
+//   .filter(mov => mov >= 1000).length;
 
 console.log(numDeposits1000);
