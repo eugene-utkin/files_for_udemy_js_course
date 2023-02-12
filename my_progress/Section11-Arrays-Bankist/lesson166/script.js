9ec78c4cdb212d08bb2f4dcf9669cd6ab8454049
Evgeniy_Utkin <eugene.utkin@gmail.com>
2023-02-12 19:21:10 +0600

    map and toUpperCase used

--- my_progress/Section11-Arrays-Bankist/lesson166/script.js
+++ my_progress/Section11-Arrays-Bankist/lesson166/script.js
@@ -717,7 +717,10 @@ console.log(deposits, withdrawals);
 const convertTitleCase = function (title) {
   const exceptions = ['a', 'an', 'the', 'but', 'or', 'on', 'in', 'with'];
 
-  const titleCase = title.toLowerCase().split(' ');
+  const titleCase = title
+    .toLowerCase()
+    .split(' ')
+    .map(word => word[0].toUpperCase() + word.slice(1));
   return titleCase;
 };
 console.log(convertTitleCase('this is a nice title'));
