32ec865d3ad3d1803f65a2814984a6400c91fb3f
Evgeniy_Utkin <eugene.utkin@gmail.com>
2023-02-12 19:23:12 +0600

    includes and ternary operator used

--- my_progress/Section11-Arrays-Bankist/lesson166/script.js
+++ my_progress/Section11-Arrays-Bankist/lesson166/script.js
@@ -720,7 +720,9 @@ const convertTitleCase = function (title) {
   const titleCase = title
     .toLowerCase()
     .split(' ')
-    .map(word => word[0].toUpperCase() + word.slice(1));
+    .map(word =>
+      exceptions.includes(word) ? word : word[0].toUpperCase() + word.slice(1)
+    );
   return titleCase;
 };
 console.log(convertTitleCase('this is a nice title'));
