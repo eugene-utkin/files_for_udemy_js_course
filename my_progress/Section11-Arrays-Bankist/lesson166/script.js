cf7375d81cea62d67fe6297d98f3b2e9bac92410
Evgeniy_Utkin <eugene.utkin@gmail.com>
2023-02-12 19:25:47 +0600

    'and' added to exceptions

--- my_progress/Section11-Arrays-Bankist/lesson166/script.js
+++ my_progress/Section11-Arrays-Bankist/lesson166/script.js
@@ -715,7 +715,7 @@ console.log(deposits, withdrawals);
 // 4.
 // this is a nice title -> This Is a Nice Title
 const convertTitleCase = function (title) {
-  const exceptions = ['a', 'an', 'the', 'but', 'or', 'on', 'in', 'with'];
+  const exceptions = ['a', 'an', 'and', 'the', 'but', 'or', 'on', 'in', 'with'];
 
   const titleCase = title
     .toLowerCase()
