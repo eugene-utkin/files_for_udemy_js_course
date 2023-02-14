7005931fc18ecf7ec626a68130775892c91ab6fd
Evgeniy_Utkin <eugene.utkin@gmail.com>
2023-02-14 12:40:45 +0600

    const dogs added

--- my_progress/Section11-Arrays-Bankist/lesson167/script.js
+++ my_progress/Section11-Arrays-Bankist/lesson167/script.js
@@ -732,3 +732,9 @@ console.log(convertTitleCase('and here is another title with an EXAMPLE'));
 */
 
 // Coding Challenge #4
+const dogs = [
+  { weight: 22, curFood: 250, owners: ['Alice', 'Bob'] },
+  { weight: 8, curFood: 200, owners: ['Matilda'] },
+  { weight: 13, curFood: 275, owners: ['Sarah', 'John'] },
+  { weight: 32, curFood: 340, owners: ['Michael'] },
+];
