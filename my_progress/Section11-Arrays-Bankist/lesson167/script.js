1f475aaa70536602e534145adff5691844ad812e
Evgeniy_Utkin <eugene.utkin@gmail.com>
2023-02-14 13:01:52 +0600

    1. comment added

--- my_progress/Section11-Arrays-Bankist/lesson167/script.js
+++ my_progress/Section11-Arrays-Bankist/lesson167/script.js
@@ -738,6 +738,8 @@ const dogs = [
   { weight: 13, curFood: 275, owners: ['Sarah', 'John'] },
   { weight: 32, curFood: 340, owners: ['Michael'] },
 ];
+
+// 1.
 dogs.map(dog => {
   dog['recommendedFood'] = dog.weight ** 0.75 * 28;
 });
