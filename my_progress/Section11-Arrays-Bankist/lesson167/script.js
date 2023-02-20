75769bc26bc0d7036040037c001d897384dc2646
Evgeniy_Utkin <eugene.utkin@gmail.com>
2023-02-20 18:21:49 +0600

    console log added

--- my_progress/Section11-Arrays-Bankist/lesson167/script.js
+++ my_progress/Section11-Arrays-Bankist/lesson167/script.js
@@ -815,3 +815,8 @@ console.log(dogs);
 // 2.
 const dogSarah = dogs.find(dog => dog.owners.includes('Sarah'));
 console.log(dogSarah);
+console.log(
+  `Sarah's dog is eating too ${
+    dogSarah.curFood > dogSarah.recFood ? 'much' : 'little'
+  }`
+);
