43c9c72b9f530579551d351ba0c021644ebea91c
Evgeniy_Utkin <eugene.utkin@gmail.com>
2023-02-22 11:22:02 +0600

    comment with example added

--- my_progress/Section11-Arrays-Bankist/lesson167/script.js
+++ my_progress/Section11-Arrays-Bankist/lesson167/script.js
@@ -844,6 +844,7 @@ console.log(`${ownersEatTooLittle.join(' and ')}'s dogs eat too little!`);
 console.log(dogs.some(dog => dog.curFood === dog.recFood));
 
 // 6.
+// current > (recommended * 0.90) && current < (recommended * 1.10)
 console.log(
   dogs.some(
     dog => dog.curFood > dog.recFood * 0.9 && dog.curFood < dog.recFood * 1.1
