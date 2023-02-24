5d3abb8335512d75fbfc7597622680b3654e48da
Evgeniy_Utkin <eugene.utkin@gmail.com>
2023-02-24 21:03:03 +0600

    3/10 comment added

--- my_progress/Section12-Numbers-Dates-Timers-Bankist/lesson170/script.js
+++ my_progress/Section12-Numbers-Dates-Timers-Bankist/lesson170/script.js
@@ -254,6 +254,6 @@ btnSort.addEventListener('click', function (e) {
 
 console.log(23 === 23.0);
 
-// Base 10 - 0 to 9. 1/10 = 0.1
+// Base 10 - 0 to 9. 1/10 = 0.1. 3/10 = 3.3333333
 // Binary base 2 - 0 1
 console.log(0.1 + 0.2);
