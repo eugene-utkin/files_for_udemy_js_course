1efd1f4483eb5b525ea8b412565b5f20000bf253
Evgeniy_Utkin <eugene.utkin@gmail.com>
2023-02-25 18:53:31 +0600

    Number replaced with +

--- my_progress/Section12-Numbers-Dates-Timers-Bankist/lesson170/script.js
+++ my_progress/Section12-Numbers-Dates-Timers-Bankist/lesson170/script.js
@@ -223,7 +223,7 @@ btnClose.addEventListener('click', function (e) {
 
   if (
     inputCloseUsername.value === currentAccount.username &&
-    Number(inputClosePin.value) === currentAccount.pin
+    +inputClosePin.value === currentAccount.pin
   ) {
     const index = accounts.findIndex(
       acc => acc.username === currentAccount.username
