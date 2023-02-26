dccb6439e4c6c374f80e16d0f42685e1e79bc6d4
Evgeniy_Utkin <eugene.utkin@gmail.com>
2023-02-26 18:40:58 +0600

    fourth isNaN example

--- my_progress/Section12-Numbers-Dates-Timers-Bankist/lesson170/script.js
+++ my_progress/Section12-Numbers-Dates-Timers-Bankist/lesson170/script.js
@@ -275,3 +275,4 @@ console.log(Number.parseFloat('  2.5rem  '));
 console.log(Number.isNaN(20));
 console.log(Number.isNaN('20'));
 console.log(Number.isNaN(+'20X'));
+console.log(Number.isNaN(20 / 0));
