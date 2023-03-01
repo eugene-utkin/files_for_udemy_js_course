58792fb56e88dd1ac5329de5ab782c8001be851a
Evgeniy_Utkin <eugene.utkin@gmail.com>
2023-03-01 18:55:54 +0600

    Math.min example

--- my_progress/Section12-Numbers-Dates-Timers-Bankist/lesson171/script.js
+++ my_progress/Section12-Numbers-Dates-Timers-Bankist/lesson171/script.js
@@ -297,3 +297,5 @@ console.log(8 ** (1 / 3));
 console.log(Math.max(5, 18, 23, 11, 2));
 console.log(Math.max(5, 18, '23', 11, 2));
 console.log(Math.max(5, 18, '23px', 11, 2));
+
+console.log(Math.min(5, 18, 23, 11, 2));
