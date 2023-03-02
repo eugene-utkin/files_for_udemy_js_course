fa41d0f85ea186deb33c36b8c095a59c72ebe760
Evgeniy_Utkin <eugene.utkin@gmail.com>
2023-03-02 19:47:29 +0600

    previous example commented out

--- my_progress/Section12-Numbers-Dates-Timers-Bankist/lesson171/script.js
+++ my_progress/Section12-Numbers-Dates-Timers-Bankist/lesson171/script.js
@@ -307,7 +307,7 @@ console.log(Math.trunc(Math.random() * 6) + 1);
 const randomInt = (min, max) =>
   Math.trunc(Math.random() * (max - min) + 1) + min;
 // 0...1 -> 0...(max - min) -> min...max
-console.log(randomInt(10, 20));
+// console.log(randomInt(10, 20));
 
 // Rounding integers
 console.log(Math.trunc(23.3));
