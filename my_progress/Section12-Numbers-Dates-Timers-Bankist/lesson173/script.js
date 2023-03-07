62d20370fc00920d5f869658107f6420f240eded
Evgeniy_Utkin <eugene.utkin@gmail.com>
2023-03-07 20:08:52 +0600

    priceCents renamed

--- my_progress/Section12-Numbers-Dates-Timers-Bankist/lesson173/script.js
+++ my_progress/Section12-Numbers-Dates-Timers-Bankist/lesson173/script.js
@@ -364,5 +364,5 @@ labelBalance.addEventListener('click', function () {
 const diameter = 287_460_000_000;
 console.log(diameter);
 
-const priceCents = 345_99;
-console.log(priceCents);
+const price = 345_99;
+console.log(price);
