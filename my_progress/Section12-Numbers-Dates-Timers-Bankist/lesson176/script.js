2eeb41fcc22b787248d0bcbc1c751f6cef2db815
Evgeniy_Utkin <eugene.utkin@gmail.com>
2023-03-17 14:38:38 +0600

    padStart used on date

--- my_progress/Section12-Numbers-Dates-Timers-Bankist/lesson176/script.js
+++ my_progress/Section12-Numbers-Dates-Timers-Bankist/lesson176/script.js
@@ -161,7 +161,7 @@ updateUI(currentAccount);
 containerApp.style.opacity = 100;
 
 const now = new Date();
-const day = now.getDate();
+const day = `${now.getDate()}`.padStart(2, 0);
 const month = now.getMonth() + 1;
 const year = now.getFullYear();
 const hour = now.getHours();
