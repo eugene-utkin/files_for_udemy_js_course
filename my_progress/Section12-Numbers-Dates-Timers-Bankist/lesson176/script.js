3d74695639cdf8cc99700bb25dc37dc53433bc22
Evgeniy_Utkin <eugene.utkin@gmail.com>
2023-03-18 20:48:07 +0600

    movements__date div added to movements html

--- my_progress/Section12-Numbers-Dates-Timers-Bankist/lesson176/script.js
+++ my_progress/Section12-Numbers-Dates-Timers-Bankist/lesson176/script.js
@@ -96,6 +96,7 @@ const displayMovements = function (acc, sort = false) {
         <div class="movements__type movements__type--${type}">${
       i + 1
     } ${type}</div>
+        <div class="movements__date">${displayDate}€</div>
         <div class="movements__value">${mov.toFixed(2)}€</div>
       </div>
     `;
