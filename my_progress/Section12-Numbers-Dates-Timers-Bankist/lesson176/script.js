f7b6782a4d87ce29e510693077720a68f8d8bda2
Evgeniy_Utkin <eugene.utkin@gmail.com>
2023-03-18 20:29:01 +0600

    padStart used on month

--- my_progress/Section12-Numbers-Dates-Timers-Bankist/lesson176/script.js
+++ my_progress/Section12-Numbers-Dates-Timers-Bankist/lesson176/script.js
@@ -162,7 +162,7 @@ containerApp.style.opacity = 100;
 
 const now = new Date();
 const day = `${now.getDate()}`.padStart(2, 0);
-const month = now.getMonth() + 1;
+const month = `${now.getMonth() + 1}`.padStart(2, 0);
 const year = now.getFullYear();
 const hour = now.getHours();
 const min = now.getMinutes();
