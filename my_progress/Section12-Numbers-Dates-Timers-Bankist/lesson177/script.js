f9a626eda024576f842608070caeaeab5be6ed2b
Evgeniy_Utkin <eugene.utkin@gmail.com>
2023-03-21 22:28:47 +0600

    daysPassed function renamed

--- my_progress/Section12-Numbers-Dates-Timers-Bankist/lesson177/script.js
+++ my_progress/Section12-Numbers-Dates-Timers-Bankist/lesson177/script.js
@@ -483,4 +483,4 @@ console.log(future);
 const future = new Date(2037, 10, 19, 15, 23);
 console.log(+future);
 
-const daysPassed = (date1, date2) => date2 - date1;
+const calcDaysPassed = (date1, date2) => date2 - date1;
