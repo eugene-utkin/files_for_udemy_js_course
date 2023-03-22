48c672da33874be3bd5ebd4485bfbdb3a282d91e
Evgeniy_Utkin <eugene.utkin@gmail.com>
2023-03-22 22:08:05 +0600

    dates changed

--- my_progress/Section12-Numbers-Dates-Timers-Bankist/lesson177/script.js
+++ my_progress/Section12-Numbers-Dates-Timers-Bankist/lesson177/script.js
@@ -486,5 +486,5 @@ console.log(+future);
 const calcDaysPassed = (date1, date2) =>
   Math.abs(date2 - date1) / (1000 * 60 * 60 * 24);
 
-const days1 = calcDaysPassed(new Date(2037, 3, 14), new Date(2037, 3, 24));
+const days1 = calcDaysPassed(new Date(2037, 3, 4), new Date(2037, 3, 14));
 console.log(days1);
