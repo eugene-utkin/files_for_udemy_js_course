0f73e8fe8356760fd6ccb59dcb94445bf3dac8de
Evgeniy_Utkin <eugene.utkin@gmail.com>
2023-03-24 22:12:52 +0600

    else logic added

--- my_progress/Section12-Numbers-Dates-Timers-Bankist/lesson177/script.js
+++ my_progress/Section12-Numbers-Dates-Timers-Bankist/lesson177/script.js
@@ -91,12 +91,12 @@ const formatMovementDate = function (date) {
   if (daysPassed === 0) return 'Today';
   if (daysPassed === 1) return 'Yesterday';
   if (daysPassed <= 7) return `${daysPassed} days ago`;
-
-  const day = `${date.getDate()}`.padStart(2, 0);
-  const month = `${date.getMonth() + 1}`.padStart(2, 0);
-  const year = date.getFullYear();
-
-  return `${day}/${month}/${year}`;
+  else {
+    const day = `${date.getDate()}`.padStart(2, 0);
+    const month = `${date.getMonth() + 1}`.padStart(2, 0);
+    const year = date.getFullYear();
+    return `${day}/${month}/${year}`;
+  }
 };
 
 const displayMovements = function (acc, sort = false) {
