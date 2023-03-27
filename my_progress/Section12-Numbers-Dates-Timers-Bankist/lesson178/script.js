0682ffda63809753836d1d03f5406d7a74c92206
Evgeniy_Utkin <eugene.utkin@gmail.com>
2023-03-27 21:51:43 +0600

    locale from currentAccount used

--- my_progress/Section12-Numbers-Dates-Timers-Bankist/lesson178/script.js
+++ my_progress/Section12-Numbers-Dates-Timers-Bankist/lesson178/script.js
@@ -208,12 +208,13 @@ btnLogin.addEventListener('click', function (e) {
       year: 'numeric',
       weekday: 'long',
     };
-    const locale = navigator.language;
-    console.log(locale);
+    // const locale = navigator.language;
+    // console.log(locale);
 
-    labelDate.textContent = new Intl.DateTimeFormat(locale, options).format(
-      now
-    );
+    labelDate.textContent = new Intl.DateTimeFormat(
+      currentAccount.locale,
+      options
+    ).format(now);
 
     // const day = `${now.getDate()}`.padStart(2, 0);
     // const month = `${now.getMonth() + 1}`.padStart(2, 0);
