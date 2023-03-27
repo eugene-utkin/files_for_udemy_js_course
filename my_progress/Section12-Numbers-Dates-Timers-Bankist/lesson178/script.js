ee7419758ba9f9623784260d017dcb74175ef84d
Evgeniy_Utkin <eugene.utkin@gmail.com>
2023-03-27 21:56:30 +0600

    old date logic commented out

--- my_progress/Section12-Numbers-Dates-Timers-Bankist/lesson178/script.js
+++ my_progress/Section12-Numbers-Dates-Timers-Bankist/lesson178/script.js
@@ -91,10 +91,10 @@ const formatMovementDate = function (date) {
   if (daysPassed === 0) return 'Today';
   if (daysPassed === 1) return 'Yesterday';
   if (daysPassed <= 7) return `${daysPassed} days ago`;
-  const day = `${date.getDate()}`.padStart(2, 0);
-  const month = `${date.getMonth() + 1}`.padStart(2, 0);
-  const year = date.getFullYear();
-  return `${day}/${month}/${year}`;
+  // const day = `${date.getDate()}`.padStart(2, 0);
+  // const month = `${date.getMonth() + 1}`.padStart(2, 0);
+  // const year = date.getFullYear();
+  // return `${day}/${month}/${year}`;
 };
 
 const displayMovements = function (acc, sort = false) {
