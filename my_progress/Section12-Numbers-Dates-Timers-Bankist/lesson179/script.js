99eab1b179438a2f868e274390ef6a33a75a3854
Evgeniy_Utkin <eugene.utkin@gmail.com>
2023-03-29 23:15:24 +0600

    added example for Syria

--- my_progress/Section12-Numbers-Dates-Timers-Bankist/lesson179/script.js
+++ my_progress/Section12-Numbers-Dates-Timers-Bankist/lesson179/script.js
@@ -523,3 +523,4 @@ const num = 3884764.23;
 
 console.log('US:      ', new Intl.NumberFormat('en-US').format(num));
 console.log('Germany: ', new Intl.NumberFormat('de-DE').format(num));
+console.log('Syria: ', new Intl.NumberFormat('ar-SY').format(num));
