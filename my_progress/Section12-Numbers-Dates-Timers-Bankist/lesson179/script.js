f83c15d4e36aacc8b0c8bfa01bd0067674be3810
Evgeniy_Utkin <eugene.utkin@gmail.com>
2023-03-30 22:36:38 +0600

    navigator.language used

--- my_progress/Section12-Numbers-Dates-Timers-Bankist/lesson179/script.js
+++ my_progress/Section12-Numbers-Dates-Timers-Bankist/lesson179/script.js
@@ -524,4 +524,7 @@ const num = 3884764.23;
 console.log('US:      ', new Intl.NumberFormat('en-US').format(num));
 console.log('Germany: ', new Intl.NumberFormat('de-DE').format(num));
 console.log('Syria: ', new Intl.NumberFormat('ar-SY').format(num));
-console.log('Browser: ', new Intl.NumberFormat(navigator.language).format(num));
+console.log(
+  navigator.language,
+  new Intl.NumberFormat(navigator.language).format(num)
+);
