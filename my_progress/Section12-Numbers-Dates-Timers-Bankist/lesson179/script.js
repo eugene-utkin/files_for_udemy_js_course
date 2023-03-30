0112ef724b233b864115a6aae5548f6ff0614c55
Evgeniy_Utkin <eugene.utkin@gmail.com>
2023-03-30 23:19:52 +0600

    options used

--- my_progress/Section12-Numbers-Dates-Timers-Bankist/lesson179/script.js
+++ my_progress/Section12-Numbers-Dates-Timers-Bankist/lesson179/script.js
@@ -526,10 +526,10 @@ const options = {
   unit: 'mile-per-hour',
 };
 
-console.log('US:      ', new Intl.NumberFormat('en-US').format(num));
-console.log('Germany: ', new Intl.NumberFormat('de-DE').format(num));
-console.log('Syria:   ', new Intl.NumberFormat('ar-SY').format(num));
+console.log('US:      ', new Intl.NumberFormat('en-US', options).format(num));
+console.log('Germany: ', new Intl.NumberFormat('de-DE', options).format(num));
+console.log('Syria:   ', new Intl.NumberFormat('ar-SY', options).format(num));
 console.log(
   navigator.language,
-  new Intl.NumberFormat(navigator.language).format(num)
+  new Intl.NumberFormat(navigator.language, options).format(num)
 );
