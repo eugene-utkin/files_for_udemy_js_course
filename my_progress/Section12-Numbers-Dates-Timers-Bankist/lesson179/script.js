28076a3e1c0049f250af0dcad4206d591c58a985
Evgeniy_Utkin <eugene.utkin@gmail.com>
2023-03-30 22:37:58 +0600

    const options added

--- my_progress/Section12-Numbers-Dates-Timers-Bankist/lesson179/script.js
+++ my_progress/Section12-Numbers-Dates-Timers-Bankist/lesson179/script.js
@@ -521,6 +521,8 @@ console.log(days1);
 
 const num = 3884764.23;
 
+const options = {};
+
 console.log('US:      ', new Intl.NumberFormat('en-US').format(num));
 console.log('Germany: ', new Intl.NumberFormat('de-DE').format(num));
 console.log('Syria: ', new Intl.NumberFormat('ar-SY').format(num));
