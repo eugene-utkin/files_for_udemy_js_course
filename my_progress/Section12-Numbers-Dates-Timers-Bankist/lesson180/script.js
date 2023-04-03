a05e65c14a02ecf1054cba7b39addbe4c522e149
Evgeniy_Utkin <eugene.utkin@gmail.com>
2023-04-03 22:11:57 +0600

    setTimeout used in btnLoan

--- my_progress/Section12-Numbers-Dates-Timers-Bankist/lesson180/script.js
+++ my_progress/Section12-Numbers-Dates-Timers-Bankist/lesson180/script.js
@@ -275,15 +275,17 @@ btnLoan.addEventListener('click', function (e) {
   const amount = Math.floor(inputLoanAmount.value);
 
   if (amount > 0 && currentAccount.movements.some(mov => mov >= amount * 0.1)) {
-    // Add movement
-    currentAccount.movements.push(amount);
+    setTimeout(function () {
+      // Add movement
+      currentAccount.movements.push(amount);
 
-    // Add loan date
-    // Add transfer date
-    currentAccount.movementsDates.push(new Date().toISOString());
+      // Add loan date
+      // Add transfer date
+      currentAccount.movementsDates.push(new Date().toISOString());
 
-    // Update UI
-    updateUI(currentAccount);
+      // Update UI
+      updateUI(currentAccount);
+    }, 2500);
   }
   inputLoanAmount.value = '';
 });
