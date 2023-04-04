a44e6c1a9cfd55b313ed9ca49a46ce792d300443
Evgeniy_Utkin <eugene.utkin@gmail.com>
2023-04-04 18:31:22 +0600

    setInterval used

--- my_progress/Section12-Numbers-Dates-Timers-Bankist/lesson181/script.js
+++ my_progress/Section12-Numbers-Dates-Timers-Bankist/lesson181/script.js
@@ -187,8 +187,10 @@ const startLogOutTimer = function () {
   // Set time to 5 minutes
   let time = 100;
   // Call the timer every second
-  // In each call, print the remaining time to UI
-  // When 0 seconds, stop timer and log out user
+  setInterval(function () {
+    // In each call, print the remaining time to UI
+    // When 0 seconds, stop timer and log out user
+  }, 1000);
 };
 
 ///////////////////////////////////////
