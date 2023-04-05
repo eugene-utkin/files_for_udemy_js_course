f5b7e3cac7b16969322e94167f5bf21f0c3f53b4
Evgeniy_Utkin <eugene.utkin@gmail.com>
2023-04-05 20:27:25 +0600

    padStart used on min

--- my_progress/Section12-Numbers-Dates-Timers-Bankist/lesson181/script.js
+++ my_progress/Section12-Numbers-Dates-Timers-Bankist/lesson181/script.js
@@ -188,7 +188,7 @@ const startLogOutTimer = function () {
   let time = 100;
   // Call the timer every second
   setInterval(function () {
-    const min = String(Math.trunc(time / 60));
+    const min = String(Math.trunc(time / 60)).padStart(2, 0);
     const sec = time % 60;
 
     // In each call, print the remaining time to UI
