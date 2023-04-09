8aeb666472fab8fa74056a991ab5f676fa7d00b4
Evgeniy_Utkin <eugene.utkin@gmail.com>
2023-04-09 22:51:20 +0600

    button code finished

--- my_progress/Section13-Advanced-DOM-Bankist/lesson186/script.js
+++ my_progress/Section13-Advanced-DOM-Bankist/lesson186/script.js
@@ -55,4 +55,4 @@ message.classList.add('cookie-message');
 // message.textContent =
 //   'We use cookies for inproved functionality and analytics.';
 message.innerHTML =
-  'We use cookies for inproved functionality and analytics. <button class="btn btn--close-cookie"';
+  'We use cookies for inproved functionality and analytics. <button class="btn btn--close-cookie">Got it!</button>';
