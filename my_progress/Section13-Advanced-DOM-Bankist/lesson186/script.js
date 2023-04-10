c9bc49956232338b3008bd19fdc8cf16b2274e78
Evgeniy_Utkin <eugene.utkin@gmail.com>
2023-04-10 19:44:02 +0600

    extra code commented out

--- my_progress/Section13-Advanced-DOM-Bankist/lesson186/script.js
+++ my_progress/Section13-Advanced-DOM-Bankist/lesson186/script.js
@@ -61,5 +61,5 @@ message.innerHTML =
 header.append(message);
 // header.append(message.cloneNode(true));
 
-header.before(message);
-header.after(message);
+// header.before(message);
+// header.after(message);
