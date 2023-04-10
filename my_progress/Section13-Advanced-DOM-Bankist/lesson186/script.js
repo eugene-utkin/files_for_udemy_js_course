b49bad8addc2301e38bd62b1c7344d676b30bf9b
Evgeniy_Utkin <eugene.utkin@gmail.com>
2023-04-10 19:09:03 +0600

    const header added

--- my_progress/Section13-Advanced-DOM-Bankist/lesson186/script.js
+++ my_progress/Section13-Advanced-DOM-Bankist/lesson186/script.js
@@ -39,7 +39,7 @@ console.log(document.documentElement);
 console.log(document.head);
 console.log(document.body);
 
-document.querySelector('.header');
+const header = document.querySelector('.header');
 const allSections = document.querySelectorAll('.section');
 console.log(allSections);
 
