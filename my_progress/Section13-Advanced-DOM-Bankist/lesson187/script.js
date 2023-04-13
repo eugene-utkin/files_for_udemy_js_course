ed7c41a115a2625141956295af7c24856e63d9fe
Evgeniy_Utkin <eugene.utkin@gmail.com>
2023-04-13 21:19:15 +0600

    className attribute example

--- my_progress/Section13-Advanced-DOM-Bankist/lesson187/script.js
+++ my_progress/Section13-Advanced-DOM-Bankist/lesson187/script.js
@@ -91,4 +91,5 @@ document.documentElement.style.setProperty('--color-primary', 'orangered');
 const logo = document.querySelector('.nav__logo');
 console.log(logo.alt);
 console.log(logo.src);
+console.log(logo.className);
 console.log(logo.designer);
