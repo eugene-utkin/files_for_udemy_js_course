5b816b8cc1ae61e719bad55fa815ada592883857
Evgeniy_Utkin <eugene.utkin@gmail.com>
2023-04-15 20:12:59 +0600

    c class used in contains example

--- my_progress/Section13-Advanced-DOM-Bankist/lesson187/script.js
+++ my_progress/Section13-Advanced-DOM-Bankist/lesson187/script.js
@@ -113,4 +113,4 @@ console.log(logo.dataset.versionNumber);
 logo.classList.add('c');
 logo.classList.remove('c');
 logo.classList.toggle('c');
-logo.classList.contains();
+logo.classList.contains('c');
