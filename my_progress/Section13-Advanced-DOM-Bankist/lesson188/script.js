10fc3b4b8eaeff52498451dcf38de6de30ccd49a
Evgeniy_Utkin <eugene.utkin@gmail.com>
2023-04-16 21:38:22 +0600

    const s1coords added

--- my_progress/Section13-Advanced-DOM-Bankist/lesson188/script.js
+++ my_progress/Section13-Advanced-DOM-Bankist/lesson188/script.js
@@ -123,4 +123,6 @@ logo.className = 'jonas';
 const btnScrollTo = document.querySelector('.btn--scroll-to');
 const section1 = document.querySelector('#section--1');
 
-btnScrollTo.addEventListener('click', function (e) {});
+btnScrollTo.addEventListener('click', function (e) {
+  const s1coords = section1.getBoundingClientRect();
+});
