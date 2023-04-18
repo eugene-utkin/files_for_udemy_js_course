b0f3b2b5b77f0be5842b94ba9bab0e0caecfa17f
Evgeniy_Utkin <eugene.utkin@gmail.com>
2023-04-18 20:31:38 +0600

    removeEventListener example

--- my_progress/Section13-Advanced-DOM-Bankist/lesson189/script.js
+++ my_progress/Section13-Advanced-DOM-Bankist/lesson189/script.js
@@ -156,6 +156,8 @@ const h1 = document.querySelector('h1');
 
 const alertH1 = function (e) {
   alert('addEventListener: Great! You are reading the heading :D');
+
+  h1.removeEventListener('mouseenter', alertH1);
 };
 
 h1.addEventListener('mouseenter', alertH1);
