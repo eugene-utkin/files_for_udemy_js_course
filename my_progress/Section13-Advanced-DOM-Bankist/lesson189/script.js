6c7aa8957df32bf9f32ca61dd8453989f3080bbc
Evgeniy_Utkin <eugene.utkin@gmail.com>
2023-04-18 20:29:30 +0600

    alertH1 function called

--- my_progress/Section13-Advanced-DOM-Bankist/lesson189/script.js
+++ my_progress/Section13-Advanced-DOM-Bankist/lesson189/script.js
@@ -158,9 +158,7 @@ const alertH1 = function (e) {
   alert('addEventListener: Great! You are reading the heading :D');
 };
 
-h1.addEventListener('mouseenter', function (e) {
-  alert('addEventListener: Great! You are reading the heading :D');
-});
+h1.addEventListener('mouseenter', alertH1);
 
 h1.onmouseenter = function (e) {
   alert('onmouseenter: Great! You are reading the heading :D');
