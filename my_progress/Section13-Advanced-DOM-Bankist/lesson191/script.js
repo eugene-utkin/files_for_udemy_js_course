f45e75515dfc17cc1b5f58e8e23764bcecaf6230
Evgeniy_Utkin <eugene.utkin@gmail.com>
2023-04-20 22:22:43 +0600

    comparison fixed

--- my_progress/Section13-Advanced-DOM-Bankist/lesson191/script.js
+++ my_progress/Section13-Advanced-DOM-Bankist/lesson191/script.js
@@ -177,7 +177,7 @@ const randomColor = () =>
 document.querySelector('.nav__link').addEventListener('click', function (e) {
   this.style.backgroundColor = randomColor();
   console.log('LINK', e.target, e.currentTarget);
-  console.log(e.target === this);
+  console.log(e.currentTarget === this);
 });
 
 document.querySelector('.nav__links').addEventListener('click', function (e) {
