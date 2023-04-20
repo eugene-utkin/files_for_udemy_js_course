83ad01a46803cace418a9c5589bfda7b30f169f0
Evgeniy_Utkin <eugene.utkin@gmail.com>
2023-04-20 22:16:35 +0600

    currentTarget added to second console log

--- my_progress/Section13-Advanced-DOM-Bankist/lesson191/script.js
+++ my_progress/Section13-Advanced-DOM-Bankist/lesson191/script.js
@@ -181,7 +181,7 @@ document.querySelector('.nav__link').addEventListener('click', function (e) {
 
 document.querySelector('.nav__links').addEventListener('click', function (e) {
   this.style.backgroundColor = randomColor();
-  console.log('CONTAINER', e.target);
+  console.log('CONTAINER', e.target, e.currentTarget);
 });
 
 document.querySelector('.nav').addEventListener('click', function (e) {
