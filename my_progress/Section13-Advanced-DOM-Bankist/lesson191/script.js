275a094bdcc7a51b75452301924e9b1696a08f2d
Evgeniy_Utkin <eugene.utkin@gmail.com>
2023-04-21 18:33:08 +0600

    more constants moved

--- my_progress/Section13-Advanced-DOM-Bankist/lesson191/script.js
+++ my_progress/Section13-Advanced-DOM-Bankist/lesson191/script.js
@@ -1,16 +1,14 @@
 'use strict';
 
+const modal = document.querySelector('.modal');
+const overlay = document.querySelector('.overlay');
+const btnCloseModal = document.querySelector('.btn--close-modal');
+const btnsOpenModal = document.querySelectorAll('.btn--show-modal');
 const btnScrollTo = document.querySelector('.btn--scroll-to');
 const section1 = document.querySelector('#section--1');
 
 ///////////////////////////////////////
 // Modal window
-
-const modal = document.querySelector('.modal');
-const overlay = document.querySelector('.overlay');
-const btnCloseModal = document.querySelector('.btn--close-modal');
-const btnsOpenModal = document.querySelectorAll('.btn--show-modal');
-
 const openModal = function (e) {
   e.preventDefault();
   modal.classList.remove('hidden');
