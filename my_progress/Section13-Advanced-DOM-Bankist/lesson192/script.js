f9bd86f0004a01f46bb5fd5ad25abf7e90d7aedc
Evgeniy_Utkin <eugene.utkin@gmail.com>
2023-04-22 11:32:49 +0600

    const id added

--- my_progress/Section13-Advanced-DOM-Bankist/lesson192/script.js
+++ my_progress/Section13-Advanced-DOM-Bankist/lesson192/script.js
@@ -67,6 +67,7 @@ document.querySelectorAll('.nav__link').forEach(function (el) {
   el.addEventListener('click', function (e) {
     e.preventDefault();
     console.log('LINK');
+    const id = this.getAttribute('href');
   });
 });
 
