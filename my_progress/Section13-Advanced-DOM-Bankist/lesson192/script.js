574e59fb1410206c205d8859e5a7a7dfc0a3c903
Evgeniy_Utkin <eugene.utkin@gmail.com>
2023-04-22 12:32:54 +0600

    first variant commented out

--- my_progress/Section13-Advanced-DOM-Bankist/lesson192/script.js
+++ my_progress/Section13-Advanced-DOM-Bankist/lesson192/script.js
@@ -63,14 +63,14 @@ btnScrollTo.addEventListener('click', function (e) {
 
 ////////////////////////////////////////
 // Page navigation
-document.querySelectorAll('.nav__link').forEach(function (el) {
-  el.addEventListener('click', function (e) {
-    e.preventDefault();
-    const id = this.getAttribute('href');
-    console.log(id);
-    document.querySelector(id).scrollIntoView({ behavior: 'smooth' });
-  });
-});
+// document.querySelectorAll('.nav__link').forEach(function (el) {
+//   el.addEventListener('click', function (e) {
+//     e.preventDefault();
+//     const id = this.getAttribute('href');
+//     console.log(id);
+//     document.querySelector(id).scrollIntoView({ behavior: 'smooth' });
+//   });
+// });
 
 ////////////////////////////////////////
 ////////////////////////////////////////
