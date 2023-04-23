7307f328257e964880fad82f8bceb86614a66003
Evgeniy_Utkin <eugene.utkin@gmail.com>
2023-04-23 19:48:10 +0600

    .highlight elements selected

--- my_progress/Section13-Advanced-DOM-Bankist/lesson193/script.js
+++ my_progress/Section13-Advanced-DOM-Bankist/lesson193/script.js
@@ -221,3 +221,4 @@ document.querySelector('.nav').addEventListener('click', function (e) {
 const h1 = document.querySelector('h1');
 
 // Going downwards: child
+console.log(h1.querySelectorAll('.highlight'));
