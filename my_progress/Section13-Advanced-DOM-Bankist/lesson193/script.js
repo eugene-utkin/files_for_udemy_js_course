4bae0932c80e4876adaa743a1d089a64fc83eae0
Evgeniy_Utkin <eugene.utkin@gmail.com>
2023-04-24 15:56:43 +0600

    lastElementChild example

--- my_progress/Section13-Advanced-DOM-Bankist/lesson193/script.js
+++ my_progress/Section13-Advanced-DOM-Bankist/lesson193/script.js
@@ -225,3 +225,4 @@ console.log(h1.querySelectorAll('.highlight'));
 console.log(h1.childNodes);
 console.log(h1.children);
 h1.firstElementChild.style.color = 'white';
+h1.lastElementChild.style.color = 'orangered';
