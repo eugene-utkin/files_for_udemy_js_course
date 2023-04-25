5884b30be1a7093786196bd73cb32c3b0849ae4f
Evgeniy_Utkin <eugene.utkin@gmail.com>
2023-04-25 20:37:37 +0600

    old code commented out

--- my_progress/Section13-Advanced-DOM-Bankist/lesson194/script.js
+++ my_progress/Section13-Advanced-DOM-Bankist/lesson194/script.js
@@ -217,7 +217,7 @@ document.querySelector('.nav').addEventListener('click', function (e) {
   console.log('NAV', e.target, e.currentTarget);
 });
 */
-
+/*
 const h1 = document.querySelector('h1');
 
 // Going downwards: child
@@ -246,3 +246,4 @@ console.log(h1.parentElement.children);
 [...h1.parentElement.children].forEach(function (el) {
   if (el !== h1) el.style.transform = 'scale(0.5)';
 });
+*/
