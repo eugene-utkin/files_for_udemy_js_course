6b271ec875e82137251ffb568104e877ae09e62d
Evgeniy_Utkin <eugene.utkin@gmail.com>
2023-04-27 18:26:01 +0600

    all consts moved

--- my_progress/Section13-Advanced-DOM-Bankist/lesson195/script.js
+++ my_progress/Section13-Advanced-DOM-Bankist/lesson195/script.js
@@ -7,6 +7,9 @@ const btnsOpenModal = document.querySelectorAll('.btn--show-modal');
 const btnScrollTo = document.querySelector('.btn--scroll-to');
 const section1 = document.querySelector('#section--1');
 const nav = document.querySelector('.nav');
+const tabs = document.querySelectorAll('.operations__tab');
+const tabsContainer = document.querySelector('.operations__tab-container');
+const tabsContent = document.querySelectorAll('.operations__content');
 
 ///////////////////////////////////////
 // Modal window
@@ -91,9 +94,6 @@ document.querySelector('.nav__links').addEventListener('click', function (e) {
 ////////////////////////////////////////
 
 // Tabbed component
-const tabs = document.querySelectorAll('.operations__tab');
-const tabsContainer = document.querySelector('.operations__tab-container');
-const tabsContent = document.querySelectorAll('.operations__content');
 
 tabsContainer.addEventListener('click', function (e) {
   const clicked = e.target.closest('.operations__tab');
