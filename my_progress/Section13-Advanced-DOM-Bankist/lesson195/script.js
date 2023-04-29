c130c336377cf6972d6989aa8eb5a4d4facf6797
Evgeniy_Utkin <eugene.utkin@gmail.com>
2023-04-29 17:56:33 +0600

    opacity parameter added to handleHover function

--- my_progress/Section13-Advanced-DOM-Bankist/lesson195/script.js
+++ my_progress/Section13-Advanced-DOM-Bankist/lesson195/script.js
@@ -115,16 +115,16 @@ tabsContainer.addEventListener('click', function (e) {
 });
 
 // Menu fade animation
-const handleHover = function () {
+const handleHover = function (e, opacity) {
   if (e.target.classList.contains('nav__link')) {
     const link = e.target;
     const siblings = link.closest('.nav').querySelectorAll('.nav__link');
     const logo = link.closest('.nav').querySelector('img');
 
     siblings.forEach(el => {
-      if (el !== link) el.style.opacity = 0.5;
+      if (el !== link) el.style.opacity = opacity;
     });
-    logo.style.opacity = 0.5;
+    logo.style.opacity = opacity;
   }
 };
 
