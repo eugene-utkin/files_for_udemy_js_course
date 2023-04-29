675fb386df05f08ad22bb24efbf87ebcd6cfc3ea
Evgeniy_Utkin <eugene.utkin@gmail.com>
2023-04-29 18:39:05 +0600

    bind used in mouseout

--- my_progress/Section13-Advanced-DOM-Bankist/lesson195/script.js
+++ my_progress/Section13-Advanced-DOM-Bankist/lesson195/script.js
@@ -130,9 +130,7 @@ const handleHover = function (e, opacity) {
 
 nav.addEventListener('mouseover', handleHover.bind(0.5));
 
-nav.addEventListener('mouseout', function (e) {
-  handleHover(e, 1);
-});
+nav.addEventListener('mouseout', handleHover.bind(1));
 
 /*
 // Selecting elements
