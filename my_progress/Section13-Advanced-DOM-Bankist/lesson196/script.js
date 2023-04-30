241b3875766be2ed8211f67d08612ab521f3fce7
Evgeniy_Utkin <eugene.utkin@gmail.com>
2023-04-30 18:51:59 +0600

    sticky nav logic implemented

--- my_progress/Section13-Advanced-DOM-Bankist/lesson196/script.js
+++ my_progress/Section13-Advanced-DOM-Bankist/lesson196/script.js
@@ -138,6 +138,9 @@ console.log(initialCoords);
 
 window.addEventListener('scroll', function () {
   console.log(window.scrollY);
+
+  if (this.window.scrollY > initialCoords.top) nav.classList.add('sticky');
+  else nav.classList.remove('sticky');
 });
 
 /*
