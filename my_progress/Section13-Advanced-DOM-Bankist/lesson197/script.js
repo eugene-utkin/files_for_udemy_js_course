16a6770a3f21c2670f61c010a85f80e1e8fd10ec
Evgeniy_Utkin <eugene.utkin@gmail.com>
2023-05-01 21:05:13 +0600

    old code commented out

--- my_progress/Section13-Advanced-DOM-Bankist/lesson197/script.js
+++ my_progress/Section13-Advanced-DOM-Bankist/lesson197/script.js
@@ -134,15 +134,15 @@ nav.addEventListener('mouseout', handleHover.bind(1));
 
 ///////////////////////////////////
 // Sticky navigation
-const initialCoords = section1.getBoundingClientRect();
-console.log(initialCoords);
+// const initialCoords = section1.getBoundingClientRect();
+// console.log(initialCoords);
 
-window.addEventListener('scroll', function () {
-  console.log(window.scrollY);
+// window.addEventListener('scroll', function () {
+//   console.log(window.scrollY);
 
-  if (this.window.scrollY > initialCoords.top) nav.classList.add('sticky');
-  else nav.classList.remove('sticky');
-});
+//   if (this.window.scrollY > initialCoords.top) nav.classList.add('sticky');
+//   else nav.classList.remove('sticky');
+// });
 
 /*
 // Selecting elements
