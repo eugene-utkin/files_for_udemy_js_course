65964c13b7e68c068d1ba6550b4962dc643160d2
Evgeniy_Utkin <eugene.utkin@gmail.com>
2023-05-03 15:33:43 +0600

    else condition added

--- my_progress/Section13-Advanced-DOM-Bankist/lesson197/script.js
+++ my_progress/Section13-Advanced-DOM-Bankist/lesson197/script.js
@@ -167,6 +167,7 @@ const stickyNav = function (entries) {
   console.log(entry);
 
   if (!entry.isIntersecting) nav.classList.add('sticky');
+  else nav.classList.remove('sticky');
 };
 
 const headerObserver = new IntersectionObserver(stickyNav, {
