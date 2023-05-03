5faa786aa242bf5632361370be34d46b4ee72791
Evgeniy_Utkin <eugene.utkin@gmail.com>
2023-05-03 15:26:48 +0600

    consolel og entry added

--- my_progress/Section13-Advanced-DOM-Bankist/lesson197/script.js
+++ my_progress/Section13-Advanced-DOM-Bankist/lesson197/script.js
@@ -164,6 +164,7 @@ const header = document.querySelector('.header');
 
 const stickyNav = function (entries) {
   const [entry] = entries;
+  console.log(entry);
 };
 
 const headerObserver = new IntersectionObserver(stickyNav, {
