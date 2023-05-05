8c0ca2b37a956bdcde15d119c77e780178cdefe0
Evgeniy_Utkin <eugene.utkin@gmail.com>
2023-05-05 17:22:46 +0600

    threshold added

--- my_progress/Section13-Advanced-DOM-Bankist/lesson198/script.js
+++ my_progress/Section13-Advanced-DOM-Bankist/lesson198/script.js
@@ -185,6 +185,7 @@ const revealSection = function (entries, observer) {};
 
 const sectionObserver = new IntersectionObserver(revealSection, {
   root: null,
+  threshold: 0.15,
 });
 allSections.forEach(function (section) {
   sectionObserver.observe(section);
