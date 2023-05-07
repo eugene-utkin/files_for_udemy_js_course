6905adfb96ebb609aa4ee46e45c7099b2461e45f
Evgeniy_Utkin <eugene.utkin@gmail.com>
2023-05-07 18:18:42 +0600

    lazy load implemented

--- my_progress/Section13-Advanced-DOM-Bankist/lesson199/script.js
+++ my_progress/Section13-Advanced-DOM-Bankist/lesson199/script.js
@@ -208,6 +208,7 @@ const loadImg = function (entries, observer) {
   if (!entry.isIntersecting) return;
 
   // Replace src with data-src
+  entry.target.src = entry.target.dataset.src;
 };
 
 const imgObserver = new IntersectionObserver(loadImg, {
