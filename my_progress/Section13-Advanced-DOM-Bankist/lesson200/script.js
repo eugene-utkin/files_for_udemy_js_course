5e0d5853ad03c5c109eb733ba77eb67502e41ca7
Evgeniy_Utkin <eugene.utkin@gmail.com>
2023-05-08 22:42:55 +0600

    example comment added

--- my_progress/Section13-Advanced-DOM-Bankist/lesson200/script.js
+++ my_progress/Section13-Advanced-DOM-Bankist/lesson200/script.js
@@ -228,6 +228,7 @@ imgTargets.forEach(img => imgObserver.observe(img));
 const slides = document.querySelectorAll('.slide');
 
 slides.forEach((s, i) => (s.style.transform = `translateX()`));
+// 0%, 100%, 200%, 300%
 
 ////////////////////////////////////////////
 ////////////////////////////////////////////
