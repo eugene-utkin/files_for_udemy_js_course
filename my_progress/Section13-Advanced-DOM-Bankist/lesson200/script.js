fd95f5f2d0a56b6fa234ead0da2938b7a4be17f7
Evgeniy_Utkin <eugene.utkin@gmail.com>
2023-05-09 20:54:13 +0600

    const btnLeft added

--- my_progress/Section13-Advanced-DOM-Bankist/lesson200/script.js
+++ my_progress/Section13-Advanced-DOM-Bankist/lesson200/script.js
@@ -226,6 +226,7 @@ imgTargets.forEach(img => imgObserver.observe(img));
 
 // Slider
 const slides = document.querySelectorAll('.slide');
+const btnLeft = document.querySelector('.slider__btn--left');
 
 const slider = document.querySelector('.slider');
 slider.style.transform = 'scale(0.4) translateX(-800px)';
