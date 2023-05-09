c112f63c7287aeb90cf941dd9c9abe170c8f0381
Evgeniy_Utkin <eugene.utkin@gmail.com>
2023-05-09 20:52:03 +0600

    translateX style added

--- my_progress/Section13-Advanced-DOM-Bankist/lesson200/script.js
+++ my_progress/Section13-Advanced-DOM-Bankist/lesson200/script.js
@@ -228,7 +228,7 @@ imgTargets.forEach(img => imgObserver.observe(img));
 const slides = document.querySelectorAll('.slide');
 
 const slider = document.querySelector('.slider');
-slider.style.transform = 'scale(0.2)';
+slider.style.transform = 'scale(0.2) translateX(-800px)';
 slider.style.overflow = 'visible';
 
 slides.forEach((s, i) => (s.style.transform = `translateX(${100 * i}%)`));
