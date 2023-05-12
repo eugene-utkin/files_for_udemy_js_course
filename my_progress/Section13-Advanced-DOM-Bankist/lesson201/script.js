7b3fc54f9203b347fcc5498f2cb6f9559c3cfaa1
Evgeniy_Utkin <eugene.utkin@gmail.com>
2023-05-12 16:05:01 +0600

    createDots function created

--- my_progress/Section13-Advanced-DOM-Bankist/lesson201/script.js
+++ my_progress/Section13-Advanced-DOM-Bankist/lesson201/script.js
@@ -237,6 +237,10 @@ const maxSlide = slides.length;
 // slider.style.transform = 'scale(0.4) translateX(-800px)';
 // slider.style.overflow = 'visible';
 
+const createDots = function () {
+  slides.forEach(function (_, i) {});
+};
+
 const goToSlide = function (slide) {
   slides.forEach(
     (s, i) => (s.style.transform = `translateX(${100 * (i - slide)}%)`)
