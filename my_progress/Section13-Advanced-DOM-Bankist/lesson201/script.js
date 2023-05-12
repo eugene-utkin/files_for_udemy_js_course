a51856821f8fbdbbc96b2e41c77fd42ee8b81d3d
Evgeniy_Utkin <eugene.utkin@gmail.com>
2023-05-12 19:18:21 +0600

    console log added to if condition

--- my_progress/Section13-Advanced-DOM-Bankist/lesson201/script.js
+++ my_progress/Section13-Advanced-DOM-Bankist/lesson201/script.js
@@ -284,6 +284,7 @@ document.addEventListener('keydown', function (e) {
 
 dotContainer.addEventListener('click', function (e) {
   if (e.target.classList.contains('dots__dot')) {
+    console.log('DOT');
   }
 });
 
