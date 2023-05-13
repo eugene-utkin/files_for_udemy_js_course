670db0d144e29b452ba1b4aae28365bbf554f1df
Evgeniy_Utkin <eugene.utkin@gmail.com>
2023-05-13 22:25:26 +0600

    e added to console log

--- my_progress/Section13-Advanced-DOM-Bankist/lesson202/script.js
+++ my_progress/Section13-Advanced-DOM-Bankist/lesson202/script.js
@@ -473,5 +473,5 @@ console.log(h1.parentElement.children);
 */
 
 document.addEventListener('DOMContentLoaded', function (e) {
-  console.log('HTML parsed and DOM tree built!');
+  console.log('HTML parsed and DOM tree built!', e);
 });
