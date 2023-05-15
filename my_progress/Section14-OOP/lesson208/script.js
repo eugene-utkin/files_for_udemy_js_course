907ec612a11995a013e28cb5160d9bef67825069
Evgeniy_Utkin <eugene.utkin@gmail.com>
2023-05-15 20:06:03 +0600

    Never do this comment added

--- my_progress/Section14-OOP/lesson208/script.js
+++ my_progress/Section14-OOP/lesson208/script.js
@@ -5,6 +5,7 @@ const Person = function (firstName, birthYear) {
   this.firstName = firstName;
   this.birthYear = birthYear;
 
+  // Never do this
   this.calcAge = function () {
     console.log(2037 - this.birthYear);
   };
