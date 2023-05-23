5a108b7b02b6aa52be4be24e12533c5a79ec9b9c
Evgeniy_Utkin <eugene.utkin@gmail.com>
2023-05-23 14:38:39 +0600

    Constructor Functions and the new Operator comment added

--- my_progress/Section14-OOP/lesson215/script.js
+++ my_progress/Section14-OOP/lesson215/script.js
@@ -1,6 +1,7 @@
 'use strict';
 
 ///////////////////////////////////////
+// Constructor Functions and the new Operator
 const Person = function (firstName, birthYear) {
   // Instance properties
   this.firstName = firstName;
