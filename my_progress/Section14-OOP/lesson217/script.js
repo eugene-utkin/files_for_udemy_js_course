2c4b64a97ac2719f220609a88576be283a730140
Evgeniy_Utkin <eugene.utkin@gmail.com>
2023-05-24 21:06:15 +0600

    class Car created

--- my_progress/Section14-OOP/lesson217/script.js
+++ my_progress/Section14-OOP/lesson217/script.js
@@ -264,3 +264,23 @@ sarah.calcAge();
 // Coding Challenge #2
 
 // 1.
+class Car {
+  constructor(make, speed) {
+    this.make = make;
+    this.speed = speed;
+  }
+
+  accelerate() {
+    this.speed += 10;
+    console.log(
+      `${this.make} is accelerating! Current speed is ${this.speed} km/h`
+    );
+  }
+
+  brake() {
+    this.speed -= 5;
+    console.log(
+      `${this.make} is slowing down! Current speed is ${this.speed} km/h`
+    );
+  }
+}
