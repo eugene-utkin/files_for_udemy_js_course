afbc4694f0cc4f304603e0f416b1a6f00807b7f1
Evgeniy_Utkin <eugene.utkin@gmail.com>
2023-05-24 21:01:08 +0600

    old code commented out

--- my_progress/Section14-OOP/lesson217/script.js
+++ my_progress/Section14-OOP/lesson217/script.js
@@ -235,6 +235,7 @@ console.log(account.latest);
 account.latest = 50;
 console.log(account.movements);
 */
+/*
 const PersonProto = {
   calcAge() {
     console.log(2037 - this.birthYear);
@@ -257,3 +258,4 @@ console.log(steven.__proto__ === PersonProto);
 const sarah = Object.create(PersonProto);
 sarah.init('Sarah', 1979);
 sarah.calcAge();
+*/
