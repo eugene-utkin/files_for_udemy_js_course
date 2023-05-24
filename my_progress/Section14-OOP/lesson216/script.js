31c7066cabd228a970e1012a55503bd18747aaf1
Evgeniy_Utkin <eugene.utkin@gmail.com>
2023-05-24 19:52:46 +0600

    init method added to PersonProto

--- my_progress/Section14-OOP/lesson216/script.js
+++ my_progress/Section14-OOP/lesson216/script.js
@@ -239,6 +239,11 @@ const PersonProto = {
   calcAge() {
     console.log(2037 - this.birthYear);
   },
+
+  init(firstName, birthYear) {
+    this.firstName = firstName;
+    this.birthYear = birthYear;
+  },
 };
 
 const steven = Object.create(PersonProto);
