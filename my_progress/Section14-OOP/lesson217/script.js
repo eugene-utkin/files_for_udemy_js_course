a65798f0d96b16ac4c45ea5b3b71272e6c0adcc2
Evgeniy_Utkin <eugene.utkin@gmail.com>
2023-05-26 18:37:00 +0600

    old code copied

--- my_progress/Section14-OOP/lesson217/script.js
+++ my_progress/Section14-OOP/lesson217/script.js
@@ -324,3 +324,17 @@ console.log(`The US speed is ${car3.speedUS} mi/h or ${car3.speed} km/h.`);
 */
 
 // Teacher's solution
+const Car = function (make, speed) {
+  this.make = make;
+  this.speed = speed;
+};
+
+Car.prototype.accelerate = function () {
+  this.speed += 10;
+  console.log(`${this.make} is going at ${this.speed} km/h`);
+};
+
+Car.prototype.brake = function () {
+  this.speed -= 5;
+  console.log(`${this.make} is going at ${this.speed} km/h`);
+};
