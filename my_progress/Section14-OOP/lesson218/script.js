9821f7653ca1b94a2fb3afb84abbd5026ca5c993
Evgeniy_Utkin <eugene.utkin@gmail.com>
2023-05-28 19:16:17 +0600

    introduce method added to prototype

--- my_progress/Section14-OOP/lesson218/script.js
+++ my_progress/Section14-OOP/lesson218/script.js
@@ -376,5 +376,9 @@ const Student = function (firstName, birthYear, course) {
   this.course = course;
 };
 
+Student.prototype.introduce = function () {
+  console.log(`My name is ${this.firstName} and I study ${this.course}`);
+};
+
 const mike = new Student('Mike', 2020, 'Computer Science');
 console.log(mike);
