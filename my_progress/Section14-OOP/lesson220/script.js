cbfda7120a22f7d4aca9ba9e8f8311c98ed30b2d
Evgeniy_Utkin <eugene.utkin@gmail.com>
2023-05-31 16:57:53 +0600

    PersonCl class copied

--- my_progress/Section14-OOP/lesson220/script.js
+++ my_progress/Section14-OOP/lesson220/script.js
@@ -497,3 +497,37 @@ tesla.accelerate();
 
 /////////////////////////////////////
 // Inheritance Between "Classes": ES6 Classes
+
+class PersonCl {
+  constructor(fullName, birthYear) {
+    this.fullName = fullName;
+    this.birthYear = birthYear;
+  }
+
+  // Instance methods
+  calcAge() {
+    console.log(2037 - this.birthYear);
+  }
+
+  greet() {
+    console.log(`Hey ${this.firstName}`);
+  }
+
+  get age() {
+    return 2037 - this.birthYear;
+  }
+
+  set fullName(name) {
+    if (name.includes(' ')) this._fullName = name;
+    else alert(`${name} is not a full name!`);
+  }
+
+  get fullName() {
+    return this._fullName;
+  }
+
+  static hey() {
+    console.log('Hey there 👋');
+    console.log(this);
+  }
+}
