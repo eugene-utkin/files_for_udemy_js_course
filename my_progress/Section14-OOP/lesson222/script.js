9b99790535a8de4ef85012272c5c4e98073acbe0
Evgeniy_Utkin <eugene.utkin@gmail.com>
2023-06-02 12:43:48 +0600

    class Account created

--- my_progress/Section14-OOP/lesson222/script.js
+++ my_progress/Section14-OOP/lesson222/script.js
@@ -592,3 +592,11 @@ jay.init('Jay', 2010, 'Computer Science');
 jay.introduce();
 jay.calcAge();
 */
+
+class Account {
+  constructor(owner, currency, pin) {
+    this.owner = owner;
+    this.currency = currency;
+    this.pin = pin;
+  }
+}
