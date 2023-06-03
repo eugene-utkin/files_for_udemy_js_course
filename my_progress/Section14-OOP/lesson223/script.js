e31ab6930436c06430feddb2eea050fa3dbe6ed8
Evgeniy_Utkin <eugene.utkin@gmail.com>
2023-06-03 18:05:37 +0600

    _pin moved

--- my_progress/Section14-OOP/lesson223/script.js
+++ my_progress/Section14-OOP/lesson223/script.js
@@ -597,8 +597,8 @@ class Account {
   constructor(owner, currency, pin) {
     this.owner = owner;
     this.currency = currency;
-    this._pin = pin;
     // Protected property
+    this._pin = pin;
     this._movements = [];
     this.locale = navigator.language;
 
