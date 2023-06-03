d4d1acb90ed62fa9ee11c5a8f4fb1ea5d39a23db
Evgeniy_Utkin <eugene.utkin@gmail.com>
2023-06-03 17:24:10 +0600

    _ used

--- my_progress/Section14-OOP/lesson223/script.js
+++ my_progress/Section14-OOP/lesson223/script.js
@@ -628,8 +628,8 @@ class Account {
 
 const acc1 = new Account('Jonas', 'EUR', 1111);
 
-// acc1.movements.push(250);
-// acc1.movements.push(-140);
+// acc1._movements.push(250);
+// acc1._movements.push(-140);
 acc1.deposit(250);
 acc1.withdraw(140);
 acc1.requestLoan(1000);
