83146f4ee59dcc6acde13f28fcaa8dba4915bc6a
Evgeniy_Utkin <eugene.utkin@gmail.com>
2023-06-06 17:32:05 +0600

    code rearranged

--- my_progress/Section14-OOP/lesson225/script.js
+++ my_progress/Section14-OOP/lesson225/script.js
@@ -655,14 +655,13 @@ const acc1 = new Account('Jonas', 'EUR', 1111);
 
 // acc1._movements.push(250);
 // acc1._movements.push(-140);
+// acc1.approveLoan(1000);
+
 acc1.deposit(250);
 acc1.withdraw(140);
 acc1.requestLoan(1000);
-// acc1.approveLoan(1000);
 console.log(acc1.getMovements());
-
 console.log(acc1);
-console.log(acc1.pin);
 Account.helper();
 
 // console.log(acc1.#movements);
