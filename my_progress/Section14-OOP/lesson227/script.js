871e7a644847cc34520af07c9fe019b04e32880a
Evgeniy_Utkin <eugene.utkin@gmail.com>
2023-06-09 18:55:38 +0600

    chargeBatery called

--- my_progress/Section14-OOP/lesson227/script.js
+++ my_progress/Section14-OOP/lesson227/script.js
@@ -795,4 +795,4 @@ class EVCl extends CarCl {
 const rivian = new EVCl('Rivian', 120, 23);
 console.log(rivian);
 // console.log(rivian.#charge);
-rivian.accelerate().accelerate().accelerate().brake();
+rivian.accelerate().accelerate().accelerate().brake().chargeBattery(50);
