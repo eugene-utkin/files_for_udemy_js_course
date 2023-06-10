b3f02b2b340dd794fb9a6a6a90aef8cd214c23ab
Evgeniy_Utkin <eugene.utkin@gmail.com>
2023-06-10 20:19:27 +0600

    used link to google maps

--- my_progress/Section15-Mapty/lesson232/script.js
+++ my_progress/Section15-Mapty/lesson232/script.js
@@ -16,7 +16,7 @@ if (navigator.geolocation) {
     function (position) {
       const { latitude } = position.coords;
       const { longitude } = position.coords;
-      console.log(latitude, longitude);
+      console.log(`https://www.google.com/maps/@${latitude},${longitude}z`);
     },
     function () {
       alert('Could not get your position');
