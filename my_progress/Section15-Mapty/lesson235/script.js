d2e72f4829d3441d8e7fc0f3aee502e9c333bfae
Evgeniy_Utkin <eugene.utkin@gmail.com>
2023-06-13 13:49:21 +0600

    old code commented out

--- my_progress/Section15-Mapty/lesson235/script.js
+++ my_progress/Section15-Mapty/lesson235/script.js
@@ -29,22 +29,21 @@ if (navigator.geolocation) {
       }).addTo(map);
 
       map.on('click', function (mapEvent) {
-        console.log(mapEvent);
-        const { lat, lng } = mapEvent.latlng;
-
-        L.marker([lat, lng])
-          .addTo(map)
-          .bindPopup(
-            L.popup({
-              maxWidth: 250,
-              minWidth: 100,
-              autoClose: false,
-              closeOnClick: false,
-              className: 'running-popup',
-            })
-          )
-          .setPopupContent('Workout')
-          .openPopup();
+        // console.log(mapEvent);
+        // const { lat, lng } = mapEvent.latlng;
+        // L.marker([lat, lng])
+        //   .addTo(map)
+        //   .bindPopup(
+        //     L.popup({
+        //       maxWidth: 250,
+        //       minWidth: 100,
+        //       autoClose: false,
+        //       closeOnClick: false,
+        //       className: 'running-popup',
+        //     })
+        //   )
+        //   .setPopupContent('Workout')
+        //   .openPopup();
       });
     },
     function () {
