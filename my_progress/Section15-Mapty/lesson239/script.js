ae9c8150073fc540fdbbc8398d8854d36585240c
Evgeniy_Utkin <eugene.utkin@gmail.com>
2023-06-18 14:15:40 +0600

    cadence validation added

--- my_progress/Section15-Mapty/lesson239/script.js
+++ my_progress/Section15-Mapty/lesson239/script.js
@@ -123,7 +123,11 @@ class App {
     if (type === 'running') {
       const cadence = +inputCadence.value;
       // Check if data is valid
-      if (!Number.isFinite(distance) || !Number.isFinite(duration))
+      if (
+        !Number.isFinite(distance) ||
+        !Number.isFinite(duration) ||
+        !Number.isFinite(cadence)
+      )
         return alert('Inputs have to be positive numbers');
     }
 
