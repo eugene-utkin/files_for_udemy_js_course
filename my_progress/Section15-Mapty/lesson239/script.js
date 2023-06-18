b37953cf21149a4c4be0227047a6a1f2c30a844e
Evgeniy_Utkin <eugene.utkin@gmail.com>
2023-06-18 14:14:38 +0600

    duration validation added

--- my_progress/Section15-Mapty/lesson239/script.js
+++ my_progress/Section15-Mapty/lesson239/script.js
@@ -123,7 +123,7 @@ class App {
     if (type === 'running') {
       const cadence = +inputCadence.value;
       // Check if data is valid
-      if (!Number.isFinite(distance))
+      if (!Number.isFinite(distance) || !Number.isFinite(duration))
         return alert('Inputs have to be positive numbers');
     }
 
