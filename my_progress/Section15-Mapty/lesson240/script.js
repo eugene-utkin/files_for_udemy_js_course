3d9dc139320444b292cd280620bc6e6a7b709ec8
Evgeniy_Utkin <eugene.utkin@gmail.com>
2023-06-20 19:04:54 +0600

    workout id used

--- my_progress/Section15-Mapty/lesson240/script.js
+++ my_progress/Section15-Mapty/lesson240/script.js
@@ -195,7 +195,7 @@ class App {
 
   _renderWorkout(workout) {
     const html = `
-      <li class="workout workout--running" data-id="1234567890">
+      <li class="workout workout--running" data-id="${workout.id}">
         <h2 class="workout__title">Running on April 14</h2>
         <div class="workout__details">
           <span class="workout__icon">🏃‍♂️</span>
