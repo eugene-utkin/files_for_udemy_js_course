3ab6a0b92e3a418b24e00ca33506b1b94e6b5525
Evgeniy_Utkin <eugene.utkin@gmail.com>
2023-06-21 17:09:06 +0600

    cycling html added

--- my_progress/Section15-Mapty/lesson240/script.js
+++ my_progress/Section15-Mapty/lesson240/script.js
@@ -233,6 +233,21 @@ class App {
               </div>
             </li>
           `;
+
+    if (workout.type === 'running')
+      html += `
+            <div class="workout__details">
+            <span class="workout__icon">⚡️</span>
+            <span class="workout__value">16</span>
+            <span class="workout__unit">km/h</span>
+          </div>
+          <div class="workout__details">
+            <span class="workout__icon">⛰</span>
+            <span class="workout__value">223</span>
+            <span class="workout__unit">m</span>
+          </div>
+        </li>
+      `;
   }
 }
 
