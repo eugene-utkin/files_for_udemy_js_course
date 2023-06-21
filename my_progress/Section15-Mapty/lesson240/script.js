9bc06be8f091ce5efb7550642957d48f6360a633
Evgeniy_Utkin <eugene.utkin@gmail.com>
2023-06-21 16:39:55 +0600

    workout pace used

--- my_progress/Section15-Mapty/lesson240/script.js
+++ my_progress/Section15-Mapty/lesson240/script.js
@@ -223,7 +223,7 @@ class App {
       html += `
               <div class="workout__details">
                 <span class="workout__icon">⚡️</span>
-                <span class="workout__value">4.6</span>
+                <span class="workout__value">${workout.pace}</span>
                 <span class="workout__unit">min/km</span>
               </div>
               <div class="workout__details">
