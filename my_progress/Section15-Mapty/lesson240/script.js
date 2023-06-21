2082cde6d5c174b2ed218329b1230453a1a87fe6
Evgeniy_Utkin <eugene.utkin@gmail.com>
2023-06-21 16:41:27 +0600

    toFixed used

--- my_progress/Section15-Mapty/lesson240/script.js
+++ my_progress/Section15-Mapty/lesson240/script.js
@@ -223,7 +223,7 @@ class App {
       html += `
               <div class="workout__details">
                 <span class="workout__icon">⚡️</span>
-                <span class="workout__value">${workout.pace}</span>
+                <span class="workout__value">${workout.pace.toFixed(1)}</span>
                 <span class="workout__unit">min/km</span>
               </div>
               <div class="workout__details">
