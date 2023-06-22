7e982f2ff57be6c39c190104e2ec4a973aecb04b
Evgeniy_Utkin <eugene.utkin@gmail.com>
2023-06-22 11:40:38 +0600

    typo fixed

--- my_progress/Section15-Mapty/lesson240/script.js
+++ my_progress/Section15-Mapty/lesson240/script.js
@@ -16,7 +16,7 @@ class Workout {
     // prettier-ignore
     const months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
 
-    this.description = `${this.type[0].toUppercase()}${this.type.slice(1)} on ${
+    this.description = `${this.type[0].toUpperCase()}${this.type.slice(1)} on ${
       months[this.date.getMonth()]
     } ${this.date.getDate()}`;
   }
