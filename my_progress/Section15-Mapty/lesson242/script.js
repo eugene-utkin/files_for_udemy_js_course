3582bca59ba79f00f79236c07570063846b8baa0
Evgeniy_Utkin <eugene.utkin@gmail.com>
2023-06-24 15:39:14 +0600

    JSON.parse used

--- my_progress/Section15-Mapty/lesson242/script.js
+++ my_progress/Section15-Mapty/lesson242/script.js
@@ -306,7 +306,7 @@ class App {
   }
 
   _getLocalStorage() {
-    const data = localStorage.getItem('workouts');
+    const data = JSON.parse(localStorage.getItem('workouts'));
     console.log(data);
   }
 }
