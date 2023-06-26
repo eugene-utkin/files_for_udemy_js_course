deae858df1f1ca33001733f34e2c709dd7936fbe
Evgeniy_Utkin <eugene.utkin@gmail.com>
2023-06-26 15:26:43 +0600

    load eventListener added to request

--- my_progress/Section16-Asynchronous/lesson248/script.js
+++ my_progress/Section16-Asynchronous/lesson248/script.js
@@ -8,3 +8,5 @@ const countriesContainer = document.querySelector('.countries');
 const request = new XMLHttpRequest();
 request.open('GET', 'https://restcountries.com/v3.1/name/portugal');
 request.send();
+
+request.addEventListener('load', function () {});
