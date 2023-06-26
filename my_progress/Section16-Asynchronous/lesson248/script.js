30f1ff0713e358048a7f1a1cf8e5f58957c4d617
Evgeniy_Utkin <eugene.utkin@gmail.com>
2023-06-26 16:37:15 +0600

    console log responseText

--- my_progress/Section16-Asynchronous/lesson248/script.js
+++ my_progress/Section16-Asynchronous/lesson248/script.js
@@ -9,4 +9,6 @@ const request = new XMLHttpRequest();
 request.open('GET', 'https://restcountries.com/v3.1/name/portugal');
 request.send();
 
-request.addEventListener('load', function () {});
+request.addEventListener('load', function () {
+  console.log(this.responseText);
+});
