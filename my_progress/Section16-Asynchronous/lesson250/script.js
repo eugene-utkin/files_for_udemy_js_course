995f336b4a6a39a3822ebafaf1e9cf9b08aa6752
Evgeniy_Utkin <eugene.utkin@gmail.com>
2023-06-28 14:16:30 +0600

    [] removed

--- my_progress/Section16-Asynchronous/lesson250/script.js
+++ my_progress/Section16-Asynchronous/lesson250/script.js
@@ -89,7 +89,7 @@ const getCountryAndNeighbour = function (country) {
     request2.send();
 
     request2.addEventListener('load', function () {
-      const [data2] = JSON.parse(this.responseText);
+      const data2 = JSON.parse(this.responseText);
       console.log(data2);
     });
   });
