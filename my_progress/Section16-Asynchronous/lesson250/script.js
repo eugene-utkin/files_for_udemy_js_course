10f6a1d37f9eb8d1fbc0df65cccbbe51801f22a3
Evgeniy_Utkin <eugene.utkin@gmail.com>
2023-06-29 15:31:54 +0600

    setTimeout used again

--- my_progress/Section16-Asynchronous/lesson250/script.js
+++ my_progress/Section16-Asynchronous/lesson250/script.js
@@ -105,5 +105,8 @@ setTimeout(() => {
   console.log('1 second passed');
   setTimeout(() => {
     console.log('2 seconds passed');
+    setTimeout(() => {
+      console.log('3 seconds passed');
+    }, 1000);
   }, 1000);
 }, 1000);
