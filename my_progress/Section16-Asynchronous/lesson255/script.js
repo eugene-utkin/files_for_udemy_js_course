6dbf81a1f0a562f7f74a3c82850eae8555468690
Evgeniy_Utkin <eugene.utkin@gmail.com>
2023-07-03 16:29:45 +0600

    typo fixed

--- my_progress/Section16-Asynchronous/lesson255/script.js
+++ my_progress/Section16-Asynchronous/lesson255/script.js
@@ -141,7 +141,44 @@ const getJSON = function (url, errorMsg = 'Something went wrong') {
   });
 };
 
-//
+// const getCountryData = function (country) {
+//   // Country 1
+//   fetch(`https://countries-api-836d.onrender.com/countries/name/${country}`)
+//     .then(response => {
+//       console.log(response);
+
+//       if (!response.ok)
+//         throw new Error(`Country not found (${response.status})`);
+
+//       return response.json();
+//     })
+//     .then(data => {
+//       renderCountry(data[0]);
+//       // const neighbour = data[0].borders?.[0];
+//       const neighbour = 'dfsdfdef';
+
+//       if (!neighbour) return;
+
+//       // Country 2
+//       return fetch(
+//         `https://countries-api-836d.onrender.com/countries/alpha/${neighbour}`
+//       );
+//     })
+//     .then(response => {
+//       if (!response.ok)
+//         throw new Error(`Country not found (${response.status})`);
+
+//       return response.json();
+//     })
+//     .then(data => renderCountry(data, 'neighbour'))
+//     .catch(err => {
+//       console.error(`${err} 💥💥💥`);
+//       renderError(`Something went wrong 💥💥 ${err.message}. Try again!`);
+//     })
+//     .finally(() => {
+//       countriesContainer.style.opacity = 1;
+//     });
+// };
 
 const getCountryData = function (country) {
   // Country 1
