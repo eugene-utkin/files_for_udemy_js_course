f79ca1c10321c3d40620365f1d7fa683ec171d53
Evgeniy_Utkin <eugene.utkin@gmail.com>
2023-07-05 06:08:05 +0600

    old code comented out

--- my_progress/Section16-Asynchronous/lesson256/script.js
+++ my_progress/Section16-Asynchronous/lesson256/script.js
@@ -22,10 +22,10 @@ const renderCountry = function (data, className = '') {
   // countriesContainer.style.opacity = 1;
 };
 
-const renderError = function (msg) {
-  countriesContainer.insertAdjacentText('beforeend', msg);
-  // countriesContainer.style.opacity = 1;
-};
+// const renderError = function (msg) {
+//   countriesContainer.insertAdjacentText('beforeend', msg);
+//   // countriesContainer.style.opacity = 1;
+// };
 
 ///////////////////////////////////////
 /*
