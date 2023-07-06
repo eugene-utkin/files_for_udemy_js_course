8eb1c1bc2ee6e2f29f292a356b42219f493c692a
Evgeniy_Utkin <eugene.utkin@gmail.com>
2023-07-06 21:08:22 +0600

    another then added

--- my_progress/Section16-Asynchronous/lesson256/script.js
+++ my_progress/Section16-Asynchronous/lesson256/script.js
@@ -312,5 +312,9 @@ whereAmI(52.508, 13.381);
 const whereAmI = function (lat, lng) {
   fetch(
     `https://api.bigdatacloud.net/data/reverse-geocode-client?latitude=${lat}&longitude=${lng}&localityLanguage=en`
-  ).then(res => res.json());
+  )
+    .then(res => res.json())
+    .then(data => {
+      console.log(data);
+    });
 };
