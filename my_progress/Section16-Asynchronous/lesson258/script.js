1f77ba10e7a7521aca126add08704ad553a612a1
Evgeniy_Utkin <eugene.utkin@gmail.com>
2023-07-08 20:21:11 +0600

    old code commented out

--- my_progress/Section16-Asynchronous/lesson258/script.js
+++ my_progress/Section16-Asynchronous/lesson258/script.js
@@ -309,6 +309,7 @@ whereAmI(52.508, 13.381);
 */
 
 // Teacher's solution
+/*
 const whereAmI = function (lat, lng) {
   fetch(
     `https://api.bigdatacloud.net/data/reverse-geocode-client?latitude=${lat}&longitude=${lng}&localityLanguage=en`
@@ -335,3 +336,4 @@ const whereAmI = function (lat, lng) {
 whereAmI(52.508, 13.381);
 whereAmI(19.037, 72.873);
 whereAmI(-33.933, 18.474);
+*/
