597df9aff3de1e0fe4c12af21319d4355c820085
Evgeniy_Utkin <eugene.utkin@gmail.com>
2023-07-08 17:38:30 +0600

    all whereAmI calls uncommented

--- my_progress/Section16-Asynchronous/lesson256/script.js
+++ my_progress/Section16-Asynchronous/lesson256/script.js
@@ -333,5 +333,5 @@ const whereAmI = function (lat, lng) {
     .catch(err => console.error(`${err.message} 💥`));
 };
 whereAmI(52.508, 13.381);
-// whereAmI(19.037, 72.873);
-// whereAmI(-33.933, 18.474);
+whereAmI(19.037, 72.873);
+whereAmI(-33.933, 18.474);
