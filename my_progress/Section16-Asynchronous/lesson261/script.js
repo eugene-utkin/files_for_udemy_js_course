9ff3fc433e736ef92b1885d1c3385ec50ffb6626
Evgeniy_Utkin <eugene.utkin@gmail.com>
2023-07-15 19:30:41 +0600

    catch added

--- my_progress/Section16-Asynchronous/lesson261/script.js
+++ my_progress/Section16-Asynchronous/lesson261/script.js
@@ -507,6 +507,8 @@ const createImage = function (imgPath) {
   });
 };
 
-createImage('img/img-1.jpg').then(img => {
-  console.log('Image 1 loaded');
-});
+createImage('img/img-1.jpg')
+  .then(img => {
+    console.log('Image 1 loaded');
+  })
+  .catch();
