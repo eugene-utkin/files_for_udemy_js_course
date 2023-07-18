7da86c704b36048c0b9d6741b0ced51775fa9bbb
Evgeniy_Utkin <eugene.utkin@gmail.com>
2023-07-18 20:41:56 +0600

    const dataGeo added

--- my_progress/Section16-Asynchronous/lesson262/script.js
+++ my_progress/Section16-Asynchronous/lesson262/script.js
@@ -548,7 +548,7 @@ const whereAmI = async function (country) {
   const resGeo = await fetch(
     `https://api.bigdatacloud.net/data/reverse-geocode-client?latitude=${lat}&longitude=${lng}&localityLanguage=en`
   );
-  await resGeo.json();
+  const dataGeo = await resGeo.json();
   // fetch(
   //   `https://countries-api-836d.onrender.com/countries/name/${country}`
   // ).then(res => console.log(res));
