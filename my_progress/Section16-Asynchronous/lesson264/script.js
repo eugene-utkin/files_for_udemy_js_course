bccb6a988d3b26075db174c3b9cff8cf3347bd61
Evgeniy_Utkin <eugene.utkin@gmail.com>
2023-07-21 21:17:53 +0600

    console log modified

--- my_progress/Section16-Asynchronous/lesson264/script.js
+++ my_progress/Section16-Asynchronous/lesson264/script.js
@@ -580,6 +580,6 @@ console.log('1: Will get location');
 // const city = whereAmI();
 // console.log(city);
 whereAmI()
-  .then(city => console.log(city))
+  .then(city => console.log(`2: ${city}`))
   .catch(err => console.error(`2: ${err.message} 💥`));
 console.log('3: Finished getting location');
