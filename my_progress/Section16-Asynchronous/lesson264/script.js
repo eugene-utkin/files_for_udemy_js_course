f943359ab8cb5ddd400d37184bbf3c123ee2c78d
Evgeniy_Utkin <eugene.utkin@gmail.com>
2023-07-21 20:48:02 +0600

    then added

--- my_progress/Section16-Asynchronous/lesson264/script.js
+++ my_progress/Section16-Asynchronous/lesson264/script.js
@@ -579,5 +579,5 @@ const whereAmI = async function (country) {
 console.log('1: Will get location');
 // const city = whereAmI();
 // console.log(city);
-whereAmI();
+whereAmI().then();
 console.log('3: Finished getting location');
