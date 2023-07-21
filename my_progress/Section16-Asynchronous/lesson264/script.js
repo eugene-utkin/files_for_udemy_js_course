4a7fffa7156ef137898d1be9478fd4055d2daf44
Evgeniy_Utkin <eugene.utkin@gmail.com>
2023-07-21 23:15:33 +0600

    first way commented out

--- my_progress/Section16-Asynchronous/lesson264/script.js
+++ my_progress/Section16-Asynchronous/lesson264/script.js
@@ -583,10 +583,10 @@ console.log('1: Will get location');
 // const city = whereAmI();
 // console.log(city);
 
-whereAmI()
-  .then(city => console.log(`2: ${city}`))
-  .catch(err => console.error(`2: ${err.message} 💥`))
-  .finally(() => console.log('3: Finished getting location'));
+// whereAmI()
+//   .then(city => console.log(`2: ${city}`))
+//   .catch(err => console.error(`2: ${err.message} 💥`))
+//   .finally(() => console.log('3: Finished getting location'));
 
 (async function () {
   try {
