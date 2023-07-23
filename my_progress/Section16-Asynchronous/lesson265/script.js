4555a960efa9be37d76966a75e8bfc64e51d82ed
Evgeniy_Utkin <eugene.utkin@gmail.com>
2023-07-23 15:53:19 +0600

    Promise.all added

--- my_progress/Section16-Asynchronous/lesson265/script.js
+++ my_progress/Section16-Asynchronous/lesson265/script.js
@@ -627,6 +627,11 @@ const get3Countries = async function (c1, c2, c3) {
       `https://countries-api-836d.onrender.com/countries/name/${c3}`
     );
 
+    Promise.all([
+      getJSON(`https://countries-api-836d.onrender.com/countries/name/${c1}`),
+      getJSON(`https://countries-api-836d.onrender.com/countries/name/${c2}`),
+      getJSON(`https://countries-api-836d.onrender.com/countries/name/${c3}`),
+    ]);
     console.log([data1.capital, data2.capital, data3.capital]);
   } catch (err) {
     console.error(err);
