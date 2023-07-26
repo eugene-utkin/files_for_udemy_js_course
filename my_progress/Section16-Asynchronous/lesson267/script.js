530d32ef40afbe5a98ab71ecdd0f6bfa114ff2cb
Evgeniy_Utkin <eugene.utkin@gmail.com>
2023-07-26 18:04:33 +0600

    old comment copied

--- my_progress/Section16-Asynchronous/lesson267/script.js
+++ my_progress/Section16-Asynchronous/lesson267/script.js
@@ -695,3 +695,45 @@ Promise.any([
 
 // My solution
 const loadNPause = async function () {};
+
+/*
+let currentImage;
+
+const createImage = function (imgPath) {
+  return new Promise(function (resolve, reject) {
+    const img = document.createElement('img');
+    img.src = imgPath;
+    img.addEventListener('error', function (e) {
+      reject(new Error('There is no such file'));
+    });
+    img.addEventListener('load', function () {
+      const images = document.querySelector('.images');
+      images.append(this);
+      resolve(this);
+    });
+  });
+};
+
+const createImageAndWait = function (src) {
+  return createImage(src).then(img => {
+    currentImage = img;
+    return wait(2);
+  });
+};
+
+const hideImageAndCreateNew = function (src) {
+  currentImage.style.display = 'none';
+  return createImageAndWait(src);
+};
+
+createImageAndWait('img/img-1.jpg')
+  .then(res => hideImageAndCreateNew('img/img-2.jpg'))
+  .then(img => hideImageAndCreateNew('img/img-3.jpg'))
+  .catch(err => console.error(`${err.message} 💥`));
+
+const wait = function (seconds) {
+  return new Promise(function (resolve) {
+    setTimeout(resolve, seconds * 1000);
+  });
+};
+*/
