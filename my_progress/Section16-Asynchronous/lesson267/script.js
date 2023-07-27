75c116fef1df4795ac59048c8ad442325666090e
Evgeniy_Utkin <eugene.utkin@gmail.com>
2023-07-27 13:54:42 +0600

    loadAll function called

--- my_progress/Section16-Asynchronous/lesson267/script.js
+++ my_progress/Section16-Asynchronous/lesson267/script.js
@@ -741,6 +741,8 @@ const loadNPause = async function () {
 const images = ['img/img-1.jpg', 'img/img-2.jpg', 'img/img3.jpg'];
 const loadAll = async function (imgArr) {};
 
+loadAll(images);
+
 /*
 let currentImage;
 
