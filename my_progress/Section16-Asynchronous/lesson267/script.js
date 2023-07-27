10427fbd02270256495f311932a7f7c1400a434b
Evgeniy_Utkin <eugene.utkin@gmail.com>
2023-07-27 14:30:02 +0600

    typo fixed

--- my_progress/Section16-Asynchronous/lesson267/script.js
+++ my_progress/Section16-Asynchronous/lesson267/script.js
@@ -753,7 +753,7 @@ const loadNPause = async function () {
 // loadNPause();
 
 // Part 2
-const images = ['img/img-1.jpg', 'img/img-2.jpg', 'img/img3.jpg'];
+const images = ['img/img-1.jpg', 'img/img-2.jpg', 'img/img-3.jpg'];
 const loadAll = async function (imgArr) {
   const imgs = imgArr.map(img => createImage(img));
   console.log(imgs);
