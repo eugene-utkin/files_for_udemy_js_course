1c4daf1bf4dc9cab3ddef7ce9738ab7185984197
Evgeniy_Utkin <eugene.utkin@gmail.com>
2023-07-27 14:39:45 +0600

    classList used

--- my_progress/Section16-Asynchronous/lesson267/script.js
+++ my_progress/Section16-Asynchronous/lesson267/script.js
@@ -755,8 +755,8 @@ const loadNPause = async function () {
 // Part 2
 const images = ['img/img-1.jpg', 'img/img-2.jpg', 'img/img-3.jpg'];
 const loadAll = async function (imgArr) {
-  Promise.all(imgArr.map(img => createImage(img)))
-    .then(res => console.log(res))
+  const imgs = Promise.all(imgArr.map(img => createImage(img)))
+    .then(res => res.map(img => img.classList.add('parallel')))
     .catch(err => console.error(err));
 };
 
