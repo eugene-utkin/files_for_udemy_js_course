9f0f567193633753129211ccef83b5b9cc83f61d
Evgeniy_Utkin <eugene.utkin@gmail.com>
2023-07-31 16:50:25 +0600

    cart push added

--- my_progress/Section17-Modern-JS-Modules-Tooling/lesson272/shoppingCart.js
+++ my_progress/Section17-Modern-JS-Modules-Tooling/lesson272/shoppingCart.js
@@ -4,4 +4,6 @@ console.log('Exporting module');
 const shippingCost = 10;
 const cart = [];
 
-const addToCart = function (product, quantity) {};
+const addToCart = function (product, quantity) {
+  cart.push({ product, quantity });
+};
