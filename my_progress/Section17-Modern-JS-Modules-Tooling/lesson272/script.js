2a183f7955acb048c9319bd6cefeff6a0eb71d49
Evgeniy_Utkin <eugene.utkin@gmail.com>
2023-07-31 18:00:12 +0600

    totalPrice added to import

--- my_progress/Section17-Modern-JS-Modules-Tooling/lesson272/script.js
+++ my_progress/Section17-Modern-JS-Modules-Tooling/lesson272/script.js
@@ -1,5 +1,5 @@
 // Importing module
-import { addToCart } from './shoppingCart.js';
+import { addToCart, totalPrice } from './shoppingCart.js';
 
 console.log('Importing module');
 // console.log(shippingCost);
