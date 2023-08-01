253ddfc77775d355997baf46062c568f1836dc6b
Evgeniy_Utkin <eugene.utkin@gmail.com>
2023-08-01 20:47:31 +0600

    addToCart call commented out

--- my_progress/Section17-Modern-JS-Modules-Tooling/lesson272/script.js
+++ my_progress/Section17-Modern-JS-Modules-Tooling/lesson272/script.js
@@ -1,10 +1,10 @@
 // Importing module
 // import { addToCart, totalPrice as price, tq } from './shoppingCart.js';
+// addToCart('bread', 5);
 
 import * as ShoppingCart from './shoppingCart';
 
 console.log('Importing module');
 // console.log(shippingCost);
 
-addToCart('bread', 5);
 console.log(price, tq);
