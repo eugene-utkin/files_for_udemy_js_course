ace6c3fb527750a80aba0aba588fca1aa892180a
Evgeniy_Utkin <eugene.utkin@gmail.com>
2023-08-01 20:48:07 +0600

    console log commented out

--- my_progress/Section17-Modern-JS-Modules-Tooling/lesson272/script.js
+++ my_progress/Section17-Modern-JS-Modules-Tooling/lesson272/script.js
@@ -1,10 +1,9 @@
 // Importing module
 // import { addToCart, totalPrice as price, tq } from './shoppingCart.js';
 // addToCart('bread', 5);
+// console.log(price, tq);
 
 import * as ShoppingCart from './shoppingCart';
 
 console.log('Importing module');
 // console.log(shippingCost);
-
-console.log(price, tq);
