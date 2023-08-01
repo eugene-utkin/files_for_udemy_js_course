a0c44575089d9882d1434b01ffc1e50a05cee03c
Evgeniy_Utkin <eugene.utkin@gmail.com>
2023-08-01 22:14:03 +0600

    import add from example

--- my_progress/Section17-Modern-JS-Modules-Tooling/lesson272/script.js
+++ my_progress/Section17-Modern-JS-Modules-Tooling/lesson272/script.js
@@ -9,3 +9,5 @@ console.log('Importing module');
 import * as ShoppingCart from './shoppingCart.js';
 ShoppingCart.addToCart('bread', 5);
 console.log(ShoppingCart.totalPrice);
+
+import add from './shoppingCart.js';
