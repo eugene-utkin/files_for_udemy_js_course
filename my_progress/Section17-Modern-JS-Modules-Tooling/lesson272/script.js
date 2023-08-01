94442c78371ae1570018fd819aef321e662888d2
Evgeniy_Utkin <eugene.utkin@gmail.com>
2023-08-01 20:46:13 +0600

    import * example

--- my_progress/Section17-Modern-JS-Modules-Tooling/lesson272/script.js
+++ my_progress/Section17-Modern-JS-Modules-Tooling/lesson272/script.js
@@ -1,6 +1,8 @@
 // Importing module
 import { addToCart, totalPrice as price, tq } from './shoppingCart.js';
 
+import * as ShoppingCart from './shoppingCart';
+
 console.log('Importing module');
 // console.log(shippingCost);
 
