3b47a50522ca27e01ea57e63f776173ae9cd2fd1
Evgeniy_Utkin <eugene.utkin@gmail.com>
2023-08-03 21:33:24 +0600

    at used

--- my_progress/Section17-Modern-JS-Modules-Tooling/lesson273/script.js
+++ my_progress/Section17-Modern-JS-Modules-Tooling/lesson273/script.js
@@ -30,6 +30,8 @@ const getLastPost = async function () {
   const res = await fetch('https://jsonplaceholder.typicode.com/posts');
   const data = await res.json();
   console.log(data);
+
+  { title: data.at(-1).title, text: data.at(-1).body };
 };
 
 getLastPost();
