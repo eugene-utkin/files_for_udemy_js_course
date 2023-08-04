cc851dfce52df8fd84e5d78b411fcf48b43da55f
Evgeniy_Utkin <eugene.utkin@gmail.com>
2023-08-04 21:26:34 +0600

    addToCart function added

--- my_progress/Section17-Modern-JS-Modules-Tooling/lesson274/script.js
+++ my_progress/Section17-Modern-JS-Modules-Tooling/lesson274/script.js
@@ -49,4 +49,9 @@ console.log(lastPost2);
   const shippingCost = 10;
   const totalPrice = 237;
   const totalQuantity = 23;
+
+  const addToCart = function (product, quantity) {
+    cart.push({ product, quantity });
+    console.log(`${quantity} ${product} added to cart`);
+  };
 })();
