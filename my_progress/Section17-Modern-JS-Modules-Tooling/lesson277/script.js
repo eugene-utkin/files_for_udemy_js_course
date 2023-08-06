2dd462fb60c6d1c46a1e13c5101b23269644eabc
Evgeniy_Utkin <eugene.utkin@gmail.com>
2023-08-06 21:57:41 +0600

    const stateDeepClone moved

--- my_progress/Section17-Modern-JS-Modules-Tooling/lesson277/script.js
+++ my_progress/Section17-Modern-JS-Modules-Tooling/lesson277/script.js
@@ -99,8 +99,9 @@ const state = {
   user: { loggedIn: true },
 };
 const stateClone = Object.assign({}, state);
+const stateDeepClone = cloneDeep(state);
+
 state.user.loggedIn = false;
 console.log(stateClone);
 
-const stateDeepClone = cloneDeep(state);
 console.log(stateDeepClone);
