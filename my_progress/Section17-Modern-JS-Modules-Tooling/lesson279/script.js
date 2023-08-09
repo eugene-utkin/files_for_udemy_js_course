bca05863a433fec72635320184f169516e35eea4
Evgeniy_Utkin <eugene.utkin@gmail.com>
2023-08-09 15:25:11 +0600

    original import restored

--- my_progress/Section17-Modern-JS-Modules-Tooling/lesson279/script.js
+++ my_progress/Section17-Modern-JS-Modules-Tooling/lesson279/script.js
@@ -126,6 +126,6 @@ console.log(cart.find(el => el.quantity >= 2));
 
 Promise.resolve('TEST').then(x => console.log(x));
 
-// import 'core-js/stable';
+import 'core-js/stable';
 // import 'core-js/stable/array/find';
 // import 'core-js/stable/promise';
