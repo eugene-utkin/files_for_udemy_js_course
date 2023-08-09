93ccd2a70e52d06491b6f186f2cb623ddd0fdf3f
Evgeniy_Utkin <eugene.utkin@gmail.com>
2023-08-09 15:24:33 +0600

    extra promises commented out

--- my_progress/Section17-Modern-JS-Modules-Tooling/lesson279/script.js
+++ my_progress/Section17-Modern-JS-Modules-Tooling/lesson279/script.js
@@ -127,5 +127,5 @@ console.log(cart.find(el => el.quantity >= 2));
 Promise.resolve('TEST').then(x => console.log(x));
 
 // import 'core-js/stable';
-import 'core-js/stable/array/find';
-import 'core-js/stable/promise';
+// import 'core-js/stable/array/find';
+// import 'core-js/stable/promise';
