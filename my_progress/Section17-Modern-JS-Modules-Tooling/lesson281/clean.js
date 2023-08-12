36ba412bc82e12f1c41606587802cabee8bc416f
Evgeniy_Utkin <eugene.utkin@gmail.com>
2023-08-12 20:40:02 +0600

    bigExpenses function renamed

--- my_progress/Section17-Modern-JS-Modules-Tooling/lesson281/clean.js
+++ my_progress/Section17-Modern-JS-Modules-Tooling/lesson281/clean.js
@@ -35,7 +35,7 @@ const checkExpenses = function () {
 };
 checkExpenses();
 
-const bigExpenses = function (limit) {
+const logBigExpenses = function (limit) {
   let output = '';
   for (let el of budget) {
     if (el.value <= -limit) {
