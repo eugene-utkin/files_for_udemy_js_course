5a387270e54be03edf38a80ce001173de5f8e6d9
Evgeniy_Utkin <eugene.utkin@gmail.com>
2023-08-14 20:23:40 +0600

    another freeze example

--- my_progress/Section17-Modern-JS-Modules-Tooling/lesson283/clean.js
+++ my_progress/Section17-Modern-JS-Modules-Tooling/lesson283/clean.js
@@ -1,6 +1,6 @@
 'strict mode';
 
-const budget = [
+const budget = Object.freeze([
   { value: 250, description: 'Sold old TV 📺', user: 'jonas' },
   { value: -45, description: 'Groceries 🥑', user: 'jonas' },
   { value: 3500, description: 'Monthly salary 👩‍💻', user: 'jonas' },
@@ -9,7 +9,7 @@ const budget = [
   { value: -20, description: 'Candy 🍭', user: 'matilda' },
   { value: -125, description: 'Toys 🚂', user: 'matilda' },
   { value: -1800, description: 'New Laptop 💻', user: 'jonas' },
-];
+]);
 
 const spendingLimits = Object.freeze({
   jonas: 1500,
