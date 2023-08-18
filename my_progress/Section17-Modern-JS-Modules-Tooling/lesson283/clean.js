ce8b6a726d4246347e17d87049b9daa247126912
Evgeniy_Utkin <eugene.utkin@gmail.com>
2023-08-18 21:53:46 +0600

    cur used

--- my_progress/Section17-Modern-JS-Modules-Tooling/lesson283/clean.js
+++ my_progress/Section17-Modern-JS-Modules-Tooling/lesson283/clean.js
@@ -70,7 +70,7 @@ const logBigExpenses = function (state, bigLimit) {
     .filter(entry => entry.value <= -bigLimit)
     // .map(entry => entry.description.slice(-2))
     // .join(' / ');
-    .reduce((str, cur) => `${str} /`, '');
+    .reduce((str, cur) => `${str} / ${cur}`, '');
 
   console.log(bigExpenses);
 
