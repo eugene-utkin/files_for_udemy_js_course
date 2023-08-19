493fdfc7fc15d860f6b33e963b707ba01a012932
Evgeniy_Utkin <eugene.utkin@gmail.com>
2023-08-19 16:14:33 +0600

    alert added

--- my_progress/Section18-forkify/lesson288/src/js/controller.js
+++ my_progress/Section18-forkify/lesson288/src/js/controller.js
@@ -17,5 +17,7 @@ const showRecipe = async function () {
     const res = await fetch(
       'https://forkify-api.herokuapp.com/api/v2/recipes/5ed6604591c37cdc054bc886'
     );
-  } catch (err) {}
+  } catch (err) {
+    alert(err);
+  }
 };
