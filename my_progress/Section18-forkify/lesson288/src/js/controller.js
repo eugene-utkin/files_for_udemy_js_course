52edf5576a752ea307885a2d2cfbe8d134002169
Evgeniy_Utkin <eugene.utkin@gmail.com>
2023-08-19 15:58:24 +0600

    fetch used

--- my_progress/Section18-forkify/lesson288/src/js/controller.js
+++ my_progress/Section18-forkify/lesson288/src/js/controller.js
@@ -12,4 +12,6 @@ const timeout = function (s) {
 
 ///////////////////////////////////////
 
-console.log('TEST');
+fetch(
+  'https://forkify-api.herokuapp.com/api/v2/recipes/5ed6604591c37cdc054bc886'
+);
