506a1f8dcd606256fab87d31934ff09d991aef1e
Evgeniy_Utkin <eugene.utkin@gmail.com>
2023-08-21 19:18:19 +0600

    new fetch added

--- my_progress/Section18-forkify/lesson288/src/js/controller.js
+++ my_progress/Section18-forkify/lesson288/src/js/controller.js
@@ -15,6 +15,7 @@ const timeout = function (s) {
 const showRecipe = async function () {
   try {
     const res = await fetch();
+    ('https://forkify-api.herokuapp.com/api/v2/recipes/5ed6604591c37cdc054bcc40');
     // 'https://forkify-api.herokuapp.com/api/v2/recipes/5ed6604591c37cdc054bc886'
     const data = await res.json();
 
