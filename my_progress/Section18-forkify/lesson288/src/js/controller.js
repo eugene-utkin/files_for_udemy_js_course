7578c985ca46c6e04beda3e760d51e548e81ddef
Evgeniy_Utkin <eugene.utkin@gmail.com>
2023-08-21 19:18:57 +0600

    fetch fixed

--- my_progress/Section18-forkify/lesson288/src/js/controller.js
+++ my_progress/Section18-forkify/lesson288/src/js/controller.js
@@ -14,8 +14,9 @@ const timeout = function (s) {
 
 const showRecipe = async function () {
   try {
-    const res = await fetch();
-    ('https://forkify-api.herokuapp.com/api/v2/recipes/5ed6604591c37cdc054bcc40');
+    const res = await fetch(
+      'https://forkify-api.herokuapp.com/api/v2/recipes/5ed6604591c37cdc054bcc40'
+    );
     // 'https://forkify-api.herokuapp.com/api/v2/recipes/5ed6604591c37cdc054bc886'
     const data = await res.json();
 
