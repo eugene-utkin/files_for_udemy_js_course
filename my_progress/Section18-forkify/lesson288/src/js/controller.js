6b42f6f42400a5bc9019e9a06eec5b6e5d7876b5
Evgeniy_Utkin <eugene.utkin@gmail.com>
2023-08-21 19:16:33 +0600

    old fetch commented out

--- my_progress/Section18-forkify/lesson288/src/js/controller.js
+++ my_progress/Section18-forkify/lesson288/src/js/controller.js
@@ -14,9 +14,8 @@ const timeout = function (s) {
 
 const showRecipe = async function () {
   try {
-    const res = await fetch(
-      'https://forkify-api.herokuapp.com/api/v2/recipes/5ed6604591c37cdc054bc886'
-    );
+    const res = await fetch();
+    // 'https://forkify-api.herokuapp.com/api/v2/recipes/5ed6604591c37cdc054bc886'
     const data = await res.json();
 
     if (!res.ok) throw new Error(`${data.message} (${res.status})`);
