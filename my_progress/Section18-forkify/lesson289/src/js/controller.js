210800bc46b6523a2e0697cada1a6a13761374ab
Evgeniy_Utkin <eugene.utkin@gmail.com>
2023-08-22 21:11:47 +0600

    previous import commented out

--- my_progress/Section18-forkify/lesson289/src/js/controller.js
+++ my_progress/Section18-forkify/lesson289/src/js/controller.js
@@ -1,4 +1,4 @@
-import icons from '../img/icons.svg'; // Parcel 1
+// import icons from '../img/icons.svg'; // Parcel 1
 import icons from 'url:../img/icons.svg'; // Parcel 2
 
 const recipeContainer = document.querySelector('.recipe');
