0322ef05579abe155e4e867f986c1eb3d14fe329
Evgeniy_Utkin <eugene.utkin@gmail.com>
2023-08-24 20:44:24 +0600

    console log moved

--- my_progress/Section18-forkify/lesson290/src/js/controller.js
+++ my_progress/Section18-forkify/lesson290/src/js/controller.js
@@ -32,10 +32,10 @@ const renderSpinner = function (parentEl) {
 const showRecipe = async function () {
   try {
     const id = window.location.hash;
+    console.log(id);
 
     // 1) Loading recipe
     renderSpinner(recipeContainer);
-    console.log(id);
 
     const res = await fetch(
       // 'https://forkify-api.herokuapp.com/api/v2/recipes/5ed6604591c37cdc054bcc40'
