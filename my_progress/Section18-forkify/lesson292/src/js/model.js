4b7b7caf2ab9b9581cc8eb28b5c9a9e1164c8a88
Evgeniy_Utkin <eugene.utkin@gmail.com>
2023-08-25 22:15:33 +0600

    code moved

--- my_progress/Section18-forkify/lesson292/src/js/model.js
+++ my_progress/Section18-forkify/lesson292/src/js/model.js
@@ -2,26 +2,26 @@ export const state = {
   recipe: {},
 };
 
-export const loadRecipe = async function () {};
+export const loadRecipe = async function () {
+  const res = await fetch(
+    `https://forkify-api.herokuapp.com/api/v2/recipes/${id}`
+  );
 
-const res = await fetch(
-  `https://forkify-api.herokuapp.com/api/v2/recipes/${id}`
-);
+  const data = await res.json();
 
-const data = await res.json();
+  if (!res.ok) throw new Error(`${data.message} (${res.status})`);
 
-if (!res.ok) throw new Error(`${data.message} (${res.status})`);
-
-console.log(res, data);
-let { recipe } = data.data;
-recipe = {
-  id: recipe.id,
-  title: recipe.title,
-  publisher: recipe.publisher,
-  sourceUrl: recipe.source_url,
-  image: recipe.image_url,
-  servings: recipe.servings,
-  cookingTime: recipe.cooking_time,
-  ingredients: recipe.ingredients,
+  console.log(res, data);
+  let { recipe } = data.data;
+  recipe = {
+    id: recipe.id,
+    title: recipe.title,
+    publisher: recipe.publisher,
+    sourceUrl: recipe.source_url,
+    image: recipe.image_url,
+    servings: recipe.servings,
+    cookingTime: recipe.cooking_time,
+    ingredients: recipe.ingredients,
+  };
+  console.log(recipe);
 };
-console.log(recipe);
