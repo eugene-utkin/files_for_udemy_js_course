1eccbfb343c8e2424de33f9ee864be86835df120
Evgeniy_Utkin <eugene.utkin@gmail.com>
2023-08-25 22:14:39 +0600

    old loadRecipe code cut

--- my_progress/Section18-forkify/lesson292/src/js/model.js
+++ my_progress/Section18-forkify/lesson292/src/js/model.js
@@ -3,3 +3,25 @@ export const state = {
 };
 
 export const loadRecipe = async function () {};
+
+const res = await fetch(
+  `https://forkify-api.herokuapp.com/api/v2/recipes/${id}`
+);
+
+const data = await res.json();
+
+if (!res.ok) throw new Error(`${data.message} (${res.status})`);
+
+console.log(res, data);
+let { recipe } = data.data;
+recipe = {
+  id: recipe.id,
+  title: recipe.title,
+  publisher: recipe.publisher,
+  sourceUrl: recipe.source_url,
+  image: recipe.image_url,
+  servings: recipe.servings,
+  cookingTime: recipe.cooking_time,
+  ingredients: recipe.ingredients,
+};
+console.log(recipe);
