204f41ca4ee75eb0a3b4c7119fd1ad20d3e400b1
Evgeniy_Utkin <eugene.utkin@gmail.com>
2023-08-26 15:25:04 +0600

    load recipe called

--- my_progress/Section18-forkify/lesson292/src/js/controller.js
+++ my_progress/Section18-forkify/lesson292/src/js/controller.js
@@ -40,6 +40,7 @@ const showRecipe = async function () {
     renderSpinner(recipeContainer);
 
     // 1) Loading recipe
+    model.loadRecipe(id);
 
     // 2) Rendering recipe
     const markup = `
