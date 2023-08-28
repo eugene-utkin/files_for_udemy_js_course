8bdcd209f8186e467217f722070d4ccce00ecb6c
Evgeniy_Utkin <eugene.utkin@gmail.com>
2023-08-28 21:21:56 +0600

    clear call fixed

--- my_progress/Section18-forkify/lesson292/src/js/views/recipeView.js
+++ my_progress/Section18-forkify/lesson292/src/js/views/recipeView.js
@@ -5,7 +5,7 @@ class RecipeView {
   render(data) {
     this.#data = data;
     const markup = this.#generateMarkup();
-    this.#clear;
+    this.#clear();
     this.#parentElement.insertAdjacentHTML('afterbegin', markup);
   }
 
