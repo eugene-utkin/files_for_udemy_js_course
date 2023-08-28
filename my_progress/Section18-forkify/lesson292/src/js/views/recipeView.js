4bef693e4104ddc42db7518b2e0cdfa96ba3536f
Evgeniy_Utkin <eugene.utkin@gmail.com>
2023-08-28 22:35:38 +0600

    Fraction used

--- my_progress/Section18-forkify/lesson292/src/js/views/recipeView.js
+++ my_progress/Section18-forkify/lesson292/src/js/views/recipeView.js
@@ -95,7 +95,9 @@ class RecipeView {
                 <svg class="recipe__icon">
                   <use href="${icons}#icon-check"></use>
                 </svg>
-                <div class="recipe__quantity">${ing.quantity}</div>
+                <div class="recipe__quantity">${new Fraction(
+                  ing.quantity
+                ).toString()}</div>
                 <div class="recipe__description">
                   <span class="recipe__unit">${ing.unit}</span>
                   ${ing.description}
