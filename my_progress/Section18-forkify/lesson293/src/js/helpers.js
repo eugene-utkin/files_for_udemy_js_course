90b33bd763db9f3fea8a55de007719dce97dca7c
Evgeniy_Utkin <eugene.utkin@gmail.com>
2023-08-29 14:50:51 +0600

    async added

--- my_progress/Section18-forkify/lesson293/src/js/helpers.js
+++ my_progress/Section18-forkify/lesson293/src/js/helpers.js
@@ -1,6 +1,6 @@
-const getJSON = function(url) {
+const getJSON = async function (url) {
   const res = await fetch(`${API_URL}/${id}`);
   const data = await res.json();
 
   if (!res.ok) throw new Error(`${data.message} (${res.status})`);
-}
+};
