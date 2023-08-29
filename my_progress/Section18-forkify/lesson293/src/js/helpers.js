22e100f05c3ea472f54de2371d64d96222887855
Evgeniy_Utkin <eugene.utkin@gmail.com>
2023-08-29 14:52:17 +0600

    url parameter used

--- my_progress/Section18-forkify/lesson293/src/js/helpers.js
+++ my_progress/Section18-forkify/lesson293/src/js/helpers.js
@@ -1,5 +1,5 @@
 export const getJSON = async function (url) {
-  const res = await fetch(`${API_URL}/${id}`);
+  const res = await fetch(url);
   const data = await res.json();
 
   if (!res.ok) throw new Error(`${data.message} (${res.status})`);
