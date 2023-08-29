556b7533ffb3bdc4a6ec17ce5b4b28d70f13a02f
Evgeniy_Utkin <eugene.utkin@gmail.com>
2023-08-29 14:50:07 +0600

    indentation fixed

--- my_progress/Section18-forkify/lesson293/src/js/helpers.js
+++ my_progress/Section18-forkify/lesson293/src/js/helpers.js
@@ -1,6 +1,6 @@
 const getJSON = function() {
-    const res = await fetch(`${API_URL}/${id}`);
-    const data = await res.json();
+  const res = await fetch(`${API_URL}/${id}`);
+  const data = await res.json();
 
-    if (!res.ok) throw new Error(`${data.message} (${res.status})`);
+  if (!res.ok) throw new Error(`${data.message} (${res.status})`);
 }
