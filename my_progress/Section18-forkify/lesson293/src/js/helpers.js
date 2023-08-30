03c6971004e19df9d44923abb42ae8e0681de25f
Evgeniy_Utkin <eugene.utkin@gmail.com>
2023-08-30 15:08:41 +0600

    0.5 used for timeout

--- my_progress/Section18-forkify/lesson293/src/js/helpers.js
+++ my_progress/Section18-forkify/lesson293/src/js/helpers.js
@@ -8,7 +8,7 @@ const timeout = function (s) {
 
 export const getJSON = async function (url) {
   try {
-    const res = await Promise.race([fetch(url), timeout(10)]);
+    const res = await Promise.race([fetch(url), timeout(0.5)]);
     const data = await res.json();
 
     if (!res.ok) throw new Error(`${data.message} (${res.status})`);
