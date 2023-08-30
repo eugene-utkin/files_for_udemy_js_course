7cc3381e2dcfd76a8e5051a1586c5a640a051437
Evgeniy_Utkin <eugene.utkin@gmail.com>
2023-08-30 14:00:07 +0600

    return added

--- my_progress/Section18-forkify/lesson293/src/js/helpers.js
+++ my_progress/Section18-forkify/lesson293/src/js/helpers.js
@@ -4,5 +4,6 @@ export const getJSON = async function (url) {
     const data = await res.json();
 
     if (!res.ok) throw new Error(`${data.message} (${res.status})`);
+    return data;
   } catch (err) {}
 };
