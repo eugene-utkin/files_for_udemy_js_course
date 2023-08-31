bf682320176489fe9edf7c05337d918b7c784998
Evgeniy_Utkin <eugene.utkin@gmail.com>
2023-08-31 14:24:52 +0600

    icons used

--- my_progress/Section18-forkify/lesson295/src/js/views/recipeView.js
+++ my_progress/Section18-forkify/lesson295/src/js/views/recipeView.js
@@ -34,7 +34,7 @@ class RecipeView {
       <div class="error">
         <div>
           <svg>
-            <use href="src/img/icons.svg#icon-alert-triangle"></use>
+            <use href="${icons}#icon-alert-triangle"></use>
           </svg>
         </div>
         <p>No recipes found for your query. Please try again!</p>
