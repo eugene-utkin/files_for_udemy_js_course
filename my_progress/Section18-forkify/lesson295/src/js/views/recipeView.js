08e43ff4d6814b5e0f17440780cc37da432e2d95
Evgeniy_Utkin <eugene.utkin@gmail.com>
2023-08-31 14:21:29 +0600

    renderError method created

--- my_progress/Section18-forkify/lesson295/src/js/views/recipeView.js
+++ my_progress/Section18-forkify/lesson295/src/js/views/recipeView.js
@@ -29,6 +29,8 @@ class RecipeView {
     this.#parentElement.insertAdjacentHTML('afterbegin', markup);
   };
 
+  renderError() {}
+
   addHandlerRender(handler) {
     ['hashchange', 'load'].forEach(ev => window.addEventListener(ev, handler));
   }
