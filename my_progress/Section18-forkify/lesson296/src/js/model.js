d4d16e383bcd9b83824823d23a3dcf2542d252e3
Evgeniy_Utkin <eugene.utkin@gmail.com>
2023-09-03 17:53:20 +0600

    data stored in state.search.results

--- my_progress/Section18-forkify/lesson296/src/js/model.js
+++ my_progress/Section18-forkify/lesson296/src/js/model.js
@@ -37,7 +37,7 @@ export const loadSearchResults = async function (query) {
     const data = await getJSON(`${API_URL}?search=${query}`);
     console.log(data);
 
-    data.data.recipes.map(rec => {
+    state.search.results = data.data.recipes.map(rec => {
       return {
         id: rec.id,
         title: rec.title,
