bac5322897b8663b1663c6c646da787f356e143f
Evgeniy_Utkin <eugene.utkin@gmail.com>
2023-09-05 21:34:39 +0600

    query used

--- my_progress/Section18-forkify/lesson296/src/js/controller.js
+++ my_progress/Section18-forkify/lesson296/src/js/controller.js
@@ -28,7 +28,7 @@ const controlSearchResults = async function () {
     const query = searchView.getQuery();
     if (!query) return;
 
-    await model.loadSearchResults('pizza');
+    await model.loadSearchResults(query);
     console.log(model.state.search.results);
   } catch (err) {
     console.log(err);
