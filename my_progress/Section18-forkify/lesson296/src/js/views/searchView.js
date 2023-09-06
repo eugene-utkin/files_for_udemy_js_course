8c452fdd838a76beb65250321ae93cbfd43958a6
Evgeniy_Utkin <eugene.utkin@gmail.com>
2023-09-06 18:32:25 +0600

    search field emptied

--- my_progress/Section18-forkify/lesson296/src/js/views/searchView.js
+++ my_progress/Section18-forkify/lesson296/src/js/views/searchView.js
@@ -5,7 +5,9 @@ class SearchView {
     return this.#parentEl.querySelector('.search__field').value;
   }
 
-  clearInput() {}
+  clearInput() {
+    this.#parentEl.querySelector('.search__field').value = '';
+  }
 
   addHandlerSearch(handler) {
     this.#parentEl.addEventListener('submit', function (e) {
