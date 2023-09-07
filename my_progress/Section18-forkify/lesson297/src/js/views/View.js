8b66a4a11f2c4aba9f854f3462143deee17ddb7d
Evgeniy_Utkin <eugene.utkin@gmail.com>
2023-09-07 16:35:43 +0600

    renderSpinner method moved to View

--- my_progress/Section18-forkify/lesson297/src/js/views/View.js
+++ my_progress/Section18-forkify/lesson297/src/js/views/View.js
@@ -11,4 +11,16 @@ export default class View {
   _clear() {
     this._parentElement.innerHTML = '';
   }
+
+  renderSpinner() {
+    const markup = `
+      <div class="spinner">
+        <svg>
+          <use href="${icons}#icon-loader"></use>
+        </svg>
+      </div
+    `;
+    this._clear();
+    this._parentElement.insertAdjacentHTML('afterbegin', markup);
+  }
 }
--- my_progress/Section18-forkify/lesson297/src/js/views/recipeView.js
+++ my_progress/Section18-forkify/lesson297/src/js/views/recipeView.js
@@ -9,18 +9,6 @@ class RecipeView {
   _errorMessage = 'We could not find that recipe. Please try another one!';
   _message = '';
 
-  renderSpinner() {
-    const markup = `
-      <div class="spinner">
-        <svg>
-          <use href="${icons}#icon-loader"></use>
-        </svg>
-      </div
-    `;
-    this._clear();
-    this._parentElement.insertAdjacentHTML('afterbegin', markup);
-  }
-
   renderError(message = this._errorMessage) {
     const markup = `
       <div class="error">
