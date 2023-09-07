22ba27f5d92a88494f47ede828b993d9a7a9f697
Evgeniy_Utkin <eugene.utkin@gmail.com>
2023-09-07 15:43:24 +0600

    _ used again

--- my_progress/Section18-forkify/lesson297/src/js/views/searchView.js
+++ my_progress/Section18-forkify/lesson297/src/js/views/searchView.js
@@ -2,7 +2,7 @@ class SearchView {
   _parentEl = document.querySelector('.search');
 
   getQuery() {
-    const query = this.#parentEl.querySelector('.search__field').value;
+    const query = this._parentEl.querySelector('.search__field').value;
     this.#clearInput();
     return query;
   }
