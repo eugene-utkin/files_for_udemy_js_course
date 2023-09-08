72316afb868596afd7f995ead4c821063bf91646
Evgeniy_Utkin <eugene.utkin@gmail.com>
2023-09-08 13:21:32 +0600

    generateMarkup method created

--- my_progress/Section18-forkify/lesson297/src/js/views/resultsView.js
+++ my_progress/Section18-forkify/lesson297/src/js/views/resultsView.js
@@ -2,6 +2,8 @@ import View from './View.js';
 
 class ResultsView extends View {
   _parentElement = document.querySelector('.results');
+
+  _generateMarkup() {}
 }
 
 export default new ResultsView();
