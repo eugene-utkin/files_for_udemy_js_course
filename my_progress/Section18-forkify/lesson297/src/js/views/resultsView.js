a5a675d673d170501a34ae26ed1fccbbd1bf2fc9
Evgeniy_Utkin <eugene.utkin@gmail.com>
2023-09-08 12:38:17 +0600

    parentEl renamed

--- my_progress/Section18-forkify/lesson297/src/js/views/resultsView.js
+++ my_progress/Section18-forkify/lesson297/src/js/views/resultsView.js
@@ -1,7 +1,7 @@
 import View from './View.js';
 
 class ResultsView extends View {
-  _parentEl = document.querySelector('.results');
+  _parentElement = document.querySelector('.results');
 }
 
 export default new ResultsView();
