bd595758be09f18376d8871adfbafda4c1791e3f
Evgeniy_Utkin <eugene.utkin@gmail.com>
2023-09-10 14:34:12 +0600

    argument removed from getSearchResultsPage call

--- my_progress/Section18-forkify/lesson298/src/js/controller.js
+++ my_progress/Section18-forkify/lesson298/src/js/controller.js
@@ -41,7 +41,7 @@ const controlSearchResults = async function () {
 
     // 3) Render results
     // resultsView.render(model.state.search.results);
-    resultsView.render(model.getSearchResultsPage(1));
+    resultsView.render(model.getSearchResultsPage());
   } catch (err) {
     console.log(err);
   }
