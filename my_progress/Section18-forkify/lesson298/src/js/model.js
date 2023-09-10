ce0623007e7e291e554271957c95be34d3a29369
Evgeniy_Utkin <eugene.utkin@gmail.com>
2023-09-10 14:33:10 +0600

    default value aded for page

--- my_progress/Section18-forkify/lesson298/src/js/model.js
+++ my_progress/Section18-forkify/lesson298/src/js/model.js
@@ -55,7 +55,7 @@ export const loadSearchResults = async function (query) {
   }
 };
 
-export const getSearchResultsPage = function (page) {
+export const getSearchResultsPage = function (page = state.search.page) {
   state.search.page = page;
 
   const start = (page - 1) * state.search.resultsPerPage; // 0;
