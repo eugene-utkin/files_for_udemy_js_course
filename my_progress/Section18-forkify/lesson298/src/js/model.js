d5387ef17f22afbbc01b7dcf8f55374cdff92c13
Evgeniy_Utkin <eugene.utkin@gmail.com>
2023-09-10 11:25:38 +0600

    resultsPerPage used

--- my_progress/Section18-forkify/lesson298/src/js/model.js
+++ my_progress/Section18-forkify/lesson298/src/js/model.js
@@ -55,7 +55,7 @@ export const loadSearchResults = async function (query) {
 };
 
 export const getSearchResultsPage = function (page) {
-  const start = (page - 1) * 10; // 0;
+  const start = (page - 1) * state.search.resultsPerPage; // 0;
   const end = page * 10; // 9;
 
   return state.search.results.slice(start, end);
