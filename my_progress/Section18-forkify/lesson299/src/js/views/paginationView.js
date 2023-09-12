0482ffbd195e0694d5d48f33ae4ab4b7c9234e22
Evgeniy_Utkin <eugene.utkin@gmail.com>
2023-09-12 11:01:58 +0600

    length used

--- my_progress/Section18-forkify/lesson299/src/js/views/paginationView.js
+++ my_progress/Section18-forkify/lesson299/src/js/views/paginationView.js
@@ -5,7 +5,7 @@ class PaginationView extends View {
   _parentElement = document.querySelector('.pagination');
 
   _generateMarkup() {
-    const numPages = this._data.results / this._data.resultsPerPage;
+    const numPages = this._data.results.length / this._data.resultsPerPage;
     console.log(numPages);
 
     // Page 1, and there are other pages
