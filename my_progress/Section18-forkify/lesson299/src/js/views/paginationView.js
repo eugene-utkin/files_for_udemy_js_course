65319b1443f90b1f5c9b60f1d5d7f64638419d9e
Evgeniy_Utkin <eugene.utkin@gmail.com>
2023-09-12 11:05:15 +0600

    ceil used

--- my_progress/Section18-forkify/lesson299/src/js/views/paginationView.js
+++ my_progress/Section18-forkify/lesson299/src/js/views/paginationView.js
@@ -5,7 +5,9 @@ class PaginationView extends View {
   _parentElement = document.querySelector('.pagination');
 
   _generateMarkup() {
-    const numPages = this._data.results.length / this._data.resultsPerPage;
+    const numPages = Math.ceil(
+      this._data.results.length / this._data.resultsPerPage
+    );
     console.log(numPages);
 
     // Page 1, and there are other pages
