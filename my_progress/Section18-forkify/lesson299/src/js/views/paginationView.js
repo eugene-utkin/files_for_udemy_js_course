914ba951947fc65d2be3b4df9c0609cc42cfcf22
Evgeniy_Utkin <eugene.utkin@gmail.com>
2023-09-13 11:54:05 +0600

    generateMarkupButton method called third time

--- my_progress/Section18-forkify/lesson299/src/js/views/paginationView.js
+++ my_progress/Section18-forkify/lesson299/src/js/views/paginationView.js
@@ -21,20 +21,7 @@ class PaginationView extends View {
     }
     // Other page
     if (curPage < numPages) {
-      return `
-        <button class="btn--inline pagination__btn--prev">
-          <svg class="search__icon">
-            <use href="${icons}#icon-arrow-left"></use>
-          </svg>
-          <span>Page ${curPage - 1}</span>
-        </button>
-        <button class="btn--inline pagination__btn--next">
-          <span>Page ${curPage + 1}</span>
-          <svg class="search__icon">
-            <use href="${icons}#icon-arrow-right"></use>
-          </svg>
-        </button>
-      `;
+      return this._generateMarkupButton('prev', curPage - 1);
     }
 
     // Page 1, and there are NO other pages
