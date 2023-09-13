8a84c969e44ab52557727ed6c139e61261c49ef1
Evgeniy_Utkin <eugene.utkin@gmail.com>
2023-09-13 10:50:32 +0600

    button code copied

--- my_progress/Section18-forkify/lesson299/src/js/views/paginationView.js
+++ my_progress/Section18-forkify/lesson299/src/js/views/paginationView.js
@@ -57,7 +57,12 @@ class PaginationView extends View {
 
   _generateMarkupButton(direction, pageNumber) {
     return `
-    
+      <button class="btn--inline pagination__btn--next">
+        <span>Page ${curPage + 1}</span>
+        <svg class="search__icon">
+          <use href="${icons}#icon-arrow-right"></use>
+        </svg>
+      </button>
     `;
   }
 }
