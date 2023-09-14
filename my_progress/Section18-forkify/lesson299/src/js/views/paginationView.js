f3b259aa3fbe35c22f170b767c7e8c240de56b88
Evgeniy_Utkin <eugene.utkin@gmail.com>
2023-09-14 10:56:46 +0600

    old console log removed

--- my_progress/Section18-forkify/lesson299/src/js/views/paginationView.js
+++ my_progress/Section18-forkify/lesson299/src/js/views/paginationView.js
@@ -7,7 +7,6 @@ class PaginationView extends View {
   addHandlerClick(handler) {
     this._parentElement.addEventListener('click', function (e) {
       const btn = e.target.closest('.btn--inline');
-      console.log(btn);
 
       const goToPage = btn.dataset.goto;
       console.log(goToPage);
