cd089191c706017de7aa95964f46f87ef31b8337
Evgeniy_Utkin <eugene.utkin@gmail.com>
2023-09-14 09:54:29 +0600

    const btn created

--- my_progress/Section18-forkify/lesson299/src/js/views/paginationView.js
+++ my_progress/Section18-forkify/lesson299/src/js/views/paginationView.js
@@ -5,7 +5,9 @@ class PaginationView extends View {
   _parentElement = document.querySelector('.pagination');
 
   addHandlerClick(handler) {
-    this._parentElement.addEventListener('click', function (e) {});
+    this._parentElement.addEventListener('click', function (e) {
+      const btn = e.target.closest('.btn--inline');
+    });
   }
 
   _generateMarkup() {
