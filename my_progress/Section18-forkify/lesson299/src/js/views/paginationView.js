c4f186b78fe0cc4f35db428474ea13dcccb09fb6
Evgeniy_Utkin <eugene.utkin@gmail.com>
2023-09-14 10:16:21 +0600

    console log added

--- my_progress/Section18-forkify/lesson299/src/js/views/paginationView.js
+++ my_progress/Section18-forkify/lesson299/src/js/views/paginationView.js
@@ -7,6 +7,7 @@ class PaginationView extends View {
   addHandlerClick(handler) {
     this._parentElement.addEventListener('click', function (e) {
       const btn = e.target.closest('.btn--inline');
+      console.log(btn);
     });
   }
 
