996355ba5bd8ce8740f334e48f94f12cd553e930
Evgeniy_Utkin <eugene.utkin@gmail.com>
2023-09-15 08:06:03 +0600

    comment modified

--- my_progress/Section18-forkify/lesson299/src/js/controller.js
+++ my_progress/Section18-forkify/lesson299/src/js/controller.js
@@ -52,7 +52,7 @@ const controlSearchResults = async function () {
 };
 
 const controlPagination = function (goToPage) {
-  // 1) Render results
+  // 1) Render NEW results
   // resultsView.render(model.state.search.results);
   resultsView.render(model.getSearchResultsPage(4));
 
