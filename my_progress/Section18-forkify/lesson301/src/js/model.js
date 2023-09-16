7a95a43388db13123f1cdb06e3a6636417243638
Evgeniy_Utkin <eugene.utkin@gmail.com>
2023-09-16 13:24:50 +0600

    quantity updated

--- my_progress/Section18-forkify/lesson301/src/js/model.js
+++ my_progress/Section18-forkify/lesson301/src/js/model.js
@@ -66,6 +66,7 @@ export const getSearchResultsPage = function (page = state.search.page) {
 
 export const updateServings = function (newServings) {
   state.recipe.ingredients.foreach(ing => {
+    ing.quantity = (ing.quantity * newServings) / state.recipe.servings;
     // newQt = oldQt * newServings / oldServings // 2 * 8 / 4 = 4
   });
 };
