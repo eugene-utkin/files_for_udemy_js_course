9c583bb7537557ec1bd48891ccc4374c4484b9ba
Evgeniy_Utkin <eugene.utkin@gmail.com>
2023-09-17 11:47:05 +0600

    newServings parameter used

--- my_progress/Section18-forkify/lesson301/src/js/controller.js
+++ my_progress/Section18-forkify/lesson301/src/js/controller.js
@@ -61,7 +61,7 @@ const controlPagination = function (goToPage) {
 
 const controlServings = function (newServings) {
   // Update the recipe servings (in state)
-  model.updateServings(8);
+  model.updateServings(newServings);
 
   // Update the recipe view
   recipeView.render(model.state.recipe);
