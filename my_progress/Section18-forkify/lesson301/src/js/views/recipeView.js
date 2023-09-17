e7807d2a51fbff07b89824e432bf5ec5caefc418
Evgeniy_Utkin <eugene.utkin@gmail.com>
2023-09-17 11:39:58 +0600

    console log added

--- my_progress/Section18-forkify/lesson301/src/js/views/recipeView.js
+++ my_progress/Section18-forkify/lesson301/src/js/views/recipeView.js
@@ -16,8 +16,8 @@ class RecipeView extends View {
   addHandlerUpdateServings(handler) {
     this._parentElement.addEventListener('click', function (e) {
       const btn = e.target.closest('.btn--tiny');
-
       if (!btn) return;
+      console.log(btn);
     });
   }
 
