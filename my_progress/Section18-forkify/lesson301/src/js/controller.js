c0c0163a1222297b9b892afada88bee873e9b0aa
Evgeniy_Utkin <eugene.utkin@gmail.com>
2023-09-17 11:41:02 +0600

    addHandlerUpdateServings called

--- my_progress/Section18-forkify/lesson301/src/js/controller.js
+++ my_progress/Section18-forkify/lesson301/src/js/controller.js
@@ -69,6 +69,7 @@ const controlServings = function () {
 
 const init = function () {
   recipeView.addHandlerRender(controlRecipes);
+  recipeView.addHandlerUpdateServings(controlServings);
   searchView.addHandlerSearch(controlSearchResults);
   paginationView.addHandlerClick(controlPagination);
 };
