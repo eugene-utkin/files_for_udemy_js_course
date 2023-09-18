474f947194947f8d125d523b80e7b1bd99c3a93c
Evgeniy_Utkin <eugene.utkin@gmail.com>
2023-09-18 13:35:45 +0600

    const curElements added

--- my_progress/Section18-forkify/lesson302/src/js/views/View.js
+++ my_progress/Section18-forkify/lesson302/src/js/views/View.js
@@ -23,6 +23,7 @@ export default class View {
     const newDOM = document.createRange().createContextualFragment(newMarkup);
     const newElements = newDOM.querySelectorAll('*');
     console.log(newElements);
+    const curElements = this._parentElement.querySelectorAll('*');
   }
 
   _clear() {
