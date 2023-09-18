80864f248cf9bc1d88753e48ec66422e9e4f0290
Evgeniy_Utkin <eugene.utkin@gmail.com>
2023-09-18 11:07:55 +0600

    conversion to integer added

--- my_progress/Section18-forkify/lesson301/src/js/views/recipeView.js
+++ my_progress/Section18-forkify/lesson301/src/js/views/recipeView.js
@@ -19,7 +19,7 @@ class RecipeView extends View {
       if (!btn) return;
       console.log(btn);
       const { updateTo } = btn.dataset;
-      if (updateTo > 0) handler(updateTo);
+      if (+updateTo > 0) handler(updateTo);
     });
   }
 
