0addd63d9208de0a8fa8703c93f22c9e36389068
Evgeniy_Utkin <eugene.utkin@gmail.com>
2023-09-19 13:49:13 +0600

    console log added

--- my_progress/Section18-forkify/lesson302/src/js/views/View.js
+++ my_progress/Section18-forkify/lesson302/src/js/views/View.js
@@ -32,6 +32,7 @@ export default class View {
         !newEl.isEqualNode(curEl) &&
         newEl.firstChild.nodeValue.trim() !== ''
       ) {
+        console.log(newEl.firstChild.nodeValue.trim());
         curEl.textContent = newEl.textContent;
       }
     });
