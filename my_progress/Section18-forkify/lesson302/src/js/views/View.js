79a69f7fc3e5187fc2126ee694b4672b5e66af1c
Evgeniy_Utkin <eugene.utkin@gmail.com>
2023-09-20 17:44:37 +0600

    {} removed

--- my_progress/Section18-forkify/lesson302/src/js/views/View.js
+++ my_progress/Section18-forkify/lesson302/src/js/views/View.js
@@ -38,11 +38,10 @@ export default class View {
       }
 
       // Updates changed ATTRIBUTES
-      if (!newEl.isEqualNode(curEl)) {
+      if (!newEl.isEqualNode(curEl))
         Array.from(newEl.attributes).forEach(attr =>
           curEl.setAttribute(attr.name, attr.value)
         );
-      }
     });
   }
 
