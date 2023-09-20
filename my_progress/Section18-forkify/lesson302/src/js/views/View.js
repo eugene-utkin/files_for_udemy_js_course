33763f4a145e36822683e4590894bac257ed71d0
Evgeniy_Utkin <eugene.utkin@gmail.com>
2023-09-20 16:54:43 +0600

    optional chaining added

--- my_progress/Section18-forkify/lesson302/src/js/views/View.js
+++ my_progress/Section18-forkify/lesson302/src/js/views/View.js
@@ -30,7 +30,7 @@ export default class View {
 
       if (
         !newEl.isEqualNode(curEl) &&
-        newEl.firstChild.nodeValue.trim() !== ''
+        newEl.firstChild?.nodeValue.trim() !== ''
       ) {
         console.log('💥', newEl.firstChild?.nodeValue.trim());
         curEl.textContent = newEl.textContent;
