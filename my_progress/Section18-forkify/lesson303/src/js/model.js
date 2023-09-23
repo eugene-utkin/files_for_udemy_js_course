de75ce559f708046d2e3116479062c6dfb1b29c3
Evgeniy_Utkin <eugene.utkin@gmail.com>
2023-09-23 14:00:06 +0600

    Delete bookmark comment added

--- my_progress/Section18-forkify/lesson303/src/js/model.js
+++ my_progress/Section18-forkify/lesson303/src/js/model.js
@@ -88,6 +88,7 @@ export const addBookmark = function (recipe) {
 };
 
 export const deleteBookmark = function (id) {
+  // Delete bookmark
   const index = state.bookmarks.findIndex(el => el.id === id);
   state, bookmarks.splice(index, 1);
 
