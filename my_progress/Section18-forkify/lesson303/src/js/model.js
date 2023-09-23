8ea4173be9b7105454767324b2d4fc542cb2303d
Evgeniy_Utkin <eugene.utkin@gmail.com>
2023-09-23 16:48:32 +0600

    typo fixed

--- my_progress/Section18-forkify/lesson303/src/js/model.js
+++ my_progress/Section18-forkify/lesson303/src/js/model.js
@@ -90,7 +90,7 @@ export const addBookmark = function (recipe) {
 export const deleteBookmark = function (id) {
   // Delete bookmark
   const index = state.bookmarks.findIndex(el => el.id === id);
-  state, bookmarks.splice(index, 1);
+  state.bookmarks.splice(index, 1);
 
   // Mark current recipe as NOT bookmarked
   if (id === state.recipe.id) state.recipe.bookmarked = false;
