d67e225642b3dfcc59878ba358896b49d28cddd9
Evgeniy_Utkin <eugene.utkin@gmail.com>
2023-09-24 11:16:13 +0600

    old console log removed

--- my_progress/Section18-forkify/lesson304/src/js/controller.js
+++ my_progress/Section18-forkify/lesson304/src/js/controller.js
@@ -73,7 +73,6 @@ const controlServings = function (newServings) {
 };
 
 const controlAddBookmark = function () {
-  console.log(model.state.recipe.bookmarked);
   if (!model.state.recipe.bookmarked) model.addBookmark(model.state.recipe);
   else model.deleteBookmark(model.state.recipe.id);
 
