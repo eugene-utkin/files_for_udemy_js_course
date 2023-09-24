a8832720bab2ee2ec803a3961fce97c8785f0395
Evgeniy_Utkin <eugene.utkin@gmail.com>
2023-09-24 12:30:50 +0600

    comment added

--- my_progress/Section18-forkify/lesson304/src/js/controller.js
+++ my_progress/Section18-forkify/lesson304/src/js/controller.js
@@ -73,6 +73,7 @@ const controlServings = function (newServings) {
 };
 
 const controlAddBookmark = function () {
+  // 1) Add/remove bookmark
   if (!model.state.recipe.bookmarked) model.addBookmark(model.state.recipe);
   else model.deleteBookmark(model.state.recipe.id);
 
