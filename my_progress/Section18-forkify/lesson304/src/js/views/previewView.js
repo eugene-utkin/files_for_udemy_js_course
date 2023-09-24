29a8ad054192ab2af31ae95f1cd73c5ca385b59e
Evgeniy_Utkin <eugene.utkin@gmail.com>
2023-09-24 14:24:52 +0600

    message removed from PreviewView

--- my_progress/Section18-forkify/lesson304/src/js/views/previewView.js
+++ my_progress/Section18-forkify/lesson304/src/js/views/previewView.js
@@ -4,7 +4,6 @@ import icons from 'url:../../img/icons.svg'; // Parcel 2
 class PreviewView extends View {
   _parentElement = document.querySelector('.results');
   _errorMessage = 'No recipes found for your query! Please try again ;)';
-  _message = '';
 
   _generateMarkup() {
     console.log(this._data);
