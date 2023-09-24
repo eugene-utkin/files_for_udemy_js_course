01f0ee85f7d7a2ec55463785dcf1e307006f2973
Evgeniy_Utkin <eugene.utkin@gmail.com>
2023-09-24 11:11:14 +0600

    errorMessage is modified

--- my_progress/Section18-forkify/lesson304/src/js/views/bookmarksView.js
+++ my_progress/Section18-forkify/lesson304/src/js/views/bookmarksView.js
@@ -3,7 +3,7 @@ import icons from 'url:../../img/icons.svg'; // Parcel 2
 
 class BookmarksView extends View {
   _parentElement = document.querySelector('.bookmarks__list');
-  _errorMessage = 'No recipes found for your query! Please try again ;)';
+  _errorMessage = 'No bookmarks yet';
   _message = '';
 
   _generateMarkup() {
