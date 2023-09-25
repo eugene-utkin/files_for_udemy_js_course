ee389959fed2e6ac1816e5347cb23643c0b95651
Evgeniy_Utkin <eugene.utkin@gmail.com>
2023-09-25 15:27:17 +0600

    render called

--- my_progress/Section18-forkify/lesson304/src/js/views/bookmarksView.js
+++ my_progress/Section18-forkify/lesson304/src/js/views/bookmarksView.js
@@ -9,7 +9,7 @@ class BookmarksView extends View {
 
   _generateMarkup() {
     console.log(this._data);
-    return this._data.map(this._generateMarkupPreview).join('');
+    return this._data.map(result => previewView.render(result)).join('');
   }
 }
 
