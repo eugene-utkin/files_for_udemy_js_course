c5c27191cbac920c13136b93e57ccd02a8ee0856
Evgeniy_Utkin <eugene.utkin@gmail.com>
2023-09-25 15:36:20 +0600

    this._data used sixth time

--- my_progress/Section18-forkify/lesson304/src/js/views/previewView.js
+++ my_progress/Section18-forkify/lesson304/src/js/views/previewView.js
@@ -17,7 +17,7 @@ class PreviewView extends View {
           </figure>
           <div class="preview__data">
             <h4 class="preview__title">${this._data.title}</h4>
-            <p class="preview__publisher">${result.publisher}</p>
+            <p class="preview__publisher">${this._data.publisher}</p>
           </div>
         </a>
       </li>
