fe8aab778696da8a54cb4213e898081b35c30026
Evgeniy_Utkin <eugene.utkin@gmail.com>
2023-09-25 15:34:49 +0600

    this._data used third time

--- my_progress/Section18-forkify/lesson304/src/js/views/previewView.js
+++ my_progress/Section18-forkify/lesson304/src/js/views/previewView.js
@@ -13,7 +13,7 @@ class PreviewView extends View {
           this._data.id === id ? 'preview__link--active' : ''
         }" href="#${this._data.id}">
           <figure class="preview__fig">
-            <img src="${result.image}" alt="${result.title}" />
+            <img src="${this._data.image}" alt="${result.title}" />
           </figure>
           <div class="preview__data">
             <h4 class="preview__title">${result.title}</h4>
