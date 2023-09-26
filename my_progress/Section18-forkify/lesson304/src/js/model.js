5500d6a7a168338787aa44dc3773ef57d97faeb0
Evgeniy_Utkin <eugene.utkin@gmail.com>
2023-09-26 17:07:44 +0600

    previous lesson restored

--- my_progress/Section18-forkify/lesson304/src/js/model.js
+++ my_progress/Section18-forkify/lesson304/src/js/model.js
@@ -79,18 +79,12 @@ export const updateServings = function (newServings) {
   state.recipe.servings = newServings;
 };
 
-const persistBookmarks = function () {
-  localStorage.setItem('bookmarks', JSON.stringify(state.bookmarks));
-};
-
 export const addBookmark = function (recipe) {
   // Add bookmark
   state.bookmarks.push(recipe);
 
   // Mark current recipe as bookmarked
   if (recipe.id === state.recipe.id) state.recipe.bookmarked = true;
-
-  persistBookmarks();
 };
 
 export const deleteBookmark = function (id) {
@@ -100,6 +94,4 @@ export const deleteBookmark = function (id) {
 
   // Mark current recipe as NOT bookmarked
   if (id === state.recipe.id) state.recipe.bookmarked = false;
-
-  persistBookmarks();
 };
