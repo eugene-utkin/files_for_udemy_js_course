118f630a07c6c6ef9103aaaf1ac03de28a580010
Evgeniy_Utkin <eugene.utkin@gmail.com>
2023-09-27 16:09:15 +0600

    load eventListener added

--- my_progress/Section18-forkify/lesson305/src/js/views/bookmarksView.js
+++ my_progress/Section18-forkify/lesson305/src/js/views/bookmarksView.js
@@ -7,7 +7,9 @@ class BookmarksView extends View {
   _errorMessage = 'No bookmarks yet. Find a nice recipe and bookmark it ;)';
   _message = '';
 
-  addHandlerRender(handler) {}
+  addHandlerRender(handler) {
+    window.addEventListener('load', handler);
+  }
 
   _generateMarkup() {
     return this._data
