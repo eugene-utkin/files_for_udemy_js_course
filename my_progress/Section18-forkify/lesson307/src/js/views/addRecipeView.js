c8f30e29ee6958a90ea151d3e917865e191edb26
Evgeniy_Utkin <eugene.utkin@gmail.com>
2023-09-28 20:05:18 +0600

    generateMarkupButton method deleted

--- my_progress/Section18-forkify/lesson307/src/js/views/addRecipeView.js
+++ my_progress/Section18-forkify/lesson307/src/js/views/addRecipeView.js
@@ -5,19 +5,6 @@ class PaginationView extends View {
   _parentElement = document.querySelector('.pagination');
 
   _generateMarkup() {}
-
-  generateMarkupButton(direction, pageNumber) {
-    return `
-      <button data-goto="${pageNumber}" class="btn--inline pagination__btn--${direction}">
-        <span>Page ${pageNumber}</span>
-        <svg class="search__icon">
-          <use href="${icons}#icon-arrow-${
-      direction === 'next' ? 'right' : 'left'
-    }"></use>
-        </svg>
-      </button>
-    `;
-  }
 }
 
 export default new PaginationView();
