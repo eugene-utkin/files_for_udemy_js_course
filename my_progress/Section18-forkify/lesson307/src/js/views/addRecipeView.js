1673c8716e291fa0c88c6d8bbe77dd60a83f7e1d
Evgeniy_Utkin <eugene.utkin@gmail.com>
2023-09-28 21:18:55 +0600

    _btnOpen created

--- my_progress/Section18-forkify/lesson307/src/js/views/addRecipeView.js
+++ my_progress/Section18-forkify/lesson307/src/js/views/addRecipeView.js
@@ -6,6 +6,7 @@ class AddRecipeView extends View {
 
   _window = document.querySelector('.add-recipe-window');
   _overlay = document.querySelector('.overlay');
+  _btnOpen = document.querySelector('.nav__btn--add-recipe');
 
   _generateMarkup() {}
 }
