73cc4001a9e82d667442ec7fe81e3acc81670c80
Evgeniy_Utkin <eugene.utkin@gmail.com>
2023-09-28 21:20:50 +0600

    _btnClose created

--- my_progress/Section18-forkify/lesson307/src/js/views/addRecipeView.js
+++ my_progress/Section18-forkify/lesson307/src/js/views/addRecipeView.js
@@ -7,6 +7,7 @@ class AddRecipeView extends View {
   _window = document.querySelector('.add-recipe-window');
   _overlay = document.querySelector('.overlay');
   _btnOpen = document.querySelector('.nav__btn--add-recipe');
+  _btnClose = document.querySelector('.btn--close-modal');
 
   _generateMarkup() {}
 }
