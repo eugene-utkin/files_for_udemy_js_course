f2958d7c24831ebea580ecbef8614ce588cba6e5
Evgeniy_Utkin <eugene.utkin@gmail.com>
2023-09-29 22:19:41 +0600

    _ removed from method call

--- my_progress/Section18-forkify/lesson307/src/js/controller.js
+++ my_progress/Section18-forkify/lesson307/src/js/controller.js
@@ -104,6 +104,6 @@ const init = function () {
   recipeView.addHandlerAddBookmark(controlAddBookmark);
   searchView.addHandlerSearch(controlSearchResults);
   paginationView.addHandlerClick(controlPagination);
-  addRecipeView._addHandlerUpload(controlAddRecipe);
+  addRecipeView.addHandlerUpload(controlAddRecipe);
 };
 init();
