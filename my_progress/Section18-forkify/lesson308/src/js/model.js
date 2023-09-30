698647337211cd9444ad93daac2251637141d347
Evgeniy_Utkin <eugene.utkin@gmail.com>
2023-09-30 18:38:55 +0600

    split used

--- my_progress/Section18-forkify/lesson308/src/js/model.js
+++ my_progress/Section18-forkify/lesson308/src/js/model.js
@@ -121,7 +121,7 @@ export const uploadRecipe = async function (newRecipe) {
   const ingredients = Object.entries(newRecipe)
     .filter(entry => entry[0].startsWith('ingredient') && entry[1] !== '')
     .map(ing => {
-      ing.replaceAll(' ', '');
+      ing.replaceAll(' ', '').split(',');
     });
   console.log(ingredients);
 };
