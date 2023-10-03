de13f4615be9ec7b66281d358730e4f1382b1900
Evgeniy_Utkin <eugene.utkin@gmail.com>
2023-10-03 10:25:56 +0600

    const KEY added

--- my_progress/Section18-forkify/lesson308/src/js/config.js
+++ my_progress/Section18-forkify/lesson308/src/js/config.js
@@ -1,3 +1,4 @@
 export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes/';
 export const TIMEOUT_SEC = 10;
 export const RES_PER_PAGE = 10;
+const KEY = '74e24b78-b66a-46da-bd66-4302fe6f51ba';
