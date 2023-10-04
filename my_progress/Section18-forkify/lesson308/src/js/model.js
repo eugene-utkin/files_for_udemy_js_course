13ccf96c3634dd9d294126ca5c143c0f9e8984f9
Evgeniy_Utkin <eugene.utkin@gmail.com>
2023-10-04 10:52:24 +0600

    addBookmark called

--- my_progress/Section18-forkify/lesson308/src/js/model.js
+++ my_progress/Section18-forkify/lesson308/src/js/model.js
@@ -145,6 +145,7 @@ export const uploadRecipe = async function (newRecipe) {
     };
     const data = await sendJSON(`${API_URL}?key=${KEY}`, recipe);
     state.recipe = createRecipeObject(data);
+    addBookmark(state.recipe);
   } catch (err) {
     throw err;
   }
