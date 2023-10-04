961723f6638a6c9619dcf3ca7eee448667f6fbf5
Evgeniy_Utkin <eugene.utkin@gmail.com>
2023-10-04 12:29:23 +0600

    history.pushState used

--- my_progress/Section18-forkify/lesson309/src/js/controller.js
+++ my_progress/Section18-forkify/lesson309/src/js/controller.js
@@ -113,6 +113,7 @@ const controlAddRecipe = async function (newRecipe) {
     bookmarksView.render(model.state.bookmarks);
 
     // Change ID in URL
+    window.history.pushState(null, '', `#${model.state.recipe.id}`);
 
     // Close form window
     setTimeout(function () {
