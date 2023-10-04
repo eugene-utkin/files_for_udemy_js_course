b4e056575a170d35bb1920df933d6c9e5a8134b2
Evgeniy_Utkin <eugene.utkin@gmail.com>
2023-10-04 11:22:53 +0600

    MODAL_CLOSE_SEC exported

--- my_progress/Section18-forkify/lesson308/src/js/config.js
+++ my_progress/Section18-forkify/lesson308/src/js/config.js
@@ -2,4 +2,4 @@ export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes/';
 export const TIMEOUT_SEC = 10;
 export const RES_PER_PAGE = 10;
 export const KEY = '74e24b78-b66a-46da-bd66-4302fe6f51ba';
-const MODAL_CLOSE_SEC = 2.5;
+export const MODAL_CLOSE_SEC = 2.5;
