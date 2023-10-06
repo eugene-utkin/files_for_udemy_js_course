be988a63710b834e395a2eaf2dfe232abcab8174
Evgeniy_Utkin <eugene.utkin@gmail.com>
2023-10-06 22:13:36 +0600

    rec used instead of recipe second time

--- my_progress/Section18-forkify/lesson309/src/js/model.js
+++ my_progress/Section18-forkify/lesson309/src/js/model.js
@@ -58,7 +58,7 @@ export const loadSearchResults = async function (query) {
         title: rec.title,
         publisher: rec.publisher,
         image: rec.image_url,
-        ...(rec.key && { key: recipe.key }),
+        ...(rec.key && { key: rec.key }),
       };
     });
     state.search.page = 1;
