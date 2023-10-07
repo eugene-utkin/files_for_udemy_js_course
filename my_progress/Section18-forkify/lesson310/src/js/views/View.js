a114fa4d1f6dbf84af52f1c1a8c395e97444a619
Evgeniy_Utkin <eugene.utkin@gmail.com>
2023-10-07 13:32:01 +0600

    array expected

--- my_progress/Section18-forkify/lesson310/src/js/views/View.js
+++ my_progress/Section18-forkify/lesson310/src/js/views/View.js
@@ -5,7 +5,7 @@ export default class View {
 
   /**
    * Render the received object to the DOM
-   * @param {Object} data
+   * @param {Object | Object[]} data
    * @param {*} render
    * @returns
    */
