6d895179621593773540d8ede9973710ed928795
Evgeniy_Utkin <eugene.utkin@gmail.com>
2023-10-09 18:38:03 +0600

    _generateMarkup method created

--- my_progress/Section18-forkify/lesson310/src/js/views/resultsFiltersView.js
+++ my_progress/Section18-forkify/lesson310/src/js/views/resultsFiltersView.js
@@ -2,6 +2,8 @@ import View from './View.js';
 
 class ResultsFiltersView extends View {
   _parentElement = document.querySelector('.filters');
+
+  _generateMarkup() {}
 }
 
 export default new ResultsFiltersView();
