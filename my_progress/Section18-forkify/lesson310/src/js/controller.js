25c90acbad6170aeeabf07b616396aeb186a3e92
Evgeniy_Utkin <eugene.utkin@gmail.com>
2023-10-09 16:39:59 +0600

    Render filter buttons comment added

--- my_progress/Section18-forkify/lesson310/src/js/controller.js
+++ my_progress/Section18-forkify/lesson310/src/js/controller.js
@@ -50,6 +50,8 @@ const controlSearchResults = async function () {
     // 2) Load search results
     await model.loadSearchResults(query);
 
+    // 3) Render filter buttons
+
     // 3) Render results
     // resultsView.render(model.state.search.results);
     resultsView.render(model.getSearchResultsPage());
