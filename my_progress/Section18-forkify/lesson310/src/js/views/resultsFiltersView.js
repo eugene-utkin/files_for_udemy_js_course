a3746ac5ad8cd1cc63204d199e07c4761923130c
Evgeniy_Utkin <eugene.utkin@gmail.com>
2023-10-09 18:40:43 +0600

    condition added

--- my_progress/Section18-forkify/lesson310/src/js/views/resultsFiltersView.js
+++ my_progress/Section18-forkify/lesson310/src/js/views/resultsFiltersView.js
@@ -3,7 +3,10 @@ import View from './View.js';
 class ResultsFiltersView extends View {
   _parentElement = document.querySelector('.filters');
 
-  _generateMarkup() {}
+  _generateMarkup() {
+    if (this._data.results.length > 1) {
+    }
+  }
 }
 
 export default new ResultsFiltersView();
