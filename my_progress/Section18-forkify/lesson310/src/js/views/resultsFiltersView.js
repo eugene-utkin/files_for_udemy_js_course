6bea18db49abc952d84140e5f8bf3b2fd68d2f15
Evgeniy_Utkin <eugene.utkin@gmail.com>
2023-10-10 16:57:11 +0600

    const curFilter created

--- my_progress/Section18-forkify/lesson310/src/js/views/resultsFiltersView.js
+++ my_progress/Section18-forkify/lesson310/src/js/views/resultsFiltersView.js
@@ -4,6 +4,8 @@ class ResultsFiltersView extends View {
   _parentElement = document.querySelector('.filters');
 
   _generateMarkup() {
+    const curFilter = this._data.filters;
+
     if (this._data.results.length > 1) {
     }
     return '';
