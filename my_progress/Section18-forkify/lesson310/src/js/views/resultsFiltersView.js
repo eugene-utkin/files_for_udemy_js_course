1ddb96be8ea4023893e0606fa3af590c89a48d33
Evgeniy_Utkin <eugene.utkin@gmail.com>
2023-10-10 17:04:21 +0600

    find used

--- my_progress/Section18-forkify/lesson310/src/js/views/resultsFiltersView.js
+++ my_progress/Section18-forkify/lesson310/src/js/views/resultsFiltersView.js
@@ -4,7 +4,9 @@ class ResultsFiltersView extends View {
   _parentElement = document.querySelector('.filters');
 
   _generateMarkup() {
-    const curFilter = Object.keys(this._data.filters);
+    const curFilter = Object.keys(this._data.filters).find(
+      key => this._data.filters[key] === true
+    );
 
     if (this._data.results.length > 1) {
     }
