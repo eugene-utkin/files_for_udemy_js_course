3809f2e2803c1f5f6a51203e1d081314587a86ad
Evgeniy_Utkin <eugene.utkin@gmail.com>
2023-10-13 18:23:54 +0600

    getFilteredSearchResults method created

--- my_progress/Section18-forkify/lesson310/src/js/model.js
+++ my_progress/Section18-forkify/lesson310/src/js/model.js
@@ -82,6 +82,8 @@ export const getSearchResultsPage = function (page = state.search.page) {
   return state.search.results.slice(start, end);
 };
 
+const getFilteredSearchResults = function () {};
+
 export const updateServings = function (newServings) {
   state.recipe.ingredients.forEach(ing => {
     ing.quantity = (ing.quantity * newServings) / state.recipe.servings;
