bed8ae9ba581a57cc155bde8a5080726419085cb
Evgeniy_Utkin <eugene.utkin@gmail.com>
2023-10-14 22:01:07 +0600

    fifth console log added

--- my_progress/Section18-forkify/lesson310/src/js/model.js
+++ my_progress/Section18-forkify/lesson310/src/js/model.js
@@ -97,6 +97,7 @@ export const getSearchResultsPage = function (page = state.search.page) {
 
 export const setSearchResultsFilter = function (filter) {
   console.log(filter);
+  console.log(Object.keys(state.search.filters));
   Object.keys(state.search.filters).forEach(key => {
     if (key === filter) {
       state.search.filters[filter] = true;
