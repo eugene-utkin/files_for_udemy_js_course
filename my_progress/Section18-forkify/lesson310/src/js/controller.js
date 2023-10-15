271976f5c1d8b89dc414654de9f95f880ae4b1c1
Evgeniy_Utkin <eugene.utkin@gmail.com>
2023-10-15 20:47:09 +0600

    old console log removed

--- my_progress/Section18-forkify/lesson310/src/js/controller.js
+++ my_progress/Section18-forkify/lesson310/src/js/controller.js
@@ -113,7 +113,6 @@ const controlAddRecipe = async function (newRecipe) {
 
     // Upload the new recipe data
     await model.uploadRecipe(newRecipe);
-    console.log(model.state.recipe);
 
     // Render recipe
     recipeView.render(model.state.recipe);
