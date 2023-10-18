4f49aae1a4cc6a137a59a00fe72fe59c86befded
Evgeniy_Utkin <eugene.utkin@gmail.com>
2023-10-18 18:01:07 +0600

    const markup added

--- my_progress/Section18-forkify/lesson310/src/js/views/addRecipeView.js
+++ my_progress/Section18-forkify/lesson310/src/js/views/addRecipeView.js
@@ -47,7 +47,7 @@ class AddRecipeView extends View {
       console.log(ingredient.value);
       const ingArr = ingredient.value.split(',').map(el => el.trim());
       if (ingArr.length !== 3) {
-        const sp1 = `
+        const markup = `
           <div class="ingredient-tooltip">
             Invalid ingredient format!
           </div>
