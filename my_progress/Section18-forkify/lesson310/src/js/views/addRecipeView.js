fc85bc05b9a2c0f8ec162a17cf4aec1f40cede88
Evgeniy_Utkin <eugene.utkin@gmail.com>
2023-10-21 11:45:21 +0600

    .style.transform used

--- my_progress/Section18-forkify/lesson310/src/js/views/addRecipeView.js
+++ my_progress/Section18-forkify/lesson310/src/js/views/addRecipeView.js
@@ -39,6 +39,8 @@ class AddRecipeView extends View {
   addIngredient(e) {
     e.preventDefault();
     console.log(e.target);
+
+    _window.style.transform = 'rotate(50px)';
     const ingredientMarkup = this.generateIngredientMarkup();
 
     this._btnAdd.insertAdjacentHTML('beforebegin', ingredientMarkup);
