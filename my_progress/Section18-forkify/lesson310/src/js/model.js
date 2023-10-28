3e2ed7ba568b3a165bb37b481cdccc8b387c73ba
Evgeniy_Utkin <eugene.utkin@gmail.com>
2023-10-28 17:00:16 +0600

    console log added

--- my_progress/Section18-forkify/lesson310/src/js/model.js
+++ my_progress/Section18-forkify/lesson310/src/js/model.js
@@ -152,6 +152,7 @@ const clearBookmarks = function () {
 
 export const uploadRecipe = async function (newRecipe) {
   try {
+    console.log(newRecipe);
     const ingredients = Object.entries(newRecipe)
       .filter(entry => entry[0].startsWith('ingredient') && entry[1] !== '')
       .map(ing => {
