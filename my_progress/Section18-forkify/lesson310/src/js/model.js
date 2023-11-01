4f2dbc80a3bce52e14dca4072b5dd76ae8c03df0
Evgeniy_Utkin <eugene.utkin@gmail.com>
2023-11-01 13:59:06 +0600

    map used

--- my_progress/Section18-forkify/lesson310/src/js/model.js
+++ my_progress/Section18-forkify/lesson310/src/js/model.js
@@ -149,7 +149,8 @@ export const addIngredientsToShoppingList = function (recipe) {
         return word.slice(0, 1).toUpperCase() + word.slice(1).toLowerCase();
       })
       .join(' ')
-      .split('/');
+      .split('/')
+      .map();
     console.log(ingWords);
     if (!state.shoppingList.includes(ing.description)) {
       state.shoppingList.push(ing.description);
