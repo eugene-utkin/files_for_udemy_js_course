356c7da1546cddf1ced9e102286a3600abeddea4
Evgeniy_Utkin <eugene.utkin@gmail.com>
2023-11-01 13:41:32 +0600

    console log added

--- my_progress/Section18-forkify/lesson310/src/js/model.js
+++ my_progress/Section18-forkify/lesson310/src/js/model.js
@@ -144,6 +144,7 @@ export const addIngredientsToShoppingList = function (recipe) {
   // Add ingredients
   recipe.ingredients.forEach(ing => {
     const ingWords = ing.split(' ');
+    console.log(ingWords);
     if (!state.shoppingList.includes(ing.description)) {
       state.shoppingList.push(ing.description);
     }
