cf065233b22192727d20b975ab73ca55ac585425
Evgeniy_Utkin <eugene.utkin@gmail.com>
2023-11-02 15:00:43 +0600

    slice used again

--- my_progress/Section18-forkify/lesson310/src/js/model.js
+++ my_progress/Section18-forkify/lesson310/src/js/model.js
@@ -149,7 +149,7 @@ export const addIngredientsToShoppingList = function (recipe) {
         let editedWord =
           word.slice(0, 1).toUpperCase() + word.slice(1).toLowerCase();
         editedWord.split('/').map(word => {
-          word.slice(0, 1).toUpperCase();
+          word.slice(0, 1).toUpperCase() + word.slice(1);
         });
         return word.slice(0, 1).toUpperCase() + word.slice(1).toLowerCase();
       })
