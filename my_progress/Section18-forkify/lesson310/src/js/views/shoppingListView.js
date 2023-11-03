46f8448ffaac265986d974f73fc06284f5204f58
Evgeniy_Utkin <eugene.utkin@gmail.com>
2023-11-03 16:46:15 +0600

    _errorMessage modified

--- my_progress/Section18-forkify/lesson310/src/js/views/shoppingListView.js
+++ my_progress/Section18-forkify/lesson310/src/js/views/shoppingListView.js
@@ -2,7 +2,14 @@ import View from './View.js';
 
 class ShoppingListView extends View {
   _parentElement = document.querySelector('.ingredients__list');
-  _errorMessage = 'Shopping list is empty.';
+  _errorMessage = `
+    <p>
+      Shopping list is empty.
+      <br />
+      <br />
+      Find a nice recipe and add ingredients to Shopping list.
+    </p>
+  `;
 
   addHandlerRender(handler) {
     window.addEventListener('load', handler);
