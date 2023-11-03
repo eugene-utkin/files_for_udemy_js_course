a5f5264e11511076ec5671695992e9a300b645f7
Evgeniy_Utkin <eugene.utkin@gmail.com>
2023-11-03 16:42:51 +0600

    _parentElement created

--- my_progress/Section18-forkify/lesson310/src/js/views/shoppingListView.js
+++ my_progress/Section18-forkify/lesson310/src/js/views/shoppingListView.js
@@ -1,6 +1,8 @@
 import View from './View.js';
 
 class ShoppingListView extends View {
+  _parentElement = document.querySelector('.ingredients__list');
+
   addHandlerRender(handler) {
     window.addEventListener('load', handler);
   }
