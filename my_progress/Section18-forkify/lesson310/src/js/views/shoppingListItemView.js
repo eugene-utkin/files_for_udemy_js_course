2c6cb562692804f3eaef7776f9c37735930c98fd
Evgeniy_Utkin <eugene.utkin@gmail.com>
2023-11-05 11:51:05 +0600

    constructor added

--- my_progress/Section18-forkify/lesson310/src/js/views/shoppingListItemView.js
+++ my_progress/Section18-forkify/lesson310/src/js/views/shoppingListItemView.js
@@ -3,6 +3,8 @@ import View from './View.js';
 class ShoppingListItemView extends View {
   _parentElement = '';
 
+  constructor() {}
+
   _generateMarkup() {
     return `
       <li class="shopping-list-ingredient">
