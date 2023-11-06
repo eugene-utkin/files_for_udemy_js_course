11338f2e81c251d08d590545b3d9b7d21aa51bce
Evgeniy_Utkin <eugene.utkin@gmail.com>
2023-11-06 17:04:09 +0600

    parentElement removed

--- my_progress/Section18-forkify/lesson310/src/js/views/addToCalendarView.js
+++ my_progress/Section18-forkify/lesson310/src/js/views/addToCalendarView.js
@@ -1,7 +1,5 @@
 import View from './View.js';
 
-class AddToCalendarView extends View {
-  _parentElement = document.querySelector('.add-to-calendar-window');
-}
+class AddToCalendarView extends View {}
 
 export default new AddToCalendarView();
