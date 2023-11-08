054c203a6f544bd9aed3b6fe430e5d1fdc5b2c26
Evgeniy_Utkin <eugene.utkin@gmail.com>
2023-11-08 18:38:35 +0600

    addHandlerRender method added

--- my_progress/Section18-forkify/lesson310/src/js/views/addToCalendarView.js
+++ my_progress/Section18-forkify/lesson310/src/js/views/addToCalendarView.js
@@ -2,6 +2,8 @@ import View from './View.js';
 
 class AddToCalendarView extends View {
   _parentElement = document.querySelector('.add-to-calendar-dropdown');
+
+  addHandlerRender() {}
 }
 
 export default new AddToCalendarView();
