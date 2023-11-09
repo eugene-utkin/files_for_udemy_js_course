118dcb545780811406f7f06ba82d4f016e78341c
Evgeniy_Utkin <eugene.utkin@gmail.com>
2023-11-09 14:48:52 +0600

    addToCalendarItemView imported

--- my_progress/Section18-forkify/lesson310/src/js/views/addToCalendarView.js
+++ my_progress/Section18-forkify/lesson310/src/js/views/addToCalendarView.js
@@ -1,4 +1,5 @@
 import View from './View.js';
+import addToCalendarItemView from './addToCalendarItemView.js';
 
 class AddToCalendarView extends View {
   _parentElement = document.querySelector('.add-to-calendar-dropdown');
