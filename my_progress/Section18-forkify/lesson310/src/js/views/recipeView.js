d431d3bb37a80c5f62384761427998f5bda96a61
Evgeniy_Utkin <eugene.utkin@gmail.com>
2023-11-10 15:30:11 +0600

    this._data.recipe used

--- my_progress/Section18-forkify/lesson310/src/js/views/recipeView.js
+++ my_progress/Section18-forkify/lesson310/src/js/views/recipeView.js
@@ -140,7 +140,9 @@ class RecipeView extends View {
       <div class="recipe__ingredients">
         <h2 class="heading--2">Recipe ingredients</h2>
         <ul class="recipe__ingredient-list">
-          ${this._data.ingredients.map(this._generateMarkupIngredient).join('')}
+          ${this._data.recipe.ingredients
+            .map(this._generateMarkupIngredient)
+            .join('')}
         </ul>
       </div>
 
