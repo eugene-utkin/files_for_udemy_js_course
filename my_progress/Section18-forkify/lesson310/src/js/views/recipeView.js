612bcaf6b161c8202338ef51f2e80dd2d22d4930
Evgeniy_Utkin <eugene.utkin@gmail.com>
2023-11-13 15:35:41 +0600

    const btn added

--- my_progress/Section18-forkify/lesson310/src/js/views/recipeView.js
+++ my_progress/Section18-forkify/lesson310/src/js/views/recipeView.js
@@ -40,7 +40,9 @@ class RecipeView extends View {
   }
 
   addHandlerAddToCalendar() {
-    this._parentElement.addEventListener('click', function (e) {});
+    this._parentElement.addEventListener('click', function (e) {
+      const btn = e.target.closest('');
+    });
   }
 
   addHandler;
