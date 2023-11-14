29b5a8646113e1e1872679bdb0af8b60e5b70d96
Evgeniy_Utkin <eugene.utkin@gmail.com>
2023-11-14 18:45:04 +0600

    div added

--- my_progress/Section18-forkify/lesson310/src/js/views/recipeView.js
+++ my_progress/Section18-forkify/lesson310/src/js/views/recipeView.js
@@ -185,12 +185,14 @@ class RecipeView extends View {
     const id = window.location.hash.slice(1);
     const active = day[1] && day[1]['id'] === id;
     return `
-      <span class="calendar-day">${capitalize(day[0])}:</span>
-      <button data-day="${day[0]}" class="btn--inline btn--add-to-calendar ${
+      <div>
+        <span class="calendar-day">${capitalize(day[0])}:</span>
+        <button data-day="${day[0]}" class="btn--inline btn--add-to-calendar ${
       active ? 'calendar-btn-active' : ''
     }">
-        ${capitalize(day[0])}
-      </button>
+          ${capitalize(day[0])}
+        </button>
+      </div>
     `;
   }
 }
