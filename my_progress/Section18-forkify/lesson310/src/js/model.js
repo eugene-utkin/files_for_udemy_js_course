3b9e9e52e1e407039c8c1e9d65033986623bca4d
Evgeniy_Utkin <eugene.utkin@gmail.com>
2023-11-15 14:27:07 +0600

    first condition added

--- my_progress/Section18-forkify/lesson310/src/js/model.js
+++ my_progress/Section18-forkify/lesson310/src/js/model.js
@@ -170,10 +170,12 @@ export const addIngredientsToShoppingList = function (recipe) {
 };
 
 export const addToCalendar = function (day) {
-  state.calendar[day] = {
-    id: state.recipe['id'],
-    title: state.recipe['title'],
-  };
+  if (!state.calendar[day]) {
+    state.calendar[day] = {
+      id: state.recipe['id'],
+      title: state.recipe['title'],
+    };
+  }
   console.log(state.calendar);
 };
 
