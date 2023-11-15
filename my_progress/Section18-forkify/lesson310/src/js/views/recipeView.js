9b68a861b777becf1d20398c32c4d82bf1a49ea0
Evgeniy_Utkin <eugene.utkin@gmail.com>
2023-11-15 16:43:53 +0600

    closest fixed

--- my_progress/Section18-forkify/lesson310/src/js/views/recipeView.js
+++ my_progress/Section18-forkify/lesson310/src/js/views/recipeView.js
@@ -45,7 +45,7 @@ class RecipeView extends View {
       if (!btn) return;
       btn.classList.toggle('calendar-btn-active');
       const { day } = btn.dataset;
-      const segment = e.target.closest('.calendar-title');
+      const segment = e.target.closest('.calendar-segment');
       handler(day);
     });
   }
