607e238dd5471a318b15764de55b58a273805a5b
Evgeniy_Utkin <eugene.utkin@gmail.com>
2023-11-19 10:54:24 +0600

    _errorMessage added

--- my_progress/Section18-forkify/lesson310/src/js/views/calendarView.js
+++ my_progress/Section18-forkify/lesson310/src/js/views/calendarView.js
@@ -2,6 +2,14 @@ import View from './View.js';
 
 class calendarView extends View {
   _parentElement = document.querySelector('.nav-calendar__list');
+  _errorMessage = `
+    <p>
+      Calendar is empty.
+      <br />
+      <br />
+      Find a nice recipe and add it to the Calendar.
+    </p>
+  `;
 
   addHandlerRender(handler) {
     window.addEventListener('load', handler);
