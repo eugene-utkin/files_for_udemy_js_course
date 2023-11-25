1d1d869a1546cbf7ee615d00f5361fffdb13581d
Evgeniy_Utkin <eugene.utkin@gmail.com>
2023-11-25 14:40:20 +0600

    _message added

--- my_progress/Section18-forkify/lesson310/src/js/views/caloriesView.js
+++ my_progress/Section18-forkify/lesson310/src/js/views/caloriesView.js
@@ -3,6 +3,7 @@ import View from './View.js';
 class CaloriesView extends View {
   _parentElement = document.querySelector('.calories-window');
   _errorMessage = 'Something went wrong. Please, try again.';
+  _message = '';
 }
 
 export default new CaloriesView();
