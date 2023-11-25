ed08268c2327fe8803ba325543b40a02032be9ff
Evgeniy_Utkin <eugene.utkin@gmail.com>
2023-11-25 14:37:46 +0600

    _parentElement added

--- my_progress/Section18-forkify/lesson310/src/js/views/caloriesView.js
+++ my_progress/Section18-forkify/lesson310/src/js/views/caloriesView.js
@@ -1,5 +1,7 @@
 import View from './View.js';
 
-class CaloriesView extends View {}
+class CaloriesView extends View {
+  _parentElement = document.querySelector('.calories-window');
+}
 
 export default new CaloriesView();
