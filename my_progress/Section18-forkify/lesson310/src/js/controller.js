f5cefa500b8377c4ba03f61ab1d43ff4c46695a0
Evgeniy_Utkin <eugene.utkin@gmail.com>
2023-11-29 13:11:20 +0600

    render spinnder called

--- my_progress/Section18-forkify/lesson310/src/js/controller.js
+++ my_progress/Section18-forkify/lesson310/src/js/controller.js
@@ -186,6 +186,7 @@ const controlRemoveFromCalendar = function (day) {
 
 const controlShowCalories = function () {
   console.log('Calories!');
+  caloriesView.renderSpinner();
 };
 
 const init = function () {
