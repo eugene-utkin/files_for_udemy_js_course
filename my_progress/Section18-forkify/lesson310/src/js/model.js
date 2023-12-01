a6384ca6250fc4092e57d8a41f9886425341116f
Evgeniy_Utkin <eugene.utkin@gmail.com>
2023-12-01 16:10:38 +0600

    TIMEOUT_SEC imported

--- my_progress/Section18-forkify/lesson310/src/js/model.js
+++ my_progress/Section18-forkify/lesson310/src/js/model.js
@@ -5,6 +5,7 @@ import {
   KEY,
   CALORIES_URL,
   CALORIES_KEY,
+  TIMEOUT_SEC,
 } from './config.js';
 // import { getJSON, sendJSON } from './helpers.js';
 import { AJAX, capitalize } from './helpers.js';
