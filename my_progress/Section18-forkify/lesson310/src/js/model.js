b712324d57b713f4b743c426e73aeac4f930db33
Evgeniy_Utkin <eugene.utkin@gmail.com>
2023-12-02 16:04:54 +0600

    const id used

--- my_progress/Section18-forkify/lesson310/src/js/model.js
+++ my_progress/Section18-forkify/lesson310/src/js/model.js
@@ -72,7 +72,7 @@ export const loadCalories = async function (ingredient) {
       `${CALORIES_URL}?query=${ingredient.name}&number=1&apiKey=${CALORIES_KEY}&metaInformation=true`
     );
     const id = data.results[0].id;
-    const url = `https://api.spoonacular.com/food/ingredients/{id}/information`;
+    const url = `https://api.spoonacular.com/food/ingredients/${id}/information`;
     console.log(id);
   } catch (err) {
     throw err;
