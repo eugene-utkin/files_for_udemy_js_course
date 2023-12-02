900740be9287f7ff007bafabc0ada89bd4840a99
Evgeniy_Utkin <eugene.utkin@gmail.com>
2023-12-02 16:09:59 +0600

    const ingData used

--- my_progress/Section18-forkify/lesson310/src/js/model.js
+++ my_progress/Section18-forkify/lesson310/src/js/model.js
@@ -71,7 +71,7 @@ export const loadCalories = async function (ingredient) {
     const ingData = await AJAX(
       `${CALORIES_URL}?query=${ingredient.name}&number=1&apiKey=${CALORIES_KEY}&metaInformation=true`
     );
-    const id = data.results[0].id;
+    const id = ingData.results[0].id;
     const url = `https://api.spoonacular.com/food/ingredients/${id}/information?amount=${ingredient.quantity}&unit=${ingredient.unit}`;
     console.log(id);
   } catch (err) {
