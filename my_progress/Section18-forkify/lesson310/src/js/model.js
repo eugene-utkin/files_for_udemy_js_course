9f14857e84f9cf128e57871ab442f230e0f21171
Evgeniy_Utkin <eugene.utkin@gmail.com>
2023-12-03 15:00:18 +0600

    console log modified

--- my_progress/Section18-forkify/lesson310/src/js/model.js
+++ my_progress/Section18-forkify/lesson310/src/js/model.js
@@ -78,7 +78,7 @@ export const loadCalories = async function (ingredient) {
     const id = ingData.results[0].id;
     const url = `https://api.spoonacular.com/food/ingredients/${id}/information?apiKey=${CALORIES_KEY}&amount=1`;
     const caloriesRes = await AJAX(url);
-    console.log(calories);
+    console.log(caloriesRes);
     const caloriesData = createCaloriesObject();
   } catch (err) {
     throw err;
