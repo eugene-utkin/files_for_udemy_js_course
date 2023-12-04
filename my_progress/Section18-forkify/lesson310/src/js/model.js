5847669dc9478b0ccde339f9d342f3b210a1e1ce
Evgeniy_Utkin <eugene.utkin@gmail.com>
2023-12-04 14:33:11 +0600

    const caloriesData removed

--- my_progress/Section18-forkify/lesson310/src/js/model.js
+++ my_progress/Section18-forkify/lesson310/src/js/model.js
@@ -78,7 +78,7 @@ export const loadCalories = async function (ingredient) {
     const caloriesRes = await AJAX(
       `https://api.spoonacular.com/food/ingredients/${ingData.results[0].id}/information?apiKey=${CALORIES_KEY}&amount=1`
     );
-    const caloriesData = createCaloriesObject(caloriesRes);
+    return createCaloriesObject(caloriesRes);
     return caloriesData;
   } catch (err) {
     throw err;
