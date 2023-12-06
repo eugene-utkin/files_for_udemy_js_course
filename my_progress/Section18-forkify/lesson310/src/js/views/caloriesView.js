2e97be86d18530590ebe40a472f6158fbff0e10a
Evgeniy_Utkin <eugene.utkin@gmail.com>
2023-12-06 13:07:28 +0600

    name used

--- my_progress/Section18-forkify/lesson310/src/js/views/caloriesView.js
+++ my_progress/Section18-forkify/lesson310/src/js/views/caloriesView.js
@@ -28,7 +28,7 @@ class CaloriesView extends View {
     console.log(this._data);
     return `
       <div class="calories-info">
-        <p></p>
+        <p>${this._data.name}</p>
       </div>
     `;
   }
