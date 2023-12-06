7119c9aa71c69b8fe7d9ccb29f3ac1d80232321c
Evgeniy_Utkin <eugene.utkin@gmail.com>
2023-12-06 13:17:22 +0600

    1 added

--- my_progress/Section18-forkify/lesson310/src/js/views/caloriesView.js
+++ my_progress/Section18-forkify/lesson310/src/js/views/caloriesView.js
@@ -28,7 +28,7 @@ class CaloriesView extends View {
   _generateMarkup() {
     return `
       <div class="calories-info">
-        <p>${capitalize(this._data.name)} - ${this._data.ingredientUnit}</p>
+        <p>${capitalize(this._data.name)} - 1${this._data.ingredientUnit}</p>
       </div>
     `;
   }
