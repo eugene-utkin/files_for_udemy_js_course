6248e181df75ea4872d67d27145d64db98361fd6
Evgeniy_Utkin <eugene.utkin@gmail.com>
2023-12-07 18:16:17 +0600

    addHandlerRender method removed

--- my_progress/Section18-forkify/lesson310/src/js/views/addToCalendarView.js
+++ my_progress/Section18-forkify/lesson310/src/js/views/addToCalendarView.js
@@ -1,10 +1,6 @@
 import View from './View.js';
 
 class AddToCalendarView extends View {
-  addHandlerRender(handler) {
-    window.addEventListener('load', handler);
-  }
-
   _generateMarkup() {
     return Object.entries(this._data)
       .map(day => addToCalendarItemView.render(day, false))
