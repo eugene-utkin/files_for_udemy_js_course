325abeaced6b2bded2337d8fce900be58a4c3e71
Evgeniy_Utkin <eugene.utkin@gmail.com>
2023-12-07 17:26:51 +0600

    _parentElement removed

--- my_progress/Section18-forkify/lesson310/src/js/views/addToCalendarView.js
+++ my_progress/Section18-forkify/lesson310/src/js/views/addToCalendarView.js
@@ -2,8 +2,6 @@ import View from './View.js';
 import addToCalendarItemView from './addToCalendarItemView.js';
 
 class AddToCalendarView extends View {
-  _parentElement = document.querySelector('.add-to-calendar-dropdown');
-
   addHandlerRender(handler) {
     window.addEventListener('load', handler);
   }
